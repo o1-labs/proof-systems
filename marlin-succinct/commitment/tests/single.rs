f00bf45d6f12f5af1758689133d93cca6053ae5c
Izaak Meckler <ihmeckler@gmail.com>
2019-12-18 16:02:14 -0800

    Initial implementation (#1)
    * adding everything back
    
    * This merge addresses the following comments:
    
    https://github.com/o1-labs/marlin/pull/1#discussion_r356299508
    https://github.com/o1-labs/marlin/pull/1#discussion_r356299622
    https://github.com/o1-labs/marlin/pull/1#discussion_r356314052
    https://github.com/o1-labs/marlin/pull/1#discussion_r356314213
    
    * This merge implements:
    
    1. code compactification with Rust ? operator
    2. absorbing group elements into the random oracle argument and rearranging the argument
    
    and thus addresses the following comments:
    
    https://github.com/o1-labs/marlin/pull/1#discussion_r356719098
    https://github.com/o1-labs/marlin/pull/1#discussion_r356717962
    https://github.com/o1-labs/marlin/pull/1#discussion_r356716203
    https://github.com/o1-labs/marlin/pull/1#discussion_r356314213
    https://github.com/o1-labs/marlin/pull/1#discussion_r356314052
    https://github.com/o1-labs/marlin/pull/1#discussion_r356714751
    https://github.com/o1-labs/marlin/pull/1#discussion_r356299622
    
    * This merges the following:
    1. Optimizations to the index compiler
    2. Minor code cleanup
    
    * This merge completes the following: optimizations to the prover's computation of h1, h2, h3 & g1, g2, g3 polynomials
    
    * This merge completes the implementaton of all the prover performance related optimizations
    
    * corrections and clanup
    
    * corrections and clanup
    
    * This merge completes the following
    1. Simplifies the implementation by merging batch_prover.rs & batch_verifier.rs functionality into prover.rs & verifier.rs
    2. Improves the performance tests
    
    * minor correction
    
    * remove single proof verification function since the same functionality is available via batch verification
    
    * This merge implements the batch verifier performance optimization with batch evaluation of Lagrenge polynomial
    
    * This merge replaces the modifications of the previous merge with better solution without the need of inversion by the verifier
    
    * minor cleanup
    
    * minor simplifications and cleanup
    
    * minor cleanup
    
    * correction for group size
    
    * correction for group size
    
    * replace naive witness check with efficient one
    
    * handle errors properly
    
    * handle errors properly
    
    * correction
    
    * minor correction

---
+++ marlin-succinct/commitment/tests/single.rs
@@ -0,0 +1,54 @@
+/*****************************************************************************************************************
+
+This source file implements the polynomial commitment unit test suite driver.
+The following tests are implemented:
+
+1. Polynomial commiment test
+    1. Generate URS instance of sufficient depth
+    2. Generate a random polynomials vector over the base field that fits into the URS depth.
+       The polynomial coefficients are random base field elements.
+    3. Commit to the polynomials against the URS instance
+    4. Evaluate the polynomials at a given randomly generated base field elements
+    5. Open the polynomials commitment at the given random base field element producing the opening proof
+    6. Verify the commitment opening proofs against the:
+        a. the URS instance
+        b. Polynomial evaluations at the given base field element
+        c. The given base field elements
+        d. Commitment opening proofs
+
+*****************************************************************************************************************/
+
+use algebra::{curves::bls12_381::Bls12_381, Field, PairingEngine, UniformRand};
+use commitment::urs::URS;
+use ff_fft::DensePolynomial;
+use rand_core::OsRng;
+
+#[test]
+fn single_commitment_test()
+{
+    test::<Bls12_381>();
+}
+
+fn test<E: PairingEngine>()
+{
+    let rng = &mut OsRng;
+    let depth = 500;
+
+    // generate sample URS
+    let urs = URS::<E>::create(depth, rng);
+
+    // generate sample random vector of polynomials over the base field, commit and evaluate them
+    let mut plnms: Vec<(E::Fr, E::Fr, Vec<(E::G1Affine, E::Fr, usize)>, E::G1Affine)> = Vec::new();
+    for _ in 0..10
+    {
+        let plnm = DensePolynomial::<E::Fr>::rand(depth-1, rng);
+        let y = E::Fr::rand(rng);
+        // Commit/Open and verify the polynomial commitments
+        match (urs.commit(&plnm, plnm.coeffs.len()), urs.open(&plnm, y))
+        {
+            (Ok(comm), Ok(prf)) => {plnms.push((y, E::Fr::one(), vec![(comm, plnm.evaluate(y), plnm.coeffs.len())], prf));}
+            (_,_) => {panic!("This error should not happen")}
+        }
+    }
+    assert_eq!(true, urs.verify(&vec![plnms; 1], rng));
+}
---
+++ marlin-succinct/oracle/src/rndoracle.rs
@@ -0,0 +1,88 @@
+/*****************************************************************************************************************
+
+This source file implements the random oracle argument API for Marlin.
+
+*****************************************************************************************************************/
+
+use std::fmt;
+use algebra::{PairingEngine, PrimeField, ToBytes, BigInteger, to_bytes};
+pub use super::poseidon::{ArithmeticSpongeParams, ArithmeticSponge, Sponge};
+
+#[derive(Debug, Clone, Copy)]
+pub enum ProofError
+{
+    WitnessCsInconsistent,
+    PolyDivision,
+    PolyCommit,
+    PolyExponentiate,
+    ProofCreation,
+    ProofVerification,
+    OpenProof,
+    SumCheck,
+    ConstraintInconsist,
+    EvaluationGroup,
+    OracleCommit,
+    RuntimeEnv
+}
+
+// Implement `Display` for ProofError
+impl fmt::Display for ProofError
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "({})", self)
+    }
+}
+
+pub struct RandomOracleArgument<E: PairingEngine>
+{
+    sponge: ArithmeticSponge<E::Fr>,
+    params: ArithmeticSpongeParams<E::Fr>,
+}
+
+impl<E: PairingEngine> RandomOracleArgument<E>
+{
+    pub fn new(params: ArithmeticSpongeParams<E::Fr>) -> Self
+    {
+        RandomOracleArgument::<E>
+        {
+            sponge: ArithmeticSponge::<E::Fr>::new(),
+            params,
+        }
+    }
+
+    pub fn commit_points(&mut self, points: &[E::G1Affine]) -> Result<bool, ProofError>
+    {
+        for point in points
+        {
+            let mut bytes: &[u8] = &to_bytes!(point).unwrap();
+            let mut limbs = <E::Fr as PrimeField>::BigInt::default();
+            let mut io_status = false;
+            while bytes.len() > 0
+            {
+                match limbs.read_le(&mut bytes)
+                {
+                    // make sure at least one scalar is obtained from the point
+                    Ok(_) => {io_status = true}
+                    _ => {}
+                }
+            }
+            if !io_status {return Err(ProofError::OracleCommit)}
+            self.sponge.absorb(&self.params, &E::Fr::from_repr(limbs));
+        }
+        Ok(true)
+    }
+
+    pub fn commit_scalars(&mut self, scalars: &[E::Fr])
+    {
+        for scalar in scalars.iter()
+        {
+            self.sponge.absorb(&self.params, scalar);
+        }
+    }
+
+    pub fn challenge(&mut self) -> E::Fr
+    {
+        self.sponge.squeeze(&self.params)
+    }    
+}
