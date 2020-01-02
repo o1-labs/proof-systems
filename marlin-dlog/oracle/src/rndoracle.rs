a8a0202332fbf70a6853143f7d2e8bdadd11cd50
Izaak Meckler <ihmeckler@gmail.com>
2020-01-02 11:58:18 -0800

    remove unused code from rndoracle

--- marlin-dlog/oracle/src/rndoracle.rs
+++ marlin-dlog/oracle/src/rndoracle.rs
@@ -5,7 +5,6 @@ This source file implements the random oracle argument API for Marlin.
 *****************************************************************************************************************/
 
 use std::fmt;
-use algebra::{PairingEngine, PrimeField, ToBytes, BigInteger, to_bytes};
 pub use super::poseidon::{ArithmeticSpongeParams, ArithmeticSponge, Sponge};
 
 #[derive(Debug, Clone, Copy)]
@@ -32,57 +31,4 @@ impl fmt::Display for ProofError
     {
         write!(f, "({})", self)
     }
-}
-
-pub struct RandomOracleArgument<E: PairingEngine>
-{
-    sponge: ArithmeticSponge<E::Fr>,
-    params: ArithmeticSpongeParams<E::Fr>,
-}
-
-impl<E: PairingEngine> RandomOracleArgument<E>
-{
-    pub fn new(params: ArithmeticSpongeParams<E::Fr>) -> Self
-    {
-        RandomOracleArgument::<E>
-        {
-            sponge: ArithmeticSponge::<E::Fr>::new(),
-            params,
-        }
-    }
-
-    pub fn commit_points(&mut self, points: &[E::G1Affine]) -> Result<bool, ProofError>
-    {
-        for point in points
-        {
-            let mut bytes: &[u8] = &to_bytes!(point).unwrap();
-            let mut limbs = <E::Fr as PrimeField>::BigInt::default();
-            let mut io_status = false;
-            while bytes.len() > 0
-            {
-                match limbs.read_le(&mut bytes)
-                {
-                    // make sure at least one scalar is obtained from the point
-                    Ok(_) => {io_status = true}
-                    _ => {}
-                }
-            }
-            if !io_status {return Err(ProofError::OracleCommit)}
-            self.sponge.absorb(&self.params, &E::Fr::from_repr(limbs));
-        }
-        Ok(true)
-    }
-
-    pub fn commit_scalars(&mut self, scalars: &[E::Fr])
-    {
-        for scalar in scalars.iter()
-        {
-            self.sponge.absorb(&self.params, scalar);
-        }
-    }
-
-    pub fn challenge(&mut self) -> E::Fr
-    {
-        self.sponge.squeeze(&self.params)
-    }    
 }
