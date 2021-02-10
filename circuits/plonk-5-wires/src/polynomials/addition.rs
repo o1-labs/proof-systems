112b309c3a5449f8674b35f382f5b6bfb3110a2b
mrmr1993 <mr_1993@hotmail.co.uk>
2021-02-10 16:04:44 +0000

    Refactor addition polynomials to be clearer

--- circuits/plonk-5-wires/src/polynomials/addition.rs
+++ circuits/plonk-5-wires/src/polynomials/addition.rs
@@ -43,33 +43,55 @@ impl<F: FftField + SquareRootField> ConstraintSystem<F>
     pub fn ecad_quot(&self, polys: &WitnessOverDomains<F>, alpha: &[F]) -> Evaluations<F, D<F>>
     {
         if self.addm.is_zero() {return self.zero4.clone()}
+
+        let x1 = &polys.d4.this.w[0];
+        let y1 = &polys.d4.this.w[1];
+        let x2 = &polys.d4.this.w[2];
+        let y2 = &polys.d4.this.w[3];
+        let x3 = &polys.d4.next.w[0];
+        let y3 = &polys.d4.next.w[1];
+        let r = &polys.d4.this.w[4];
+
+        let y31 = &(y3 + y1);
+        let x13 = &(x1 - x3);
+        let x21 = &(x2 - x1);
+
         /*
             (x2 - x1) * (y3 + y1) - (y2 - y1) * (x1 - x3)
             (x1 + x2 + x3) * (x1 - x3) * (x1 - x3) - (y3 + y1) * (y3 + y1)
             (x2 - x1) * r = 1
         */
-        let y31 = &(&polys.d4.next.w[1] + &polys.d4.this.w[1]);
-        let x13 = &(&polys.d4.this.w[0] - &polys.d4.next.w[0]);
-        let x21 = &(&polys.d4.this.w[2] - &polys.d4.this.w[0]);
+        let check_1 = &(&(x21 * y31) - &(&(y2 - y1) * x13));
+        let check_2 = &(&(&(&(x1 + x2) + x3) * &x13.pow(2)) - &y31.pow(2));
+        let check_3 = &(&(x21 * r) - &self.l04);
 
-        &(&(&(&(x21 * y31) - &(&(&polys.d4.this.w[3] - &polys.d4.this.w[1]) * x13)).scale(alpha[0])
-        +
-        &(&(&(&(&polys.d4.this.w[0] + &polys.d4.this.w[2]) + &polys.d4.next.w[0]) * &x13.pow(2)) - &y31.pow(2)).scale(alpha[1]))
-        +
-        &(&(x21 * &polys.d4.this.w[4]) - &self.l04).scale(alpha[2]))
-        *
-        &self.addl
+        &(&(  &check_1.scale(alpha[0])
+            + &check_2.scale(alpha[1]))
+            + &check_3.scale(alpha[2]))
+        * &self.addl
     }
 
     pub fn ecad_scalars(evals: &Vec<ProofEvaluations<F>>, alpha: &[F]) -> F
     {
-        let y31 = evals[1].w[1] + &evals[0].w[1];
-        let x13 = evals[0].w[0] - &evals[1].w[0];
-        let x21 = evals[0].w[2] - &evals[0].w[0];
+        let x1 = evals[0].w[0];
+        let y1 = evals[0].w[1];
+        let x2 = evals[0].w[2];
+        let y2 = evals[0].w[3];
+        let x3 = evals[1].w[0];
+        let y3 = evals[1].w[1];
+        let r  = evals[0].w[4];
 
-        ((x21 * y31) - &((evals[0].w[3] - &evals[0].w[1]) * x13)) * &alpha[0] +
-        &(((evals[0].w[0] + &evals[0].w[2] + &evals[1].w[0]) * &x13.square() - &y31.square()) * &alpha[1]) +
-        &((x21 * &evals[0].w[4] - &F::one()) * &alpha[2])
+        let y31 = y3 + y1;
+        let x13 = x1 - x3;
+        let x21 = x2 - x1;
+
+        let check_1 = (x21 * y31) - ((y2 - y1) * x13);
+        let check_2 = ((x1 + x2 + x3) * x13.square()) - y31.square();
+        let check_3 = (x21 * r) - F::one();
+
+          (check_1 * alpha[0])
+        + (check_2 * alpha[1])
+        + (check_3 * alpha[2])
     }
 
     // EC Affine addition constraint linearization poly contribution computation
