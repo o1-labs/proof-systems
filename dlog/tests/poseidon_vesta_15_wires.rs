fd23c31ca4dac2f9cfe06ebe6e64b584e8462a66
David Wong <davidwong.crypto@gmail.com>
2021-08-27 12:09:12 -0700

    [15-wires] make poseidon test pass

--- dlog/tests/poseidon_vesta_15_wires.rs
+++ dlog/tests/poseidon_vesta_15_wires.rs
@@ -4,7 +4,7 @@ use algebra::{
         pallas::Affine as Other,
         vesta::{Affine, VestaParameters},
     },
-    UniformRand, Zero,
+    Field, One, UniformRand, Zero,
 };
 use array_init::array_init;
 use colored::Colorize;
@@ -18,66 +18,95 @@ use oracle::{
     poseidon::{ArithmeticSponge, Plonk15SpongeConstants, Sponge, SpongeConstants},
     sponge::{DefaultFqSponge, DefaultFrSponge},
 };
-use plonk_15_wires_circuits::wires::Wire;
+use plonk_15_wires_circuits::wires::{Wire, COLUMNS};
 use plonk_15_wires_circuits::{
     gate::CircuitGate,
-    gates::poseidon::{round_range, ROUNDS_PER_ROW, SPONGE_WIDTH},
+    gates::poseidon::{round_to_cols, ROUNDS_PER_ROW, SPONGE_WIDTH},
     nolookup::constraints::ConstraintSystem,
 };
 use plonk_15_wires_protocol_dlog::{
     index::{Index, SRSSpec},
     prover::ProverProof,
 };
-use rand_core::OsRng;
+use rand::{rngs::StdRng, SeedableRng};
 use std::time::Instant;
 use std::{io, io::Write};
 
-// const PERIOD: usize = Plonk15SpongeConstants::ROUNDS_FULL + 1;
+// aliases
+
+type SpongeParams = Plonk15SpongeConstants;
+type BaseSponge = DefaultFqSponge<VestaParameters, SpongeParams>;
+type ScalarSponge = DefaultFrSponge<Fp, SpongeParams>;
+
+// const PERIOD: usize = SpongeParams::ROUNDS_FULL + 1;
 // const M: usize = PERIOD * (NUM_POS-1);
 // const MAX_SIZE: usize = N; // max size of poly chunks
 const PUBLIC: usize = 0;
 
 const NUM_POS: usize = 1; // 1360; // number of Poseidon hashes in the circuit
-const ROUNDS_PER_HASH: usize = Plonk15SpongeConstants::ROUNDS_FULL;
+const ROUNDS_PER_HASH: usize = SpongeParams::ROUNDS_FULL;
 const POS_ROWS_PER_HASH: usize = ROUNDS_PER_HASH / ROUNDS_PER_ROW;
 const N_LOWER_BOUND: usize = (POS_ROWS_PER_HASH + 1) * NUM_POS; // Plonk domain size
 
 #[test]
 fn poseidon_vesta_15_wires() {
-    let N = 1 << ceil_log2(N_LOWER_BOUND);
-    println!("N = {}", N);
-    println!("{} {}", ROUNDS_PER_HASH, ROUNDS_PER_ROW);
+    let max_size = 1 << ceil_log2(N_LOWER_BOUND);
+    println!("max_size = {}", max_size);
+    println!("rounds per hash = {}", ROUNDS_PER_HASH);
+    println!("rounds per row = {}", ROUNDS_PER_ROW);
+    println!(" number of rows for poseidon ={}", POS_ROWS_PER_HASH);
     assert_eq!(ROUNDS_PER_HASH % ROUNDS_PER_ROW, 0);
 
-    let c = &oracle::pasta::fp::params().round_constants;
+    let round_constants = &oracle::pasta::fp::params().round_constants;
+
+    // we keep track of an absolute row, and relative row within a gadget
+    let mut abs_row = 0;
 
     // circuit gates
-
-    let mut i = 0;
-    let mut gates: Vec<CircuitGate<Fp>> = Vec::with_capacity(N);
+    let mut gates: Vec<CircuitGate<Fp>> = Vec::with_capacity(max_size);
 
     // custom constraints for Poseidon hash function permutation
-
+    // ROUNDS_FULL full rounds constraint gates
     for _ in 0..NUM_POS {
-        // ROUNDS_FULL full rounds constraint gates
-        for j in 0..POS_ROWS_PER_HASH {
-            let wires = array_init(|col| Wire { col, row: i });
-            let coeffs = array_init(|r| {
-                let round = j * ROUNDS_PER_ROW + r + 1;
-                array_init(|k| c[round][k])
+        let first_wire = array_init(|col| Wire { col, row: abs_row });
+        let last_row = abs_row + POS_ROWS_PER_HASH;
+        let last_wire = array_init(|col| Wire { col, row: last_row });
+        let (poseidon, row) = CircuitGate::<Fp>::create_poseidon_gadget(
+            abs_row,
+            [first_wire, last_wire],
+            round_constants,
+        );
+        gates.extend(poseidon);
+        abs_row = row;
+
+        /*
+        // create a poseidon gadget manully
+        for rel_row in 0..POS_ROWS_PER_HASH {
+            // the 15 wires for this row
+            let wires = array_init(|col| Wire { col, row: abs_row });
+
+            // round constant for this row
+            let coeffs = array_init(|offset| {
+                let round = rel_row * ROUNDS_PER_ROW + offset + 1;
+                array_init(|field_el| round_constants[round][field_el])
             });
-            gates.push(CircuitGate::<Fp>::create_poseidon(i, wires, coeffs));
-            i += 1;
+
+            // create poseidon gate for this row
+            gates.push(CircuitGate::<Fp>::create_poseidon(abs_row, wires, coeffs));
+            abs_row += 1;
         }
-        let wires = array_init(|col| Wire { col, row: i });
-        gates.push(CircuitGate::<Fp>::zero(i, wires));
-        i += 1;
+
+        // final (zero) gate that contains the output of poseidon
+        let wires = array_init(|col| Wire { col, row: abs_row });
+        gates.push(CircuitGate::<Fp>::zero(abs_row, wires));
+        abs_row += 1;
+        */
     }
 
     /*
-    for j in 0..Plonk15SpongeConstants::ROUNDS_FULL-2
+    for j in 0..SpongeParams::ROUNDS_FULL-2
     {
-        gates.push(CircuitGate::<Fp>::create_poseidon(i, [Wire{col:0, row:i}, Wire{col:1, row:i}, Wire{col:2, row:i}, Wire{col:3, row:i}, Wire{col:4, row:i}], c[j].clone()));
+        gates.push(CircuitGate::<Fp>::create_poseidon(i, [Wire{col:0, row:i}, Wire{col:1, row:i}, Wire{col:2, row:i}, Wire{col:3, row:i}, Wire{col:4, row:i}], round_constants[j].clone()));
         i+=1;
     }
     gates.push(CircuitGate::<Fp>::zero(i, [Wire{col:0, row:i}, Wire{col:1, row:i}, Wire{col:2, row:i}, Wire{col:3, row:i}, Wire{col:4, row:i}]));
@@ -87,34 +116,36 @@ fn poseidon_vesta_15_wires() {
     gates.push(CircuitGate::<Fp>::zero(i, [Wire{col:0, row:i}, Wire{col:1, row:i}, Wire{col:2, row:i}, Wire{col:3, row:i}, Wire{col:4, row:i}]));
     */
 
-    let MAX_SIZE = N;
-    let srs = SRS::create(MAX_SIZE);
-
+    // create the index
+    let fp_sponge_params = oracle::pasta::fp::params();
+    let cs = ConstraintSystem::<Fp>::create(gates, fp_sponge_params, PUBLIC).unwrap();
+    let fq_sponge_params = oracle::pasta::fq::params();
     let (endo_q, _endo_r) = endos::<Other>();
-    let index = Index::<Affine>::create(
-        ConstraintSystem::<Fp>::create(gates, oracle::pasta::fp::params(), PUBLIC).unwrap(),
-        oracle::pasta::fq::params(),
-        endo_q,
-        SRSSpec::Use(&srs),
-    );
+    let srs = SRS::create(max_size);
+    let srs = SRSSpec::Use(&srs);
+
+    let index = Index::<Affine>::create(cs, fq_sponge_params, endo_q, srs);
 
     positive(&index);
 }
 
+// creates a proof and verifies it
 fn positive(index: &Index<Affine>) {
-    let rng = &mut OsRng;
+    // constant
+    let max_size = 1 << ceil_log2(N_LOWER_BOUND);
 
+    // set up
+    let rng = &mut StdRng::from_seed([0u8; 32]);
     let params = oracle::pasta::fp::params();
-    let mut sponge = ArithmeticSponge::<Fp, Plonk15SpongeConstants>::new();
-
-    let mut batch = Vec::new();
+    let mut sponge = ArithmeticSponge::<Fp, SpongeParams>::new();
     let group_map = <Affine as CommitmentCurve>::Map::setup();
 
-    let N = 1 << ceil_log2(N_LOWER_BOUND);
-    let MAX_SIZE = N;
+    // batching what?
+    let mut batch = Vec::new();
 
-    println!("{}{:?}", "Circuit size: ".yellow(), N);
-    println!("{}{:?}", "Polycommitment chunk size: ".yellow(), MAX_SIZE);
+    // debug
+    println!("{}{:?}", "Circuit size: ".yellow(), max_size);
+    println!("{}{:?}", "Polycommitment chunk size: ".yellow(), max_size);
     println!(
         "{}{:?}",
         "Number oh Poseidon hashes in the circuit: ".yellow(),
@@ -123,38 +154,22 @@ fn positive(index: &Index<Affine>) {
     println!(
         "{}{:?}",
         "Full rounds: ".yellow(),
-        Plonk15SpongeConstants::ROUNDS_FULL
+        SpongeParams::ROUNDS_FULL
     );
-    println!(
-        "{}{:?}",
-        "Sbox alpha: ".yellow(),
-        Plonk15SpongeConstants::SPONGE_BOX
-    );
-    println!("{}", "Base curve: vesta".green());
-    println!();
+    println!("{}{:?}", "Sbox alpha: ".yellow(), SpongeParams::SPONGE_BOX);
+    println!("{}", "Base curve: vesta\n".green());
     println!("{}", "Prover zk-proof computation".green());
+
     let mut start = Instant::now();
 
+    // why would there be several tests?
     for test in 0..1 {
-        //  witness for Poseidon permutation custom constraints
-        let mut w = [
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-            vec![Fp::zero(); N],
-        ];
+        // witness for Poseidon permutation custom constraints
+        // (15 columns of vectors, each vector is of size n)
+        let mut witness: [Vec<Fp>; COLUMNS] = array_init(|_| vec![Fp::zero(); max_size]);
+        println!("witness: {:?}", witness);
 
+        // creates a random initial state
         let init = vec![
             Fp::rand(rng),
             Fp::rand(rng),
@@ -162,28 +177,46 @@ fn positive(index: &Index<Affine>) {
             Fp::rand(rng),
             Fp::rand(rng),
         ];
+
+        // number of poseidon instances in the circuit
         for h in 0..NUM_POS {
-            let base = h * (POS_ROWS_PER_HASH + 1);
-            for i in 0..SPONGE_WIDTH {
-                w[round_range(0)][i][base] = init[i];
+            // index
+            // TODO: is the `+ 1` correct?
+            let first_row = h * (POS_ROWS_PER_HASH + 1);
+
+            // initialize the sponge in the circuit with our random state
+            let first_state_cols = &mut witness[round_to_cols(0)];
+            for state_idx in 0..SPONGE_WIDTH {
+                first_state_cols[state_idx][first_row] = init[state_idx];
             }
 
+            // set the sponge state
             sponge.state = init.clone();
 
-            for i in 0..POS_ROWS_PER_HASH {
-                let row = i + base;
-                for r in 0..ROUNDS_PER_ROW {
-                    let next_row = if r == ROUNDS_PER_ROW - 1 {
+            // for the poseidon rows
+            for row_idx in 0..POS_ROWS_PER_HASH {
+                let row = row_idx + first_row;
+                for round in 0..ROUNDS_PER_ROW {
+                    // the last round makes use of the next row
+                    let maybe_next_row = if round == ROUNDS_PER_ROW - 1 {
                         row + 1
                     } else {
                         row
                     };
-                    let abs_round = r + i * ROUNDS_PER_ROW;
+
+                    //
+                    let abs_round = round + row_idx * ROUNDS_PER_ROW;
+
+                    // TODO: this won't work if the circuit has an INITIAL_ARK
                     sponge.full_round(abs_round, &params);
-                    w[round_range((r + 1) % ROUNDS_PER_ROW)]
+
+                    // apply the sponge and record the result in the witness
+                    let cols_to_update = round_to_cols((round + 1) % ROUNDS_PER_ROW);
+                    witness[cols_to_update]
                         .iter_mut()
                         .zip(sponge.state.iter())
-                        .for_each(|(w, s)| w[next_row] = *s);
+                        // update the state (last update is on the next row)
+                        .for_each(|(w, s)| w[maybe_next_row] = *s);
                 }
             }
         }
@@ -193,7 +226,7 @@ fn positive(index: &Index<Affine>) {
         w.iter_mut().zip(sponge.state.iter()).for_each(|(w, s)| w.push(*s));
 
         // ROUNDS_FULL full rounds
-        for j in 0..Plonk15SpongeConstants::ROUNDS_FULL-2
+        for j in 0..SpongeParams::ROUNDS_FULL-2
         {
             sponge.full_round(j, &params);
             w.iter_mut().zip(sponge.state.iter()).for_each(|(w, s)| w.push(*s));
@@ -202,50 +235,59 @@ fn positive(index: &Index<Affine>) {
         w.iter_mut().for_each(|w| {w.push(Fp::rand(rng)); w.push(Fp::rand(rng))}); */
 
         // verify the circuit satisfiability by the computed witness
-        assert_eq!(index.cs.verify(&w), true);
+        assert_eq!(index.cs.verify(&witness), true);
 
+        // what is this thing?
         let prev = {
+            // ?
             let k = ceil_log2(index.srs.get_ref().g.len());
+
+            // random challenges
             let chals: Vec<_> = (0..k).map(|_| Fp::rand(rng)).collect();
+
+            // non-hiding commitments of b polyonmials (made out of challenges) ???
             let comm = {
-                let b = DensePolynomial::from_coefficients_vec(b_poly_coefficients(&chals));
+                let coeffs = b_poly_coefficients(&chals);
+                let b = DensePolynomial::from_coefficients_vec(coeffs);
                 index.srs.get_ref().commit_non_hiding(&b, None)
             };
+
             (chals, comm)
         };
 
-        println!("n vs domain{} {}", N, index.cs.domain.d1.size);
+        println!("n vs domain{} {}", max_size, index.cs.domain.d1.size);
 
         // add the proof to the batch
+        // TODO: create and verify should not take group_map, that should be during an init phase
         batch.push(
-            ProverProof::create::<
-                DefaultFqSponge<VestaParameters, Plonk15SpongeConstants>,
-                DefaultFrSponge<Fp, Plonk15SpongeConstants>,
-            >(&group_map, &w, &index, vec![prev])
+            ProverProof::create::<BaseSponge, ScalarSponge>(
+                &group_map,
+                &witness,
+                &index,
+                vec![prev],
+            )
             .unwrap(),
         );
 
         print!("{:?}\r", test);
         io::stdout().flush().unwrap();
     }
+    // TODO: this should move to a bench
     println!("{}{:?}", "Execution time: ".yellow(), start.elapsed());
 
+    // TODO: shouldn't verifier_index be part of ProverProof, not being passed in verify?
     let verifier_index = index.verifier_index();
 
-    let lgr_comms = vec![];
+    let lgr_comms = vec![]; // why empty?
     let batch: Vec<_> = batch
         .iter()
-        .map(|p| (&verifier_index, &lgr_comms, p))
+        .map(|proof| (&verifier_index, &lgr_comms, proof))
         .collect();
 
     // verify the proofs in batch
     println!("{}", "Verifier zk-proofs verification".green());
     start = Instant::now();
-    match ProverProof::verify::<
-        DefaultFqSponge<VestaParameters, Plonk15SpongeConstants>,
-        DefaultFrSponge<Fp, Plonk15SpongeConstants>,
-    >(&group_map, &batch)
-    {
+    match ProverProof::verify::<BaseSponge, ScalarSponge>(&group_map, &batch) {
         Err(error) => panic!("Failure verifying the prover's proofs in batch: {}", error),
         Ok(_) => {
             println!("{}{:?}", "Execution time: ".yellow(), start.elapsed());
