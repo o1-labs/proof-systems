aaaaeb1f25c13728c2f35703138b0ecd6c228031
Izaak Meckler <ihmeckler@gmail.coom>
2021-09-24 10:41:03 -0700

    add index commitments

--- dlog/plonk-15-wires/src/index.rs
+++ dlog/plonk-15-wires/src/index.rs
@@ -15,6 +15,9 @@ use commitment_dlog::{
 };
 use oracle::poseidon::{ArithmeticSpongeParams, PlonkSpongeConstants15W, SpongeConstants};
 use plonk_15_wires_circuits::{
+    polynomials::{chacha, lookup},
+    gate::{LookupInfo, LookupsUsed},
+    expr::{PolishToken, Expr, Column, Linearization},
     gates::poseidon::ROUNDS_PER_ROW,
     nolookup::constraints::{zk_w3, ConstraintSystem},
     wires::*,
@@ -66,6 +69,9 @@ where
     /// constraints system polynomials
     pub cs: ConstraintSystem<Fr<G>>,
 
+    pub lookup_used: Option<LookupsUsed>,
+    pub linearization: Linearization<Vec<PolishToken<Fr<G>>>>,
+
     /// polynomial commitment keys
     pub srs: SRSValue<'a, G>,
 
@@ -116,6 +122,9 @@ pub struct VerifierIndex<'a, G: CommitmentCurve> {
     /// endoscalar multiplication selector polynomial commitment
     pub emul_comm: PolyComm<G>,
 
+    /// Chacha polynomial commitments
+    pub chacha_comm: Option<[PolyComm<G>; 4]>,
+
     /// wire coordinate shifts
     pub shift: [Fr<G>; PERMUTS],
     /// zero-knowledge polynomial
@@ -125,6 +134,11 @@ pub struct VerifierIndex<'a, G: CommitmentCurve> {
     pub w: Fr<G>,
     /// endoscalar coefficient
     pub endo: Fr<G>,
+
+    pub lookup_used: Option<LookupsUsed>,
+    pub lookup_tables: Vec<Vec<PolyComm<G>>>,
+    pub lookup_selectors: Vec<PolyComm<G>>,
+    pub linearization: Linearization<Vec<PolishToken<Fr<G>>>>,
 
     // random oracle argument parameters
     pub fr_sponge_params: ArithmeticSpongeParams<Fr<G>>,
@@ -142,8 +156,10 @@ where
             SRSValue::Ref(x) => SRSValue::Ref(x),
         };
 
+        let domain = self.cs.domain.d1;
+        // TODO: Switch to commit_evaluations for all index polys
         VerifierIndex {
-            domain: self.cs.domain.d1,
+            domain,
 
             sigma_comm: array_init(|i| srs.get_ref().commit_non_hiding(&self.cs.sigmam[i], None)),
             qw_comm: array_init(|i| srs.get_ref().commit_non_hiding(&self.cs.qwm[i], None)),
@@ -160,6 +176,18 @@ where
             mul_comm: srs.get_ref().commit_non_hiding(&self.cs.mulm, None),
             emul_comm: srs.get_ref().commit_non_hiding(&self.cs.emulm, None),
 
+            chacha_comm:
+                self.cs.chacha8.as_ref()
+                .map(|c| array_init(|i| srs.get_ref().commit_evaluations_non_hiding(domain, &c[i], None))),
+            lookup_selectors:
+                self.cs.lookup_selectors.iter()
+                .map(|e| srs.get_ref().commit_evaluations_non_hiding(domain, e, None))
+                .collect(),
+            lookup_tables:
+                self.cs.lookup_tables8.iter()
+                .map(|v| v.iter().map(|e| srs.get_ref().commit_evaluations_non_hiding(domain, e, None)).collect())
+                .collect(),
+
             w: zk_w3(self.cs.domain.d1),
             fr_sponge_params: self.cs.fr_sponge_params.clone(),
             fq_sponge_params: self.fq_sponge_params.clone(),
@@ -168,6 +196,8 @@ where
             max_quot_size: self.max_quot_size,
             zkpm: self.cs.zkpm.clone(),
             shift: self.cs.shift,
+            linearization: self.linearization.clone(),
+            lookup_used: self.lookup_used,
             srs,
         }
     }
@@ -188,6 +218,40 @@ where
             );
         }
         cs.endo = endo_q;
+
+        let lookup_info = LookupInfo::<Fr<G>>::create();
+        let lookup_used = lookup_info.lookup_used(&cs.gates);
+
+        let linearization = {
+            let evaluated_cols = {
+                let mut h = std::collections::HashSet::new();
+                use Column::*;
+                for i in 0..COLUMNS {
+                    h.insert(Witness(i));
+                }
+                for i in 0..(lookup_info.max_per_row + 1) {
+                    h.insert(LookupSorted(i));
+                }
+                h.insert(Z);
+                h.insert(LookupAggreg);
+                h.insert(LookupTable);
+                h
+            };
+            if lookup_used.is_some() {
+                (chacha::constraint(super::range::CHACHA.start)
+                    + Expr::combine_constraints(super::range::CHACHA.end,
+                        lookup::constraints(
+                            &cs.dummy_lookup_values[0], cs.domain.d1)))
+                .linearize(evaluated_cols)
+                .unwrap()
+            } else {
+                Linearization {
+                    constant_term: 0.into(),
+                    index_terms: vec![]
+                }
+            }
+        };
+
         Index {
             // TODO(mimoo): re-order field like in the type def
             // max_quot_size: PlonkSpongeConstants::SPONGE_BOX * (pcs.cs.domain.d1.size as usize - 1),
@@ -196,6 +260,8 @@ where
             max_poly_size,
             srs,
             cs,
+            lookup_used,
+            linearization: linearization.map(|e| e.to_polish())
         }
     }
 }
