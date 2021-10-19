c7db357b25ef2f40b63be8c1f0fdf346268cba99
Izaak Meckler <ihmeckler@gmail.coom>
2021-10-19 10:16:04 -0400

    fix compilation

--- dlog/plonk-15-wires/src/prover.rs
+++ dlog/plonk-15-wires/src/prover.rs
@@ -798,6 +798,7 @@ pub mod caml {
                 ],
                 z_comm: caml_prover_comm.z_comm.into(),
                 t_comm: caml_prover_comm.t_comm.into(),
+                lookup: None,
             }
         }
     }
