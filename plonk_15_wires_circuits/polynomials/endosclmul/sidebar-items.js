dba547583ffd0b3b4b231f5d5dbcbee8b04af1dc
imeckler <imeckler@users.noreply.github.com>
2021-10-29 14:16:21 +0000

    deploy: 32aab4c61a0ec87b9a9a4dfee76012afc37985cc

--- plonk_15_wires_circuits/polynomials/endosclmul/sidebar-items.js
+++ plonk_15_wires_circuits/polynomials/endosclmul/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"fn":[["constraint","The constraint for endoscaling."],["witness","Generates the witness values for a series of endoscaling constraints."]],"struct":[["EndoMulResult","The result of performing an endoscaling: the accumulated curve point and scalar."]]});
