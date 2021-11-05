aa47cfe4637eac9a8d355b81221a0cf38cd0368b
David Wong <davidwong.crypto@gmail.com>
2021-11-05 16:52:11 -0700

    cargo fmt

--- dlog/kimchi/src/tests/endomul_scalar.rs
+++ dlog/kimchi/src/tests/endomul_scalar.rs
@@ -1,3 +1,4 @@
+use crate::{index::Index, prover::ProverProof};
 use ark_ff::{BigInteger, BitIteratorLE, PrimeField, UniformRand};
 use array_init::array_init;
 use colored::Colorize;
@@ -6,7 +7,6 @@ use commitment_dlog::{
     srs::{endos, SRS},
 };
 use groupmap::GroupMap;
-use crate::{index::Index, prover::ProverProof};
 use kimchi_circuits::{
     gate::{CircuitGate, GateType},
     nolookup::constraints::ConstraintSystem,
