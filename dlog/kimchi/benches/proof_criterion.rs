592c5d788af9ab30621d054c9ace93326b76fa19
David Wong <davidwong.crypto@gmail.com>
2021-11-12 19:25:16 -0800

    test

--- dlog/kimchi/benches/proof_criterion.rs
+++ dlog/kimchi/benches/proof_criterion.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use kimchi::bench::proof;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("proof 20", |b| b.iter(|| proof(black_box(20))));
+    c.bench_function("proof 10", |b| b.iter(|| proof(black_box(10))));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- dlog/kimchi/benches/proof_iai.rs
+++ dlog/kimchi/benches/proof_iai.rs
@@ -2,11 +2,11 @@ use iai::black_box;
 use kimchi::bench::proof;
 
 fn iai_benchmark_short() {
-    proof(black_box(20))
+    proof(black_box(5))
 }
 
 fn iai_benchmark_long() {
-    proof(black_box(30))
+    proof(black_box(15))
 }
 
 iai::main!(iai_benchmark_short, iai_benchmark_long);
