0a25004e92465c262c0d05707449776220e2e94f
David Wong <davidwong.crypto@gmail.com>
2022-01-21 11:48:52 -0800

    [bench] get rid of witness creation

--- kimchi/src/bench.rs
+++ kimchi/src/bench.rs
@@ -33,7 +33,6 @@ pub const CIRCUIT_SIZE: usize = (1 << 15) + 1; // SRS will be 2^16
 
 pub struct BenchmarkCtx {
     group_map: BWParameters<VestaParameters>,
-    num_gates: usize,
     index: Index<Affine>,
     verifier_index: VerifierIndex<Affine>,
 }
@@ -50,7 +49,7 @@ impl BenchmarkCtx {
         #[allow(clippy::explicit_counter_loop)] // TODO: fix this clippy
         for row in 0..num_gates {
             let wires = Wire::new(row);
-            gates.push(CircuitGate::<Fp>::create_generic_mul(wires));
+            gates.push(CircuitGate::<Fp>::create_generic_const(wires, 1u32.into()));
         }
 
         for row in num_gates..CIRCUIT_SIZE {
@@ -61,20 +60,19 @@ impl BenchmarkCtx {
         // group map
         let group_map = <Affine as CommitmentCurve>::Map::setup();
 
-        // create the CS
-        let fp_sponge_params = oracle::pasta::fp::params();
-        let cs = ConstraintSystem::<Fp>::create(gates, vec![], fp_sponge_params, 0).unwrap();
-        let n = cs.domain.d1.size as usize;
-        let fq_sponge_params = oracle::pasta::fq::params();
-        let (endo_q, _endo_r) = endos::<Other>();
-
-        // create the SRS
-        let mut srs = SRS::create(n);
-        srs.add_lagrange_basis(cs.domain.d1);
-        let srs = Arc::new(srs);
-
         // create the index
-        let index = Index::<Affine>::create(cs, fq_sponge_params, endo_q, srs);
+        let index = {
+            let fp_sponge_params = oracle::pasta::fp::params();
+            let cs = ConstraintSystem::<Fp>::create(gates, vec![], fp_sponge_params, 0).unwrap();
+            let n = cs.domain.d1.size as usize;
+            let fq_sponge_params = oracle::pasta::fq::params();
+            let (endo_q, _endo_r) = endos::<Other>();
+
+            let mut srs = SRS::create(n);
+            srs.add_lagrange_basis(cs.domain.d1);
+            let srs = Arc::new(srs);
+            Index::<Affine>::create(cs, fq_sponge_params, endo_q, srs)
+        };
 
         // create the verifier index
         let verifier_index = index.verifier_index();
@@ -82,7 +80,6 @@ impl BenchmarkCtx {
         //
         BenchmarkCtx {
             group_map,
-            num_gates,
             index,
             verifier_index,
         }
@@ -94,12 +91,7 @@ impl BenchmarkCtx {
         let rng = &mut StdRng::from_seed([0u8; 32]);
 
         // create witness
-        let mut witness: [Vec<Fp>; COLUMNS] = array_init(|_| vec![Fp::zero(); CIRCUIT_SIZE]);
-        for row in 0..self.num_gates {
-            witness[0][row] = 3u32.into();
-            witness[1][row] = 5u32.into();
-            witness[2][row] = Fp::from(3u32 * 5);
-        }
+        let witness: [Vec<Fp>; COLUMNS] = array_init(|_| vec![1u32.into(); CIRCUIT_SIZE]);
 
         // previous opening for recursion
         let prev = {
@@ -141,7 +133,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_short_bench() {
+    fn test_bench() {
         // context created in 21.2235 ms
         let start = Instant::now();
         let ctx = BenchmarkCtx::new(1 << 4);
@@ -153,24 +145,6 @@ mod tests {
         println!("proof created in {}", start.elapsed().as_millis());
 
         // proof verified in 1.710 ms
-        let start = Instant::now();
-        ctx.batch_verification(vec![proof.clone()]);
-        println!("proof verified in {}", start.elapsed().as_millis());
-    }
-
-    #[test]
-    fn test_long_bench() {
-        // context created in 23.3520 ms
-        let start = Instant::now();
-        let ctx = BenchmarkCtx::new(1 << 15);
-        println!("context created in {}", start.elapsed().as_millis());
-
-        // proof created in 8.1972 ms
-        let start = Instant::now();
-        let proof = ctx.create_proof();
-        println!("proof created in {}", start.elapsed().as_millis());
-
-        // proof verified in 1.802 ms
         let start = Instant::now();
         ctx.batch_verification(vec![proof.clone()]);
         println!("proof verified in {}", start.elapsed().as_millis());
