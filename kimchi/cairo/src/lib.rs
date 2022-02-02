1d1d52fff40a3dc1a6b1aedaa8277570ceca66f3
Anaïs Querol <anais.querol>
2022-02-02 13:41:47 +0100

    fix clippy error

--- kimchi/cairo/src/lib.rs
+++ kimchi/cairo/src/lib.rs
@@ -1,3 +1,7 @@
 #![warn(missing_docs)]
 //! This module contains the code that executes a compiled Cairo program and generates the memory
+/// The Cairo runner includes code to execute a bytecode compiled Cairo program,
+/// and obtain a memory instantiation after the execution. It uses some code to
+/// represent Cairo instructions and their decomposition, together with their logic
+/// which is represented as steps of computation making up the full program.
 pub mod runner;
