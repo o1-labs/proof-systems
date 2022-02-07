10956dae7963b294a78b9940652212e52478d731
Joseph Spadavecchia <joseph@redtrie.com>
2022-02-07 14:09:49 +0000

    Added ForeignMul1 range-constraints and limb check

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -3,6 +3,7 @@ pub mod evaluations;
 pub mod field_helpers;
 pub mod serialization;
 pub mod packed_modulus;
+pub mod serialization;
 
 pub use dense_polynomial::ExtendedDensePolynomial;
 pub use evaluations::ExtendedEvaluations;
