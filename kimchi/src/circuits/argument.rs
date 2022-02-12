a9434cb560e2e0d4fbbe4543b1cc690af643cfb6
David Wong <davidwong.crypto@gmail.com>
2022-02-11 16:10:19 -0800

    fixup! [argument] introduce an Argument trait

--- kimchi/src/circuits/argument.rs
+++ kimchi/src/circuits/argument.rs
@@ -4,20 +4,59 @@
 //! Gates can be seen as filtered arguments,
 //! which apply only in some points (rows) of the domain.
 
-use ark_ff::FftField;
-
 use crate::{
     alphas::Alphas,
-    circuits::expr::{ConstantExpr, Expr},
+    circuits::expr::{prologue::*, Column},
 };
+use ark_ff::FftField;
+use serde::{Deserialize, Serialize};
 
-/// The interface for a minimal argument implementation
+use super::gate::GateType;
+
+/// A constraint type represents a polynomial that will be part of the final equation f (the circuit equation)
+#[derive(PartialEq, Eq, Clone, Copy, Hash, Debug, Serialize, Deserialize)]
+pub enum ArgumentType {
+    /// Gates in the PLONK constraint system.
+    /// As gates are mutually exclusive (a single gate is set per row),
+    /// we can reuse the same powers of alpha accross gates.
+    Gate,
+    /// The permutation argument
+    Permutation,
+    /// The lookup argument
+    Lookup,
+}
+
+/// The interface for a minimal argument implementation.
 pub trait Argument {
+    /// The field being used for this argument.
     type Field: FftField;
 
-    /// The number of constraints created by the argument
+    /// The type of constraints that this will produce.
+    /// This is important to enforce that we don't combine the constraints
+    /// with powers of alpha that collide with other mutually inclusive arguments.
+    const ARGUMENT_TYPE: ArgumentType;
+
+    /// The number of constraints created by the argument.
     const CONSTRAINTS: usize;
 
-    /// Returns constraints safely combined via the passed combinator
-    fn constraint(&self, combinator: &Alphas<Self::Field>) -> Expr<ConstantExpr<Self::Field>>;
+    /// An optional gate type, if used to define a gate.
+    /// This is used to filter the gate, to avoid applying it on the entire domain.
+    const GATE_TYPE: Option<GateType>;
+
+    /// Returns the set of constraints required to prove this argument.
+    // TODO: return a [_; Self::CONSTRAINTS] once generic consts are stable
+    fn constraints(&self) -> Vec<E<Self::Field>>;
+
+    /// Returns constraints safely combined via the passed combinator.
+    fn combined_constraints(&self, alphas: &Alphas<Self::Field>) -> E<Self::Field> {
+        let constraints = self.constraints();
+        assert!(constraints.len() == Self::CONSTRAINTS);
+        let alphas = alphas.get_exponents(Self::ARGUMENT_TYPE, Self::CONSTRAINTS);
+        let combined_constraints = E::combine_constraints(alphas, constraints);
+        if let Some(gate_type) = Self::GATE_TYPE {
+            index(gate_type) * combined_constraints
+        } else {
+            combined_constraints
+        }
+    }
 }
