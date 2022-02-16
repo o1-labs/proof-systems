4b60eaa70efaa01d78382a80590bda36bb445ae7
Anaïs Querol <anais.querol@gmail.com>
2022-02-16 19:26:50 +0100

    fix clippy else if let redundancy

--- cairo/src/memory.rs
+++ cairo/src/memory.rs
@@ -40,13 +40,11 @@ impl<F: PrimeField> Display for CairoMemory<F> {
         for i in 1..self.size() {
             // Visualize content of memory excluding the 0th dummy entry
             if let Some(elem) = self[F::from(i)] {
-                if let Err(_) = writeln!(f, "{0:>6}: 0x{1:}", i, elem.get_word().to_hex_le()) {
+                if writeln!(f, "{0:>6}: 0x{1:}", i, elem.get_word().to_hex_le()).is_err() {
                     println!("Error while writing")
                 }
-            } else {
-                if let Err(_) = writeln!(f, "{0:>6}: None", i) {
-                    println!("Error while writing")
-                }
+            } else if writeln!(f, "{0:>6}: None", i).is_err() {
+                println!("Error while writing")
             }
         }
         Ok(())
