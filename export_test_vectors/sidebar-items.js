1a03ddb1048be77d5ce7cf03a2199e35b88db62e
mrmr1993 <mrmr1993@users.noreply.github.com>
2022-03-01 15:05:58 +0000

    deploy: 3516d433411d482b9b720c3ca0d3caec0359f0ac

--- export_test_vectors/sidebar-items.js
+++ export_test_vectors/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Mode",""]],"fn":[["main",""]],"mod":[["vectors",""]]});
+initSidebarItems({"fn":[["main","“Usage: cargo run –all-features –bin export_test_vectors – [hex|b10] [basic|15w] <OUTPUT_FILE>”,"]]});
--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["cairo"] = {"name":"","files":["flags.rs","helper.rs","lib.rs","memory.rs","runner.rs","word.rs"]};
 sourcesIndex["commitment_dlog"] = {"name":"","files":["chunked.rs","combine.rs","commitment.rs","lib.rs","srs.rs"]};
-sourcesIndex["export_test_vectors"] = {"name":"","files":["export_test_vectors.rs","vectors.rs"]};
+sourcesIndex["export_test_vectors"] = {"name":"","files":["export_test_vectors.rs"]};
 sourcesIndex["flamegraph"] = {"name":"","files":["flamegraph.rs"]};
 sourcesIndex["groupmap"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["kimchi"] = {"name":"","dirs":[{"name":"circuits","dirs":[{"name":"gates","files":["endosclmul.rs","mod.rs","poseidon.rs","varbasemul.rs"]},{"name":"polynomials","files":["chacha.rs","complete_add.rs","endomul_scalar.rs","endosclmul.rs","generic.rs","lookup.rs","mod.rs","permutation.rs","poseidon.rs","varbasemul.rs"]}],"files":["argument.rs","constraints.rs","domains.rs","expr.rs","gate.rs","macros.rs","mod.rs","polynomial.rs","scalars.rs","wires.rs"]}],"files":["alphas.rs","bench.rs","error.rs","index.rs","lib.rs","plonk_sponge.rs","prover.rs","verifier.rs"]};
