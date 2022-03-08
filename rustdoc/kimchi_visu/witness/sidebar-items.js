90d93709d1b93588771d031fc4d951696faee131
mimoo <mimoo@users.noreply.github.com>
2022-03-08 23:20:53 +0000

    deploy: 89fed685d9ce369e1da100725cde2802a1205f02

---
+++ rustdoc/kimchi_visu/witness/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Witness","The type that represents the execution trace. It represents a table of [REGISTERS] columns, with `n` rows. `n` being the maximum size of the circuit, and the size of the domain."]]});
