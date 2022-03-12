30ef84b3b65b17d48d6d3919b8dd6d7f16d8103d
David Wong <davidwong.crypto@gmail.com>
2022-03-12 01:10:20 -0800

    cargo fmt

--- kimchi/src/circuits/polynomials/poseidon.rs
+++ kimchi/src/circuits/polynomials/poseidon.rs
@@ -67,7 +67,7 @@ const ROUND_EQUATIONS: [RoundEquation; ROUNDS_PER_ROW] = [
 /// Implementation of the Poseidon gate
 /// Poseidon quotient poly contribution computation `f^7 + c(x) - f(wx)`
 /// Conjunction of:
-/// 
+///
 /// ```ignore
 /// curr[round_range(1)] = round(curr[round_range(0)])
 /// curr[round_range(2)] = round(curr[round_range(1)])
@@ -88,7 +88,7 @@ const ROUND_EQUATIONS: [RoundEquation; ROUNDS_PER_ROW] = [
 ///    + rcm[round_range(1)][1]
 /// ...
 /// ```
-/// 
+///
 /// The rth position in this array contains the alphas used for the equations that
 /// constrain the values of the (r+1)th state.
 #[derive(Default)]
