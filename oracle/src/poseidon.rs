d55ad73da3cd25cf8d78bb1b9348e910362a6895
mssabr01 <sabraoui.m@gmail.com>
2022-03-17 19:39:05 -0400

    removed unused import

--- oracle/src/poseidon.rs
+++ oracle/src/poseidon.rs
@@ -1,7 +1,7 @@
 //! This module implements Poseidon Hash Function primitive
 
 use crate::constants::SpongeConstants;
-use crate::permutation::{full_round, half_rounds, poseidon_block_cipher};
+use crate::permutation::{full_round, poseidon_block_cipher};
 use ark_ff::Field;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
