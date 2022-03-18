6af2997719d403fb4067e3373782c3b1886f22f1
David Wong <davidwong.crypto@gmail.com>
2022-03-18 16:31:27 -0700

    [kimchi][lookup] moving entry

--- kimchi/src/circuits/lookup/constraints.rs
+++ kimchi/src/circuits/lookup/constraints.rs
@@ -163,67 +163,6 @@ pub fn verify<F: FftField, I: Iterator<Item = F>, G: Fn() -> I>(
         }
     }
 }
-
-pub trait Entry {
-    type Field: Field;
-    type Params;
-
-    fn evaluate(
-        p: &Self::Params,
-        j: &JointLookupSpec<Self::Field>,
-        witness: &[Vec<Self::Field>; COLUMNS],
-        row: usize,
-    ) -> Self;
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct CombinedEntry<F>(pub F);
-impl<F: Field> Entry for CombinedEntry<F> {
-    type Field = F;
-    type Params = F;
-
-    fn evaluate(
-        joint_combiner: &F,
-        j: &JointLookupSpec<F>,
-        witness: &[Vec<F>; COLUMNS],
-        row: usize,
-    ) -> CombinedEntry<F> {
-        let eval = |pos: LocalPosition| -> F {
-            let row = match pos.row {
-                Curr => row,
-                Next => row + 1,
-            };
-            witness[pos.column][row]
-        };
-
-        CombinedEntry(j.evaluate(*joint_combiner, &eval))
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct UncombinedEntry<F>(pub Vec<F>);
-
-impl<F: Field> Entry for UncombinedEntry<F> {
-    type Field = F;
-    type Params = ();
-
-    fn evaluate(
-        _: &(),
-        j: &JointLookupSpec<F>,
-        witness: &[Vec<F>; COLUMNS],
-        row: usize,
-    ) -> UncombinedEntry<F> {
-        let eval = |pos: LocalPosition| -> F {
-            let row = match pos.row {
-                Curr => row,
-                Next => row + 1,
-            };
-            witness[pos.column][row]
-        };
-
-        UncombinedEntry(j.entry.iter().map(|s| s.evaluate(&eval)).collect())
-    }
-}
 //~
 //~ Because of our ZK-rows, we can't do the trick in the plookup paper of
 //~ wrapping around to enforce consistency between the sorted lookup columns.
--- kimchi/src/circuits/lookup/tables.rs
+++ kimchi/src/circuits/lookup/tables.rs
@@ -8,3 +8,64 @@ use std::collections::{HashMap, HashSet};
 use CurrOrNext::{Curr, Next};
 
 use super::lookups::{JointLookupSpec, LocalPosition};
+
+pub trait Entry {
+    type Field: Field;
+    type Params;
+
+    fn evaluate(
+        p: &Self::Params,
+        j: &JointLookupSpec<Self::Field>,
+        witness: &[Vec<Self::Field>; COLUMNS],
+        row: usize,
+    ) -> Self;
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct CombinedEntry<F>(pub F);
+impl<F: Field> Entry for CombinedEntry<F> {
+    type Field = F;
+    type Params = F;
+
+    fn evaluate(
+        joint_combiner: &F,
+        j: &JointLookupSpec<F>,
+        witness: &[Vec<F>; COLUMNS],
+        row: usize,
+    ) -> CombinedEntry<F> {
+        let eval = |pos: LocalPosition| -> F {
+            let row = match pos.row {
+                Curr => row,
+                Next => row + 1,
+            };
+            witness[pos.column][row]
+        };
+
+        CombinedEntry(j.evaluate(*joint_combiner, &eval))
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct UncombinedEntry<F>(pub Vec<F>);
+
+impl<F: Field> Entry for UncombinedEntry<F> {
+    type Field = F;
+    type Params = ();
+
+    fn evaluate(
+        _: &(),
+        j: &JointLookupSpec<F>,
+        witness: &[Vec<F>; COLUMNS],
+        row: usize,
+    ) -> UncombinedEntry<F> {
+        let eval = |pos: LocalPosition| -> F {
+            let row = match pos.row {
+                Curr => row,
+                Next => row + 1,
+            };
+            witness[pos.column][row]
+        };
+
+        UncombinedEntry(j.entry.iter().map(|s| s.evaluate(&eval)).collect())
+    }
+}
