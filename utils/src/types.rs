5a805088d78d3cea9cc6bfe3effdb9659e0d0d83
frisitano <francesco.risitano95@gmail.com>
2022-03-19 13:52:23 +0000

    add comments

--- utils/src/types.rs
+++ utils/src/types.rs
@@ -1,3 +1,5 @@
+//! This is used to define common types and associated prologues
+
 use ark_ec::AffineCurve;
 
 /// Alias to refer to the scalar field of a curve.
@@ -6,6 +8,7 @@ pub type ScalarField<G> = <G as AffineCurve>::ScalarField;
 /// Alias to refer to the base field of a curve.
 pub type BaseField<G> = <G as AffineCurve>::BaseField;
 
+/// Fields prologue
 pub mod fields {
     pub use super::{BaseField, ScalarField};
 }
