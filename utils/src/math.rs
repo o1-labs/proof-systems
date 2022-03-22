0a753435f1a99fe9185fb94096d0d73d7fe85510
Florian <trivoplays@gmail.com>
2022-03-22 09:46:17 +0100

    add assert to test

--- utils/src/math.rs
+++ utils/src/math.rs
@@ -35,6 +35,7 @@ mod tests {
         for (d, expected_res) in tests.iter() {
             let res = ceil_log2(*d);
             println!("ceil(log2({})) = {}, expected = {}", d, res, expected_res);
+            assert!(res == *expected_res)
         }
     }
 }
