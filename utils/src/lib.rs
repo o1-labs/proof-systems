09fe7477df1eaf26eb90a80047ffa4777b731b94
Xiang Xie <xiexiangiscas@gmail.com>
2022-03-27 13:17:39 +0800

    fmt

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,11 +1,12 @@
+pub mod chunked_polynomial;
 pub mod dense_polynomial;
 pub mod evaluations;
 pub mod field_helpers;
+
 pub mod hasher;
 pub mod math;
 pub mod serialization;
 pub mod types;
-pub mod chunked_polynomial;
 
 pub use dense_polynomial::ExtendedDensePolynomial;
 pub use evaluations::ExtendedEvaluations;
