d4a1d731606877eeb48c3f5da509ef10722ba8bb
hanabi1224 <harlowmoo@gmail.com>
2022-03-30 10:41:25 +0800

    once_cell

--- hasher/tests/bench_hasher_pooling.rs
+++ hasher/tests/bench_hasher_pooling.rs
@@ -2,64 +2,62 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lockfree_object_pool::SpinLockObjectPool;
 use mina_hasher::{Hasher, PoseidonHasherKimchi, PoseidonHasherLegacy};
 mod test_vector;
+use once_cell::sync::OnceCell;
 use test_vector::*;
 
-lazy_static::lazy_static! {
-    static ref LEGACY_HASHER_POOL: SpinLockObjectPool<PoseidonHasherLegacy<TestVector>> = SpinLockObjectPool::new(
-        || mina_hasher::create_legacy::<TestVector>(()),
-        |hasher| {
-            hasher.reset();
-        }
-    );
-    static ref KIMCHI_HASHER_POOL: SpinLockObjectPool<PoseidonHasherKimchi<TestVector>> = SpinLockObjectPool::new(
-        || mina_hasher::create_kimchi::<TestVector>(()),
-        |hasher| {
-            hasher.reset();
-        }
-    );
-}
+static LEGACY_HASHER_POOL: OnceCell<SpinLockObjectPool<PoseidonHasherLegacy<TestVector>>> =
+    OnceCell::new();
+static KIMCHI_HASHER_POOL: OnceCell<SpinLockObjectPool<PoseidonHasherKimchi<TestVector>>> =
+    OnceCell::new();
 
-fn legacy_no_pooling(c: &mut Criterion) {
+fn pooling(c: &mut Criterion) {
+    LEGACY_HASHER_POOL
+        .set(SpinLockObjectPool::new(
+            || mina_hasher::create_legacy::<TestVector>(()),
+            |hasher| {
+                hasher.reset();
+            },
+        ))
+        .map_err(|_| "Failed to set legacy hasher pool")
+        .unwrap();
+    KIMCHI_HASHER_POOL
+        .set(SpinLockObjectPool::new(
+            || mina_hasher::create_kimchi::<TestVector>(()),
+            |hasher| {
+                hasher.reset();
+            },
+        ))
+        .map_err(|_| "Failed to set kimchi hasher pool")
+        .unwrap();
+
     c.bench_function("legacy hasher without pooling", |b| {
         b.iter(|| {
             let mut hasher = mina_hasher::create_legacy::<TestVector>(());
             test_vectors("legacy.json", &mut hasher);
         })
     });
-}
 
-fn legacy_pooling(c: &mut Criterion) {
     c.bench_function("legacy hasher with pooling", |b| {
         b.iter(|| {
-            let mut hasher = LEGACY_HASHER_POOL.pull();
+            let mut hasher = LEGACY_HASHER_POOL.get().unwrap().pull();
             test_vectors("legacy.json", &mut *hasher);
         })
     });
-}
 
-fn kimchi_no_pooling(c: &mut Criterion) {
     c.bench_function("kimchi hasher without pooling", |b| {
         b.iter(|| {
             let mut hasher = mina_hasher::create_kimchi::<TestVector>(());
             test_vectors("kimchi.json", &mut hasher);
         })
     });
-}
 
-fn kimchi_pooling(c: &mut Criterion) {
     c.bench_function("kimchi hasher with pooling", |b| {
         b.iter(|| {
-            let mut hasher = KIMCHI_HASHER_POOL.pull();
+            let mut hasher = KIMCHI_HASHER_POOL.get().unwrap().pull();
             test_vectors("kimchi.json", &mut *hasher);
         })
     });
 }
 
-criterion_group!(
-    benches,
-    legacy_no_pooling,
-    legacy_pooling,
-    kimchi_no_pooling,
-    kimchi_pooling
-);
+criterion_group!(benches, pooling,);
 criterion_main!(benches);
