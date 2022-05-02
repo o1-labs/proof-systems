00a7eada2bdecaab894b63248c7ed74dd9805bc3
mimoo <mimoo@users.noreply.github.com>
2022-05-02 20:14:30 +0000

    deploy: 92bf8597bd351214a5cca14aff6f0036b38dd57a

--- rustdoc/kimchi/error/sidebar-items.js
+++ rustdoc/kimchi/error/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ProofError","Errors that can arise when creating a proof"],["VerifyError","Errors that can arise when verifying a proof"]]});
+initSidebarItems({"enum":[["ProverError","Errors that can arise when creating a proof"],["SetupError","Errors that can arise when preparing the setup"],["VerifyError","Errors that can arise when verifying a proof"]]});
