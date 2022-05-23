1b6018c3e028516908114a27cd7012e7d9b3b025
Joseph Spadavecchia <joseph@redtrie.com>
2022-05-23 18:27:09 +0100

    Fix to documentation

--- kimchi/src/circuits/lookup/tables/range_check.rs
+++ kimchi/src/circuits/lookup/tables/range_check.rs
@@ -4,7 +4,7 @@ use ark_ff::Field;
 /// The range check will be performed on values in `[0, 2^12]`.
 const RANGE_CHECK_UPPERBOUND: u32 = 1 << 12;
 
-/// A single-column table containing the numbers from 0 to [RANGE_UPPERBOUND] (included).
+/// A single-column table containing the numbers from 0 to [RANGE_CHECK_UPPERBOUND] (included).
 pub fn range_check_table<F>() -> LookupTable<F>
 where
     F: Field,
