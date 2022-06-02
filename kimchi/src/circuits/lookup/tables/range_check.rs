75a5478a0dc3203a0650823312784938f9053951
Joseph Spadavecchia <joseph@redtrie.com>
2022-06-02 18:33:40 +0100

    Fix typo

--- kimchi/src/circuits/lookup/tables/range_check.rs
+++ kimchi/src/circuits/lookup/tables/range_check.rs
@@ -3,7 +3,7 @@
 use crate::circuits::lookup::tables::{LookupTable, RANGE_CHECK_TABLE_ID};
 use ark_ff::Field;
 
-/// The range check will be performed on12-bit values, i.e. those in `[0, 2^12]`
+/// The range check will be performed on 12-bit values, i.e. those in `[0, 2^12]`
 pub const RANGE_CHECK_UPPERBOUND: u32 = 1 << 12;
 
 /// A single-column table containing the numbers from 0 to [RANGE_CHECK_UPPERBOUND] (inclusive)
