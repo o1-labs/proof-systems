05836f7df6d0673ebf2db18863d4dc9fc5114f7c
David Wong <davidwong.crypto@gmail.com>
2022-06-20 10:23:49 -0700

    [bench] better bench

--- kimchi/benches/proof_criterion.rs
+++ kimchi/benches/proof_criterion.rs
@@ -5,21 +5,26 @@ pub fn bench_proof_creation(c: &mut Criterion) {
     let mut group = c.benchmark_group("Proof creation");
     group.sample_size(10).sampling_mode(SamplingMode::Flat); // for slow benchmarks
 
+    let ctx = BenchmarkCtx::new(1 << 10);
+    group.bench_function(
+        format!("proof creation (SRS size 2^{}", ctx.srs_size()),
+        |b| b.iter(|| black_box(ctx.create_proof())),
+    );
+
     let ctx = BenchmarkCtx::new(1 << 14);
-    group.bench_function("proof creation (2^15)", |b| {
-        b.iter(|| black_box(ctx.create_proof()))
-    });
-}
+    group.bench_function(
+        format!("proof creation (SRS size 2^{}", ctx.srs_size()),
+        |b| b.iter(|| black_box(ctx.create_proof())),
+    );
 
-pub fn bench_proof_verification(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Proof verification");
-    group.sample_size(10).sampling_mode(SamplingMode::Flat); // for slow benchmarks
-
-    let ctx = BenchmarkCtx::new(1 << 4); // since verification time is unrelated to the circuit size, we just reduce the time the proof takes
     let proof = ctx.create_proof();
-    group.bench_function("proof verification", |b| {
-        b.iter(|| ctx.batch_verification(black_box(vec![proof.clone()])))
-    });
+
+    group.sample_size(100).sampling_mode(SamplingMode::Auto);
+    group.bench_function(
+        format!("proof verification (SRS size 2^{}", ctx.srs_size()),
+        |b| b.iter(|| ctx.batch_verification(black_box(vec![proof.clone()]))),
+    );
 }
-criterion_group!(benches, bench_proof_creation, bench_proof_verification);
+
+criterion_group!(benches, bench_proof_creation);
 criterion_main!(benches);
