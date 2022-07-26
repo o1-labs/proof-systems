eae601aff40379e8f9337ebb2914b72dd9e7c583
querolita <anais.querol@gmail.com>
2022-07-26 16:28:51 +0200

    change mod name

--- kimchi/src/circuits/polynomials/range_check/mod.rs
+++ kimchi/src/circuits/polynomials/range_check/mod.rs
@@ -2,9 +2,9 @@
 
 mod circuitgates;
 
-pub mod gate;
+pub mod gadget;
 pub mod witness;
 
 pub use circuitgates::{RangeCheck0, RangeCheck1};
-pub use gate::*;
+pub use gadget::*;
 pub use witness::*;
