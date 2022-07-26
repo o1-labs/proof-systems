3d16cd30e6acad3d6e1b22ea6da51710ff5e55b9
querolita <querolita@users.noreply.github.com>
2022-07-26 16:45:58 +0000

    deploy: a06b5ef821d1c41a4a8bde2a4e8652fcfe94e24b

--- rustdoc/kimchi/circuits/polynomials/range_check/sidebar-items.js
+++ rustdoc/kimchi/circuits/polynomials/range_check/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["gate","Range check gate"],["witness","Range check witness computation"]],"struct":[["RangeCheck0","RangeCheck0 - Range check constraints"],["RangeCheck1","RangeCheck1 - Range check constraints"]]};
+window.SIDEBAR_ITEMS = {"mod":[["gadget","Range check gate"],["witness","Range check witness computation"]],"struct":[["RangeCheck0","RangeCheck0 - Range check constraints"],["RangeCheck1","RangeCheck1 - Range check constraints"]]};
