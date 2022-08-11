58c9e972d413cce514fc4c9693b0cc2631fdd8af
David Wong <davidwong.crypto@gmail.com>
2022-08-11 22:38:26 +0300

    [generic] add an all-zero public input test

--- kimchi/src/tests/generic.rs
+++ kimchi/src/tests/generic.rs
@@ -38,3 +38,21 @@ fn test_generic_gate_pub() {
         .setup()
         .prove_and_verify();
 }
+
+#[test]
+fn test_generic_gate_pub_all_zeros() {
+    let public = vec![Fp::from(0u8); 5];
+    let gates = create_circuit(0, public.len());
+
+    // create witness
+    let mut witness: [Vec<Fp>; COLUMNS] = array_init(|_| vec![Fp::zero(); gates.len()]);
+    fill_in_witness(0, &mut witness, &public);
+
+    // create and verify proof based on the witness
+    TestFramework::default()
+        .gates(gates)
+        .witness(witness)
+        .public_inputs(public)
+        .setup()
+        .prove_and_verify();
+}
