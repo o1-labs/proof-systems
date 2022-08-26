39d8712e61856eb825434f228c817df1602b96fe
mrmr1993 <mrmr1993@users.noreply.github.com>
2022-08-26 19:54:38 +0000

    deploy: fef5cfe909f6f297f6d8e9b3b4a128f23c9b24f3

--- rustdoc/kimchi/circuits/expr/constraints/sidebar-items.js
+++ rustdoc/kimchi/circuits/expr/constraints/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["boolean","Creates a constraint to enforce that b is either 0 or 1."],["crumb","Crumb constraint for 2-bit value x"]]};
+window.SIDEBAR_ITEMS = {"fn":[["boolean","Creates a constraint to enforce that b is either 0 or 1."],["crumb","Crumb constraint for 2-bit value x"]],"trait":[["ArithmeticOps","This trait defines a common arithmetic operations interface that can be used by constraints.  It allows us to reuse constraint code for witness computation."]]};
