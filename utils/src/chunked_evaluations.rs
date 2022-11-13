cafaec9e91164ad37d5d843cbe9b24daad3ca202
guorong <dudurong009@gmail.com>
2022-11-13 10:12:17 +0800

    Update the generic type bound in "ChunkedEvaluations" type

--- utils/src/chunked_evaluations.rs
+++ utils/src/chunked_evaluations.rs
@@ -1,13 +1,16 @@
 //! This module contains a type [ChunkedEvaluations],
 
+use ark_ff::PrimeField;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
 /// This struct contains multiple chunk evaluations.
 #[serde_as]
 #[derive(Clone, Serialize, Deserialize)]
-#[serde(bound = "F: ark_serialize::CanonicalDeserialize + ark_serialize::CanonicalSerialize")]
-pub struct ChunkedEvaluations<F> {
+pub struct ChunkedEvaluations<F>
+where
+    F: PrimeField,
+{
     /// The chunk evaluations.
     #[serde_as(as = "Vec<crate::serialization::SerdeAs>")]
     pub chunks: Vec<F>,
