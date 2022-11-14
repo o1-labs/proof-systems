15ca1b1b0cbfe7d124c2650996eee7a37b10d44c
mimoo <mimoo@users.noreply.github.com>
2022-11-14 18:20:32 +0000

    deploy: 21feac166121ba5c83e0eaa9490aaaaa78c3fce6

---
+++ rustdoc/o1_utils/chunked_evaluations/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["ChunkedEvaluations","This struct contains multiple chunk evaluations."]]};
--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -10,7 +10,7 @@ var sourcesIndex = JSON.parse('{\
 "mina_hasher":["",[],["lib.rs","poseidon.rs","roinput.rs"]],\
 "mina_poseidon":["",[["pasta",[],["fp_kimchi.rs","fp_legacy.rs","fq_kimchi.rs","fq_legacy.rs","mod.rs"]]],["constants.rs","lib.rs","permutation.rs","poseidon.rs","sponge.rs"]],\
 "mina_signer":["",[],["keypair.rs","lib.rs","pubkey.rs","schnorr.rs","seckey.rs","signature.rs"]],\
-"o1_utils":["",[],["adjacent_pairs.rs","chunked_polynomial.rs","dense_polynomial.rs","evaluations.rs","field_helpers.rs","foreign_field.rs","hasher.rs","lib.rs","math.rs","serialization.rs"]],\
+"o1_utils":["",[],["adjacent_pairs.rs","chunked_evaluations.rs","chunked_polynomial.rs","dense_polynomial.rs","evaluations.rs","field_helpers.rs","foreign_field.rs","hasher.rs","lib.rs","math.rs","serialization.rs"]],\
 "turshi":["",[],["flags.rs","helper.rs","lib.rs","memory.rs","runner.rs","word.rs"]]\
 }');
 createSourceSidebar();
