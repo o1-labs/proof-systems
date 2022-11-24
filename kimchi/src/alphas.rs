8b6bf0cc4e921abb7e3e2fed831cf4909ebe54f4
mrmr1993 <mr_1993@hotmail.co.uk>
2022-11-24 11:27:55 +0000

    cargo fmt

--- kimchi/src/alphas.rs
+++ kimchi/src/alphas.rs
@@ -323,10 +323,7 @@ mod tests {
     fn get_alphas_for_spec() {
         let gates = vec![CircuitGate::<Fp>::zero(Wire::for_row(0)); 2];
         let index = new_index_for_test(gates, 0);
-        let (_linearization, powers_of_alpha) = expr_linearization(
-            &index.cs.feature_flags,
-            true,
-        );
+        let (_linearization, powers_of_alpha) = expr_linearization(&index.cs.feature_flags, true);
         // make sure this is present in the specification
         let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         let spec_path = Path::new(&manifest_dir)
