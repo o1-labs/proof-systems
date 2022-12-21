efced72c00229853e012b323f6d92eebb6d45e4f
jspada <jspada@users.noreply.github.com>
2022-12-21 17:37:53 +0000

    deploy: 226de4aeb11b8814327ab832e4fccdce5585f473

--- rustdoc/kimchi/circuits/polynomials/foreign_field_mul/witness/sidebar-items.js
+++ rustdoc/kimchi/circuits/polynomials/foreign_field_mul/witness/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["compute_bound",""],["create","Create a foreign field multiplication witness Input: multiplicands left_input and right_input"]],"struct":[["ExternalChecks","Track external check witness data"]]};
+window.SIDEBAR_ITEMS = {"fn":[["compute_bound","Perform integer bound addition computation x’ = x + f’"],["create","Create a foreign field multiplication witness Input: multiplicands left_input and right_input"]],"struct":[["ExternalChecks","Track external check witness data"]]};
