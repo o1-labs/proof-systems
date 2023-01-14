090b3a0548fb7e62de0975fc4554e160d7ec9d5a
Joseph Spadavecchia <joseph@redtrie.com>
2023-01-14 17:59:33 +0000

    Remove debug

--- kimchi/src/tests/foreign_field_mul.rs
+++ kimchi/src/tests/foreign_field_mul.rs
@@ -191,7 +191,6 @@ where
         }
     }
 
-    println!("witness[7][0] = {}", witness[7][0].to_biguint());
     if let Some(runner) = runner {
         // Perform full test that everything is ok before invalidation
         assert_eq!(runner.prove_and_verify::<EFqSponge, EFrSponge>(), Ok(()));
@@ -682,7 +681,7 @@ fn test_max_native_multiplicands() {
 fn test_max_foreign_multiplicands() {
     let (result, witness) = run_test::<Vesta, VestaBaseSponge, VestaScalarSponge>(
         true,
-        false,
+        true,
         false,
         &secp256k1_max(),
         &secp256k1_max(),
