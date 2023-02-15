d4328999a7e0405891c2e7ec3432bbb4f724715b
Joseph Spadavecchia <joseph@redtrie.com>
2023-02-15 10:11:25 +0000

    Auto-generate specifications
        * Auto-generate kimchi specification on cargo build or cargo test
        * Auto-install book build-dependencies

---
+++ kimchi/build.rs
@@ -0,0 +1,10 @@
+// build.rs
+
+use std::process::Command;
+
+fn main() {
+    Command::new("make")
+        .args(&["-C", "../book/specifications/kimchi"])
+        .status()
+        .expect("failed to make!");
+}
