764d1f26819be0c95f7ba1374cfd13de25c35154
Joseph Spadavecchia <joseph@redtrie.com>
2023-02-16 20:06:19 +0100

    Disable auto-spec for now until we switch to use cargo-spec as library

--- kimchi/build.rs
+++ kimchi/build.rs
@@ -1,15 +1,16 @@
 // Build script
 
-use std::process::Command;
+// use std::process::Command;
 
 fn main() {
-    // Rebuild specification
-    assert!(
-        Command::new("make")
-            .args(["-C", "../book/specifications/kimchi", "build"])
-            .status()
-            .expect("failed to get status")
-            .success(),
-        "failed to generate specification markdown"
-    );
+    // TODO: Convert to use cargo-spec library
+    // // Rebuild specification
+    // assert!(
+    //     Command::new("make")
+    //         .args(["-C", "../book/specifications/kimchi", "build"])
+    //         .status()
+    //         .expect("failed to get status")
+    //         .success(),
+    //     "failed to generate specification markdown"
+    // );
 }
