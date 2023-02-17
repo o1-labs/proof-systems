8592e06fa9433965f80c4313c1b4e0b20dd4ff76
Joseph Spadavecchia <joseph@redtrie.com>
2023-02-17 16:32:19 +0100

    Finish up spec build script

--- kimchi/build.rs
+++ kimchi/build.rs
@@ -1,13 +1,25 @@
 // Build script
 
 use cargo_spec::build::{self, OutputFormat};
+use std::{env, path::PathBuf};
 
 fn main() {
     // Rebuild specification
-    build::build(
+    let manifest_path = env::var("CARGO_MANIFEST_DIR").expect("failed to get manifest path");
+    let spec_path: PathBuf = [
+        manifest_path.clone(),
         "../book/specifications/kimchi/Specification.toml".into(),
-        Some("../book/src/specs/kimchi.md".into()),
-        OutputFormat::Markdown,
-    )
-    .expect("failed to generate specification");
+    ]
+    .iter()
+    .collect();
+    let output_path: PathBuf = [manifest_path, "../book/src/specs/kimchi.md".into()]
+        .iter()
+        .collect();
+
+    let files_to_watch = build::build(spec_path, Some(output_path), OutputFormat::Markdown)
+        .expect("failed to generate specification");
+
+    for file in files_to_watch {
+        println!("cargo:rerun-if-changed={}", file.display());
+    }
 }
