287f9668eab2361d9642f442735e3c58c9113738
David Wong <davidwong.crypto@gmail.com>
2023-02-24 11:39:24 +0100

    [curves] expose projective types

--- curves/src/pasta/curves/mod.rs
+++ curves/src/pasta/curves/mod.rs
@@ -1,8 +1,8 @@
 pub mod pallas;
 pub mod vesta;
 
-pub use pallas::{Affine as Pallas, Config as PallasConfig, LegacyConfig as LegacyPallasConfig};
-pub use vesta::{Affine as Vesta, Config as VestaConfig, LegacyConfig as LegacyVestaConfig};
+pub use pallas::{Affine as Pallas, Config as PallasConfig, LegacyConfig as LegacyPallasConfig, Projective as ProjectivePallas};
+pub use vesta::{Affine as Vesta, Config as VestaConfig, LegacyConfig as LegacyVestaConfig, Projective as ProjectiveVesta};
 
 #[cfg(test)]
 mod tests;
