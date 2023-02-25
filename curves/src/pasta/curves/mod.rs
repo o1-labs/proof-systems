daafe745b56a6a198ee267a06bef5f6183227797
David Wong <davidwong.crypto@gmail.com>
2023-02-25 20:49:31 +0100

    cargo fmt

--- curves/src/pasta/curves/mod.rs
+++ curves/src/pasta/curves/mod.rs
@@ -1,8 +1,14 @@
 pub mod pallas;
 pub mod vesta;
 
-pub use pallas::{Affine as Pallas, Config as PallasConfig, LegacyConfig as LegacyPallasConfig, Projective as ProjectivePallas};
-pub use vesta::{Affine as Vesta, Config as VestaConfig, LegacyConfig as LegacyVestaConfig, Projective as ProjectiveVesta};
+pub use pallas::{
+    Affine as Pallas, Config as PallasConfig, LegacyConfig as LegacyPallasConfig,
+    Projective as ProjectivePallas,
+};
+pub use vesta::{
+    Affine as Vesta, Config as VestaConfig, LegacyConfig as LegacyVestaConfig,
+    Projective as ProjectiveVesta,
+};
 
 #[cfg(test)]
 mod tests;
