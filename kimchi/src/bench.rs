56180ab33b2ecbe09012b3d350e99319b068dcc8
David Wong <davidwong.crypto@gmail.com>
2023-03-15 14:20:13 +0100

    [bench] fix bench

--- kimchi/src/bench.rs
+++ kimchi/src/bench.rs
@@ -120,17 +120,19 @@ mod tests {
     fn test_bench() {
         // context created in 21.2235 ms
         let start = Instant::now();
-        let ctx = BenchmarkCtx::new(1 << 4);
-        println!("context created in {}", start.elapsed().as_secs());
+        let srs_size = 4;
+        let ctx = BenchmarkCtx::new(srs_size);
+        println!("testing bench code for SRS of size {srs_size}");
+        println!("context created in {}s", start.elapsed().as_secs());
 
         // proof created in 7.1227 ms
         let start = Instant::now();
         let (proof, public_input) = ctx.create_proof();
-        println!("proof created in {}", start.elapsed().as_millis());
+        println!("proof created in {}s", start.elapsed().as_secs());
 
         // proof verified in 1.710 ms
         let start = Instant::now();
         ctx.batch_verification(&vec![(proof, public_input)]);
-        println!("proof verified in {}", start.elapsed().as_millis());
+        println!("proof verified in {}", start.elapsed().as_secs());
     }
 }
