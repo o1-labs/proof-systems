5f91affca8c65438ad7931b0d4d104867f3b19a6
Chiro Hiro <chiro8x@gmail.com>
2023-10-04 14:39:04 +0700

    Update arkworks to 0.4.2 for pallas and vesta

--- curves/src/pasta/curves/pallas.rs
+++ curves/src/pasta/curves/pallas.rs
@@ -1,70 +1,75 @@
 use crate::pasta::*;
 use ark_ec::{
-    models::short_weierstrass_jacobian::{GroupAffine, GroupProjective},
-    ModelParameters, SWModelParameters,
+    models::short_weierstrass::{Affine, Projective, SWCurveConfig},
+    CurveConfig,
 };
-use ark_ff::{field_new, Zero};
+use ark_ff::{MontFp, Zero};
+
+/// G_GENERATOR_X =
+/// 1
+pub const G_GENERATOR_X: Fp = MontFp!("1");
+
+/// G1_GENERATOR_Y =
+/// 12418654782883325593414442427049395787963493412651469444558597405572177144507
+pub const G_GENERATOR_Y: Fp =
+    MontFp!("12418654782883325593414442427049395787963493412651469444558597405572177144507");
 
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub struct PallasParameters;
 
-impl ModelParameters for PallasParameters {
+impl CurveConfig for PallasParameters {
     type BaseField = Fp;
+
     type ScalarField = Fq;
-}
-
-pub type Pallas = GroupAffine<PallasParameters>;
-pub type ProjectivePallas = GroupProjective<PallasParameters>;
-
-impl SWModelParameters for PallasParameters {
-    /// COEFF_A = 0
-    const COEFF_A: Fp = field_new!(Fp, "0");
-
-    /// COEFF_B = 5
-    const COEFF_B: Fp = field_new!(Fp, "5");
 
     /// COFACTOR = 1
     const COFACTOR: &'static [u64] = &[0x1];
 
     /// COFACTOR_INV = 1
-    const COFACTOR_INV: Fq = field_new!(Fq, "1");
-
-    /// AFFINE_GENERATOR_COEFFS = (G1_GENERATOR_X, G1_GENERATOR_Y)
-    const AFFINE_GENERATOR_COEFFS: (Self::BaseField, Self::BaseField) =
-        (G_GENERATOR_X, G_GENERATOR_Y);
-
-    #[inline(always)]
-    fn mul_by_a(_: &Self::BaseField) -> Self::BaseField {
-        Self::BaseField::zero()
-    }
+    const COFACTOR_INV: Fq = MontFp!("1");
 }
 
-/// G_GENERATOR_X =
-/// 1
-pub const G_GENERATOR_X: Fp = field_new!(Fp, "1");
+pub type Pallas = Affine<PallasParameters>;
 
-/// G1_GENERATOR_Y =
-/// 12418654782883325593414442427049395787963493412651469444558597405572177144507
-pub const G_GENERATOR_Y: Fp = field_new!(
-    Fp,
-    "12418654782883325593414442427049395787963493412651469444558597405572177144507"
-);
+pub type ProjectivePallas = Projective<PallasParameters>;
+
+impl SWCurveConfig for PallasParameters {
+    const COEFF_A: Self::BaseField = MontFp!("0");
+
+    const COEFF_B: Self::BaseField = MontFp!("5");
+
+    const GENERATOR: Affine<Self> = Affine::new_unchecked(G_GENERATOR_X, G_GENERATOR_Y);
+}
+
+impl PallasParameters {
+    #[inline(always)]
+    pub fn mul_by_a(
+        _: &<PallasParameters as CurveConfig>::BaseField,
+    ) -> <PallasParameters as CurveConfig>::BaseField {
+        <PallasParameters as CurveConfig>::BaseField::zero()
+    }
+}
 
 /// legacy curve, a copy of the normal curve to support legacy sponge params
 #[derive(Copy, Clone, Default, PartialEq, Eq)]
 pub struct LegacyPallasParameters;
 
-impl ModelParameters for LegacyPallasParameters {
-    type BaseField = <PallasParameters as ModelParameters>::BaseField;
-    type ScalarField = <PallasParameters as ModelParameters>::ScalarField;
-}
-impl SWModelParameters for LegacyPallasParameters {
-    const COEFF_A: Self::BaseField = <PallasParameters as SWModelParameters>::COEFF_A;
-    const COEFF_B: Self::BaseField = <PallasParameters as SWModelParameters>::COEFF_B;
-    const COFACTOR: &'static [u64] = <PallasParameters as SWModelParameters>::COFACTOR;
-    const COFACTOR_INV: Self::ScalarField = <PallasParameters as SWModelParameters>::COFACTOR_INV;
-    const AFFINE_GENERATOR_COEFFS: (Self::BaseField, Self::BaseField) =
-        <PallasParameters as SWModelParameters>::AFFINE_GENERATOR_COEFFS;
+impl CurveConfig for LegacyPallasParameters {
+    type BaseField = <PallasParameters as CurveConfig>::BaseField;
+
+    type ScalarField = <PallasParameters as CurveConfig>::ScalarField;
+
+    const COFACTOR: &'static [u64] = <PallasParameters as CurveConfig>::COFACTOR;
+
+    const COFACTOR_INV: Self::ScalarField = <PallasParameters as CurveConfig>::COFACTOR_INV;
 }
 
-pub type LegacyPallas = GroupAffine<LegacyPallasParameters>;
+impl SWCurveConfig for LegacyPallasParameters {
+    const COEFF_A: Self::BaseField = <PallasParameters as SWCurveConfig>::COEFF_A;
+
+    const COEFF_B: Self::BaseField = <PallasParameters as SWCurveConfig>::COEFF_B;
+
+    const GENERATOR: Affine<Self> = Affine::new_unchecked(G_GENERATOR_X, G_GENERATOR_Y);
+}
+
+pub type LegacyPallas = Affine<LegacyPallasParameters>;
--- curves/src/pasta/fields/fp.rs
+++ curves/src/pasta/fields/fp.rs
@@ -1,6 +1,12 @@
-use ark_ff::{biginteger::BigInteger256 as BigInteger, FftParameters, Fp256, Fp256Parameters};
+use super::fft::{FftParameters, Fp256Parameters};
+use ark_ff::fields::{MontBackend, MontConfig};
+use ark_ff::{biginteger::BigInteger256 as BigInteger, Fp256};
 
-pub type Fp = Fp256<FpParameters>;
+#[derive(MontConfig)]
+#[modulus = "28948022309329048855892746252171976963363056481941560715954676764349967630337"]
+#[generator = "5"]
+pub struct FqConfig;
+pub type Fp = Fp256<MontBackend<FqConfig, 4>>;
 
 pub struct FpParameters;
 
@@ -12,35 +18,35 @@ impl FftParameters for FpParameters {
     const TWO_ADICITY: u32 = 32;
 
     #[rustfmt::skip]
-    const TWO_ADIC_ROOT_OF_UNITY: BigInteger = BigInteger([
+    const TWO_ADIC_ROOT_OF_UNITY: BigInteger = BigInteger::new([
         0xa28db849bad6dbf0, 0x9083cd03d3b539df, 0xfba6b9ca9dc8448e, 0x3ec928747b89c6da
     ]);
 }
 
-impl ark_ff::FpParameters for FpParameters {
+impl super::fft::FpParameters for FpParameters {
     // 28948022309329048855892746252171976963363056481941560715954676764349967630337
-    const MODULUS: BigInteger = BigInteger([
+    const MODULUS: BigInteger = BigInteger::new([
         0x992d30ed00000001,
         0x224698fc094cf91b,
         0x0,
         0x4000000000000000,
     ]);
 
-    const R: BigInteger = BigInteger([
+    const R: BigInteger = BigInteger::new([
         0x34786d38fffffffd,
         0x992c350be41914ad,
         0xffffffffffffffff,
         0x3fffffffffffffff,
     ]);
 
-    const R2: BigInteger = BigInteger([
+    const R2: BigInteger = BigInteger::new([
         0x8c78ecb30000000f,
         0xd7d30dbd8b0de0e7,
         0x7797a99bc3c95d18,
         0x96d41af7b9cb714,
     ]);
 
-    const MODULUS_MINUS_ONE_DIV_TWO: BigInteger = BigInteger([
+    const MODULUS_MINUS_ONE_DIV_TWO: BigInteger = BigInteger::new([
         0xcc96987680000000,
         0x11234c7e04a67c8d,
         0x0,
@@ -48,13 +54,13 @@ impl ark_ff::FpParameters for FpParameters {
     ]);
 
     // T and T_MINUS_ONE_DIV_TWO, where MODULUS - 1 = 2^S * T
-    const T: BigInteger = BigInteger([0x94cf91b992d30ed, 0x224698fc, 0x0, 0x40000000]);
+    const T: BigInteger = BigInteger::new([0x94cf91b992d30ed, 0x224698fc, 0x0, 0x40000000]);
 
     const T_MINUS_ONE_DIV_TWO: BigInteger =
-        BigInteger([0x4a67c8dcc969876, 0x11234c7e, 0x0, 0x20000000]);
+        BigInteger::new([0x4a67c8dcc969876, 0x11234c7e, 0x0, 0x20000000]);
 
     // GENERATOR = 5
-    const GENERATOR: BigInteger = BigInteger([
+    const GENERATOR: BigInteger = BigInteger::new([
         0xa1a55e68ffffffed,
         0x74c2a54b4f4982f3,
         0xfffffffffffffffd,
