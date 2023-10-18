76ad2a9a6c2a7b7798f7b57793f88d091893b428
Joseph Spadavecchia <joseph@redtrie.com>
2023-10-18 06:19:58 +0100

    Update formatting

--- kimchi/src/alphas.rs
+++ kimchi/src/alphas.rs
@@ -323,8 +323,11 @@ mod tests {
     fn get_alphas_for_spec() {
         let gates = vec![CircuitGate::<Fp>::zero(Wire::for_row(0)); 2];
         let index = new_index_for_test::<Vesta>(gates, 0);
-        let (_linearization, powers_of_alpha) =
-            expr_linearization::<Fp>(index.cs.custom_gate_type, Some(&index.cs.feature_flags), true);
+        let (_linearization, powers_of_alpha) = expr_linearization::<Fp>(
+            index.cs.custom_gate_type,
+            Some(&index.cs.feature_flags),
+            true,
+        );
         // make sure this is present in the specification
         let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         let spec_path = Path::new(&manifest_dir)
