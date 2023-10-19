de45087f68dcbd98e64bdd5321d9f9a74f30795e
gabrielbosio <gabrielbosio95@gmail.com>
2023-10-19 20:28:18 +0200

    Add BN254 Fq type

--- curves/src/bn254.rs
+++ curves/src/bn254.rs
@@ -1,6 +1,7 @@
 use ark_ec::models::short_weierstrass_jacobian::{GroupAffine, GroupProjective};
 
 pub type Fp = ark_bn254::Fr;
+pub type Fq = ark_bn254::Fq;
 pub type BN254Parameters = ark_bn254::g1::Parameters;
 pub type BN254 = GroupAffine<BN254Parameters>;
 pub type ProjectiveBN254 = GroupProjective<BN254Parameters>;
