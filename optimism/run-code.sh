85841b02ac2b08b17bb66a86ef5293148ea3fadf
mrmr1993 <mr_1993@hotmail.co.uk>
2023-10-23 18:27:30 +0100

    Allow an explicit filename to override

--- optimism/run-code.sh
+++ optimism/run-code.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 source rpcs.sh
 
-FILENAME="$(./generate-config.sh)"
+set +u
+if [ -z "${FILENAME}" ]; then
+    FILENAME="$(./generate-config.sh)"
+fi
+set -u
 
 source $FILENAME
 
