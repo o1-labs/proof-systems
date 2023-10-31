100e5507f3cda70e9b47248464d2cbf0296053cf
querolita <anais.querol@gmail.com>
2023-10-31 16:42:03 +0100

    delete comment about panicking inside lookups

--- kimchi/src/circuits/lookup/tables/bits16.rs
+++ kimchi/src/circuits/lookup/tables/bits16.rs
@@ -6,10 +6,6 @@ use super::BITS16_TABLE_ID;
 //~ The lookup table for 16-bits
 
 /// Returns the lookup table for all 16-bit values
-///
-/// # Panics
-///
-/// Will panic if `data` is invalid.
 pub fn bits16_table<F: Field>() -> LookupTable<F> {
     let mut data = vec![vec![]; 1];
 
