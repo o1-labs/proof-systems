83476ce1c79b841e3d8a32c68ff8b3e91b17f9eb
Richard Bonichon <richard.bonichon@gmail.com>
2023-11-01 15:27:08 +0100

    logging: using env_logger for simple logging

--- optimism/src/main.rs
+++ optimism/src/main.rs
@@ -132,6 +132,9 @@ pub fn main() -> ExitCode {
     // Initialize some data used for statistical computations
     let start = Start::create(state.step as usize);
 
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .init();
+
     let mut env = witness::Env::<ark_bn254::Fq>::create(cannon::PAGE_SIZE, state);
 
     while !env.halt {
