e0bbfbcb796490013270af1d3a4cac81168a87d0
Mikhail Volkhov <volhovm.cs@gmail.com>
2023-11-23 16:32:04 +0000

    Fill out fields of StepWitness

--- vinegar/src/lib.rs
+++ vinegar/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 use ark_ff::PrimeField;
 use kimchi::{
     circuits::{
@@ -10,9 +11,10 @@ use kimchi::{
         },
     },
     curve::KimchiCurve,
+    proof::{PointEvaluations, ProofEvaluations, ProverCommitments},
     prover_index::ProverIndex,
 };
-use poly_commitment::{commitment::CommitmentCurve, evaluation_proof::OpeningProof};
+use poly_commitment::evaluation_proof::{OpeningProof, PreChallenges};
 
 // Let F, K be the two fields (either (Fp, Fq) or (Fq, Fp)).
 // Each proof over F has an accumulator state which contains
@@ -48,7 +50,24 @@ use poly_commitment::{commitment::CommitmentCurve, evaluation_proof::OpeningProo
 // - a challenge_polynomial_commitment (coordinates in K) corresponding to P's own inner-product argument
 // - a set of IPA challenges (thought of as F-elements) corresponding to P's own inner-product argument
 
+type G1 = mina_curves::pasta::Pallas;
+type G2 = mina_curves::pasta::Vesta;
+type Fp = mina_curves::pasta::Fp;
+type Fq = mina_curves::pasta::Fq;
+
+pub const MAX_PROOFS_VERIFIED: usize = 2;
+pub const TICK_ROUNDS_N: usize = 16;
+
 pub struct ScalarChallenge {}
+pub struct ChallengePolynomialCommitments {}
+pub struct BranchData {}
+pub struct Challenge {}
+pub struct AppState {} // should be two?
+pub struct Features {}
+pub struct Digest {}
+pub struct UnfinalizedProofs {}
+pub struct PlonkVerificationKeyEvals {}
+
 //
 // prechals = OpeningProof::prechallenges(proof, &mut sponge)
 // mapped to a right domain:
@@ -56,17 +75,16 @@ pub struct ScalarChallenge {}
 //          Vector.of_list_and_length_exn
 //            (Array.map prechals ~f:Bulletproof_challenge.unpack |> Array.to_list)
 //            Tock.Rounds.n
-pub struct BulletproofChallenges {}
-pub struct ChallengePolynomialCommitments {}
-pub struct BranchData {}
-pub struct Challenge {}
-pub struct Fp {}
-pub struct Fq {}
-pub struct G1 {}
-pub struct Features {}
-pub struct Digest {}
-pub struct UnfinalizedProofs {}
-pub struct PlonkVerificationKeyEvals {}
+pub struct BulletproofChallenges<F>(pub PreChallenges<F>);
+
+/// This is data that can be computed in linear time from the proof + statement.
+///
+/// It doesn't need to be sent on the wire, but it does need to be provided to the verifier
+pub struct BulletproofAdvice<F> {
+    b: F,
+    /// sum_i r^i sum_j xi^j f_j(pt_i)
+    combined_inner_product: F,
+}
 
 //pub struct KimchiOracles {
 //    type nonrec 'f oracles =
@@ -148,7 +166,7 @@ pub struct WrapDeferredValues<WrapScalars> {
     /// The challenge used for combining polynomials
     xi: ScalarChallenge,
     /// The challenges from the inner-product argument that was partially verified.
-    bulletproof_challenges: BulletproofChallenges,
+    bulletproof_challenges: BulletproofChallenges<Fp>,
     /// Data specific to which step branch of the proof-system was verified
     branch_data: BranchData,
 }
@@ -160,7 +178,7 @@ pub struct WrapDeferredValues<WrapScalars> {
 /// Consists of some evaluations of PLONK polynomials (columns, permutation aggregation, etc.)
 /// and the remainder are things related to the inner product argument.
 pub struct StepDeferredValues<StepScalars> {
-    /// Could be either StepScalarsMinimal or StepScalarsIncircuit
+    /// Could be ei
     plonk: StepScalars,
     /// combined_inner_product = sum_{i < num_evaluation_points} sum_{j < num_polys} r^i xi^j f_j(pt_i)
     combined_inner_product: Fq,
@@ -170,7 +188,21 @@ pub struct StepDeferredValues<StepScalars> {
     /// The challenge used for combining polynomials
     xi: ScalarChallenge,
     /// The challenges from the inner-product argument that was partially verified.
-    bulletproof_challenges: BulletproofChallenges,
+    bulletproof_challenges: BulletproofChallenges<Fq>,
+}
+
+/// In-circuit type for wrap proofs
+pub struct WrapProofChecked {
+    // in mina: w_comm, z_comm, t_comm, lookup
+    messages: ProverCommitments<G1>,
+    // in mina: // lr, z_1, z_2, delta, challenge_polynomial_commitment
+    opening: OpeningProof<G1>,
+}
+
+pub struct AllEvals {
+    evals: ProofEvaluations<PointEvaluations<Vec<Fp>>>,
+    public_input: (Vec<Fp>, Vec<Fp>),
+    ft_eval1: Fp,
 }
 
 /// Represents a proof (along with its accumulation state) which wraps a
@@ -178,12 +210,12 @@ pub struct StepDeferredValues<StepScalars> {
 ///
 /// To have some notation, the proof S itself comes from a circuit that verified
 /// up to 'max_proofs_verified many wrap proofs W_0, ..., W_max_proofs_verified.
-pub struct StepWitness {
+pub struct StepWitness<WrapScalars> {
     /// The user-level statement corresponding to this proof.
-    app_state: (),
+    app_state: AppState,
     /// The polynomial commitments, polynomial evaluations, and
     /// opening proof corresponding to this latest wrap proof.
-    wrap_proof: (),
+    wrap_proof: WrapProofChecked,
     /// The accumulator state corresponding to the above proof. Contains
     /// - `deferred_values`: The values necessary for finishing the deferred "scalar field" computations.
     /// That is, computations which are over the "step" circuit's internal field that the
@@ -191,15 +223,15 @@ pub struct StepWitness {
     /// being different.
     /// - `sponge_digest_before_evaluations`: the sponge state: TODO
     /// - `messages_for_next_wrap_proof`
-    proof_state: (),
+    proof_state: WrapProofState<WrapScalars>,
     /// The evaluations from the step proof that this proof wraps
-    prev_proof_evals: (),
+    prev_proof_evals: AllEvals,
     /// The challenges c_0, ... c_{k - 1} corresponding to each W_i.
-    prev_challenge_poly_coms: (),
+    prev_challenges: [[Fp; TICK_ROUNDS_N]; MAX_PROOFS_VERIFIED],
     /// The commitments to the "challenge polynomials"
     /// \prod_{i = 0}^k (1 + c_{k - 1 - i} x^{2^i})
     /// corresponding to each of the "prev_challenges"
-    prev_challenges: (),
+    prev_challenge_poly_coms: [G2; MAX_PROOFS_VERIFIED],
 }
 
 /// The component of the proof accumulation state that is only
@@ -209,14 +241,14 @@ pub struct MessagesForNextStepProof {
     /// The actual application-level state (e.g., for Mina, this is
     /// the protocol state which contains the merkle root of the
     /// ledger, state related to consensus, etc.)
-    app_state: (),
+    app_state: AppState,
     /// The verification key corresponding to the wrap-circuit for
     /// this recursive proof system. It gets threaded through all the
     /// circuits so that the step circuits can verify proofs against
     /// it.
     dlog_plonk_index: PlonkVerificationKeyEvals,
     challenge_polynomial_commitments: ChallengePolynomialCommitments,
-    old_bulletproof_challenges: BulletproofChallenges,
+    old_bulletproof_challenges: BulletproofChallenges<Fq>,
 }
 
 /// The component of the proof accumulation state that is only
@@ -224,7 +256,7 @@ pub struct MessagesForNextStepProof {
 /// handled opaquely by any "step" circuits. *)
 pub struct MessagesForNextWrapProof {
     challenge_polynomial_commitments: G1,
-    old_bulletproof_challenges: BulletproofChallenges,
+    old_bulletproof_challenges: BulletproofChallenges<Fp>,
 }
 
 /// For each proof that a /step/ circuit verifies, we do not verify the whole proof.
@@ -282,15 +314,6 @@ pub struct StepStatement {
     /// The component of the proof accumulation state that is only computed on by the
     /// "wrapping" proof system, and that can be handled opaquely by any "step" circuits.
     messages_for_next_wrap_proof: MessagesForNextWrapProof,
-}
-
-/// This is data that can be computed in linear time from the proof + statement.
-///
-/// It doesn't need to be sent on the wire, but it does need to be provided to the verifier
-pub struct BulletproofAdvice<Fq> {
-    b: Fq,
-    /// sum_i r^i sum_j xi^j f_j(pt_i)
-    combined_inner_product: Fq,
 }
 
 /// This type models an "inductive rule". It includes
