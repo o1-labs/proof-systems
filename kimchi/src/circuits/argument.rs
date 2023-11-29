a2b54ef80509903eac469225a76a4935d81739ed
Danny Willems <be.danny.willems@gmail.com>
2023-11-29 19:04:00 +0100

    Fix build warning
    Warnings when
    
    RUSTDOCFLAGS="--enable-index-page -Zunstable-options" cargo +nightly doc --all --no-deps

--- kimchi/src/circuits/argument.rs
+++ kimchi/src/circuits/argument.rs
@@ -33,7 +33,7 @@ pub enum ArgumentType {
 
 /// The argument environment is used to specify how the argument's constraints are
 /// represented when they are built.  If the environment is created without ArgumentData
-/// and with F = Expr<F>, then the constraints are built as Expr expressions (e.g. for
+/// and with `F = Expr<F>`, then the constraints are built as Expr expressions (e.g. for
 /// use with the prover/verifier).  On the other hand, if the environment is
 /// created with ArgumentData and F = Field or F = PrimeField, then the constraints
 /// are built as expressions of real field elements and can be evaluated directly on
