d5c5758c1266e82247372c98742eff872d5b99e6
mrmr1993 <mr_1993@hotmail.co.uk>
2023-12-05 19:22:10 +0000

    Use name indices to reference named registers

--- optimism/src/mips/registers.rs
+++ optimism/src/mips/registers.rs
@@ -1,6 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::ops::{Index, IndexMut};
 
+pub const REGISTER_HI: usize = 32;
+pub const REGISTER_LO: usize = 33;
+pub const REGISTER_CURRENT_IP: usize = 34;
+pub const REGISTER_NEXT_IP: usize = 35;
+
 pub const NUM_REGISTERS: usize = 36;
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize)]
@@ -29,13 +34,13 @@ impl<T: Clone> Index<usize> for Registers<T> {
     fn index(&self, index: usize) -> &Self::Output {
         if index < 32 {
             &self.general_purpose[index]
-        } else if index == 32 {
+        } else if index == REGISTER_HI {
             &self.hi
-        } else if index == 33 {
+        } else if index == REGISTER_LO {
             &self.lo
-        } else if index == 34 {
+        } else if index == REGISTER_CURRENT_IP {
             &self.current_instruction_pointer
-        } else if index == 35 {
+        } else if index == REGISTER_NEXT_IP {
             &self.next_instruction_pointer
         } else {
             panic!("Index out of bounds");
@@ -47,13 +52,13 @@ impl<T: Clone> IndexMut<usize> for Registers<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         if index < 32 {
             &mut self.general_purpose[index]
-        } else if index == 32 {
+        } else if index == REGISTER_HI {
             &mut self.hi
-        } else if index == 33 {
+        } else if index == REGISTER_LO {
             &mut self.lo
-        } else if index == 34 {
+        } else if index == REGISTER_CURRENT_IP {
             &mut self.current_instruction_pointer
-        } else if index == 35 {
+        } else if index == REGISTER_NEXT_IP {
             &mut self.next_instruction_pointer
         } else {
             panic!("Index out of bounds");
