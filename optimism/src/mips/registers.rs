8438da5a355ac60cf25f751e5ba9dd603ffcfbf2
mrmr1993 <mr_1993@hotmail.co.uk>
2023-12-05 19:19:08 +0000

    Store instruction pointers in registers

--- optimism/src/mips/registers.rs
+++ optimism/src/mips/registers.rs
@@ -1,18 +1,25 @@
 use serde::{Deserialize, Serialize};
 use std::ops::{Index, IndexMut};
 
-pub const NUM_REGISTERS: usize = 34;
+pub const NUM_REGISTERS: usize = 36;
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize)]
 pub struct Registers<T> {
     pub general_purpose: [T; 32],
     pub hi: T,
     pub lo: T,
+    pub current_instruction_pointer: T,
+    pub next_instruction_pointer: T,
 }
 
 impl<T> Registers<T> {
     pub fn iter(&self) -> impl Iterator<Item = &T> {
-        self.general_purpose.iter().chain([&self.hi, &self.lo])
+        self.general_purpose.iter().chain([
+            &self.hi,
+            &self.lo,
+            &self.current_instruction_pointer,
+            &self.next_instruction_pointer,
+        ])
     }
 }
 
@@ -26,6 +33,10 @@ impl<T: Clone> Index<usize> for Registers<T> {
             &self.hi
         } else if index == 33 {
             &self.lo
+        } else if index == 34 {
+            &self.current_instruction_pointer
+        } else if index == 35 {
+            &self.next_instruction_pointer
         } else {
             panic!("Index out of bounds");
         }
@@ -40,6 +51,10 @@ impl<T: Clone> IndexMut<usize> for Registers<T> {
             &mut self.hi
         } else if index == 33 {
             &mut self.lo
+        } else if index == 34 {
+            &mut self.current_instruction_pointer
+        } else if index == 35 {
+            &mut self.next_instruction_pointer
         } else {
             panic!("Index out of bounds");
         }
