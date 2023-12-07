d29e197ae74d0f45fe8c3b77f2b3a75f40b6b472
querolita <anais.querol@gmail.com>
2023-12-07 16:19:50 +0100

    basic operation swith Expr

--- optimism/src/mips/constraints.rs
+++ optimism/src/mips/constraints.rs
@@ -1,5 +1,27 @@
-use kimchi::circuits::expr::{ConstantExpr, Expr};
+use ark_ff::{Field, One, Zero};
+use kimchi::circuits::{
+    expr::{ConstantExpr, Expr},
+    gate::CurrOrNext,
+};
 
 use super::column::Column;
 
-type _E<F> = Expr<ConstantExpr<F>, Column>;
+type E<F> = Expr<ConstantExpr<F>, Column>;
+
+pub fn boolean<F: Field>(x: E<F>) -> E<F> {
+    x.clone() * (x - Expr::one())
+}
+
+pub fn combine<F: Field>(constraints: impl Iterator<Item = E<F>>) -> E<F> {
+    constraints
+        .reduce(|acc, x| Expr::constant(ConstantExpr::Alpha) * acc + x)
+        .unwrap_or(E::zero())
+}
+
+pub fn curr_cell<F: Field>(col: Column) -> E<F> {
+    Expr::cell(col, CurrOrNext::Curr)
+}
+
+pub fn next_cell<F: Field>(col: Column) -> E<F> {
+    Expr::cell(col, CurrOrNext::Next)
+}
