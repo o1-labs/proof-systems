aa7cabf40cdaa1830e6cb5944c9e9be2eab154ce
dannywillems <dannywillems@users.noreply.github.com>
2023-12-08 01:06:04 +0000

    deploy: ef18839e4bf284476ffcdc38923a4874d87e35e6

--- rustdoc/kimchi_optimism/preimage_oracle/sidebar-items.js
+++ rustdoc/kimchi_optimism/preimage_oracle/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Key"],"struct":["PreImageOracle","RW","ReadWrite"]};
+window.SIDEBAR_ITEMS = {"enum":["Key"],"fn":["create_bidirectional_channel"],"struct":["PreImageOracle","RW","ReadWrite"]};
