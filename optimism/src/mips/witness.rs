115e74283a807a8424eec21737b1f6b6cc4a1c6f
mrmr1993 <mr_1993@hotmail.co.uk>
2023-12-10 07:53:53 +0000

    Move preimage oracle state to registers

--- optimism/src/mips/witness.rs
+++ optimism/src/mips/witness.rs
@@ -26,16 +26,12 @@ pub const SCRATCH_SIZE: usize = 60; // TODO: Delete and use a vector instead
 
 #[derive(Clone, Default)]
 pub struct SyscallEnv {
-    pub preimage_offset: u32,
-    pub preimage_key: [u8; 32],
     pub last_hint: Option<Vec<u8>>,
 }
 
 impl SyscallEnv {
     pub fn create(state: &State) -> Self {
         SyscallEnv {
-            preimage_key: state.preimage_key,
-            preimage_offset: state.preimage_offset,
             last_hint: state.last_hint.clone(),
         }
     }
@@ -483,13 +479,26 @@ impl<Fp: Field> Env<Fp> {
             .map(|(offset, _)| *offset)
             .collect::<Vec<_>>();
 
-        let initial_registers = Registers {
-            lo: state.lo,
-            hi: state.hi,
-            general_purpose: state.registers,
-            current_instruction_pointer: initial_instruction_pointer,
-            next_instruction_pointer,
-            heap_pointer: state.heap,
+        let initial_registers = {
+            let preimage_key = {
+                let mut preimage_key = [0u32; 8];
+                for i in 0..8 {
+                    preimage_key[i] = u32::from_be_bytes(
+                        state.preimage_key[i * 4..(i + 1) * 4].try_into().unwrap(),
+                    )
+                }
+                preimage_key
+            };
+            Registers {
+                lo: state.lo,
+                hi: state.hi,
+                general_purpose: state.registers,
+                current_instruction_pointer: initial_instruction_pointer,
+                next_instruction_pointer,
+                heap_pointer: state.heap,
+                preimage_key,
+                preimage_offset: state.preimage_offset,
+            }
         };
 
         Env {
