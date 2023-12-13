db28c55daa42fb8cea1c41ad3bc578e762da246e
Danny Willems <be.danny.willems@gmail.com>
2023-12-13 14:17:16 +0100

    Make ShellCheck happier, but not totally

--- optimism/run-vm.sh
+++ optimism/run-vm.sh
@@ -10,13 +10,13 @@ cargo run --bin kimchi_optimism --release -p kimchi_optimism -- \
     -- \
     ./ethereum-optimism/op-program/bin/op-program \
     --log.level DEBUG \
-    --l1 ${L1RPC} \
-    --l2 ${L2RPC} \
+    --l1 "${L1RPC}" \
+    --l2 "${L2RPC}" \
     --network sepolia \
-    --datadir ${OP_PROGRAM_DATA_DIR} \
-    --l1.head ${L1_HEAD} \
-    --l2.head ${L2_HEAD} \
-    --l2.outputroot ${STARTING_OUTPUT_ROOT} \
-    --l2.claim ${L2_CLAIM} \
-    --l2.blocknumber ${L2_BLOCK_NUMBER} \
+    --datadir "${OP_PROGRAM_DATA_DIR}" \
+    --l1.head "${L1_HEAD}" \
+    --l2.head "${L2_HEAD}" \
+    --l2.outputroot "${STARTING_OUTPUT_ROOT}" \
+    --l2.claim "${L2_CLAIM}" \
+    --l2.blocknumber "${L2_BLOCK_NUMBER}" \
     --server
