a270eda30241a4b45c7e6af58a1e31c8a0e20b86
querolita <anais.querol@gmail.com>
2023-12-14 16:30:40 +0100

    constraints for theta step

--- optimism/src/keccak/constraints.rs
+++ optimism/src/keccak/constraints.rs
@@ -1,12 +1,10 @@
 use crate::keccak::{
     column::KeccakColumn,
     environment::{KeccakEnv, KeccakEnvironment},
-    BoolOps,
+    {ArithOps, BoolOps, E, WORDS_IN_HASH},
 };
 use ark_ff::Field;
 use kimchi::circuits::polynomials::keccak::{DIM, QUARTERS};
-
-use super::{E, WORDS_IN_HASH};
 
 pub trait Constraints {
     type Column;
@@ -108,6 +106,60 @@ impl<Fp: Field> Constraints for KeccakEnv<Fp> {
         }
 
         // ROUND CONSTRAINTS
+        {
+            // DEFINE ROUND CONSTANT
+            // TODO: lookup round and sparse constants
+            // self.round() = [0..24)
+
+            // Define vectors storing expressions which are not in the witness layout for efficiency
+            let mut state_c: Vec<Vec<Self::Variable>> =
+                vec![vec![Self::constant(Fp::zero()); QUARTERS]; DIM];
+            let mut state_d: Vec<Vec<Self::Variable>> =
+                vec![vec![Self::constant(Fp::zero()); QUARTERS]; DIM];
+            let mut state_e: Vec<Vec<Vec<Self::Variable>>> =
+                vec![vec![vec![Self::constant(Fp::zero()); QUARTERS]; DIM]; DIM];
+
+            // STEP theta: 5 * ( 3 + 4 * 1 ) = 35 constraints
+            for x in 0..DIM {
+                let word_c = Self::from_quarters(&self.keccak_state.theta_dense_c, None, x);
+                let rem_c = Self::from_quarters(&self.keccak_state.theta_remainder_c, None, x);
+                let rot_c = Self::from_quarters(&self.keccak_state.theta_dense_rot_c, None, x);
+
+                self.constrain(
+                    self.is_round()
+                        * (word_c * Self::two_pow(1)
+                            - (self.quotient_c(x) * Self::two_pow(64) + rem_c.clone())),
+                );
+                self.constrain(self.is_round() * (rot_c - (self.quotient_c(x) + rem_c)));
+                self.constrain(self.is_round() * (Self::boolean(self.quotient_c(x))));
+
+                for q in 0..QUARTERS {
+                    state_c[x][q] = self.state_a(0, x, q)
+                        + self.state_a(1, x, q)
+                        + self.state_a(2, x, q)
+                        + self.state_a(3, x, q)
+                        + self.state_a(4, x, q);
+                    self.constrain(
+                        self.is_round()
+                            * (state_c[x][q].clone()
+                                - Self::from_shifts(
+                                    &self.keccak_state.theta_shifts_c,
+                                    None,
+                                    None,
+                                    Some(x),
+                                    Some(q),
+                                )),
+                    );
+
+                    state_d[x][q] = self.shifts_c(0, (x + DIM - 1) % DIM, q)
+                        + self.expand_rot_c((x + 1) % DIM, q);
+
+                    for (y, column_e) in state_e.iter_mut().enumerate() {
+                        column_e[x][q] = self.state_a(y, x, q) + state_d[x][q].clone();
+                    }
+                }
+            } // END theta
+        }
 
         // LOOKUP CONSTRAINTS
     }
