232de7ffc3e2b09ab397565df4becb1862b27b90
gabrielbosio <gabrielbosio95@gmail.com>
2023-12-19 19:24:48 +0100

    Add bn254 generators

--- curves/src/bn254/curve.rs
+++ curves/src/bn254/curve.rs
@@ -1,6 +1,10 @@
-use ark_bn254::g1;
+use ark_bn254::g1::{self, G1_GENERATOR_X, G1_GENERATOR_Y};
 use ark_ec::models::short_weierstrass_jacobian::{GroupAffine, GroupProjective};
+
+use super::Fq;
 
 pub type Bn254Parameters = g1::Parameters;
 pub type Bn254 = GroupAffine<Bn254Parameters>;
 pub type ProjectiveBn254 = GroupProjective<Bn254Parameters>;
+pub const G_GENERATOR_X: Fq = G1_GENERATOR_X;
+pub const G_GENERATOR_Y: Fq = G1_GENERATOR_Y;
