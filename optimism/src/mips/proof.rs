8d238d482981e2c1c81b3f72551cf6f0751c7919
mrmr1993 <mr_1993@hotmail.co.uk>
2023-12-19 23:02:05 +0000

    Proof inputs

---
+++ optimism/src/mips/proof.rs
@@ -0,0 +1,12 @@
+pub struct ProofInputs {}
+
+pub struct Proof {}
+
+pub fn prover(inputs: ProofInputs) -> Proof {
+    let ProofInputs {} = inputs;
+    Proof {}
+}
+
+pub fn verifier(proof: Proof) {
+    let Proof {} = proof;
+}
