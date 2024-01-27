e60255b9e7de97de0387f8edf3cf0f20c7ed0128
dannywillems <dannywillems@users.noreply.github.com>
2024-01-27 17:31:46 +0000

    deploy: 69f1d61fef79f5fcb1f97b5dd988b60028f3ecd5

--- rustdoc/o1_utils/sidebar-items.js
+++ rustdoc/o1_utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["adjacent_pairs","biguint_helpers","bitwise_operations","chunked_evaluations","chunked_polynomial","dense_polynomial","evaluations","field_helpers","foreign_field","hasher","math","serialization"]};
+window.SIDEBAR_ITEMS = {"mod":["adjacent_pairs","biguint_helpers","bitwise_operations","chunked_evaluations","chunked_polynomial","dense_polynomial","evaluations","field_helpers","foreign_field","hasher","math","serialization","tests"]};
