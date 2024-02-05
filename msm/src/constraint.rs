35bd2f4001dfa849ddfb3763ae062adadb49153e
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-02-05 12:57:30 +0000

    Add preliminary make_mips_witness

--- msm/src/constraint.rs
+++ msm/src/constraint.rs
@@ -1,10 +1,14 @@
-use ark_ff::Field;
+use ark_ec::AffineCurve;
+use ark_ff::{Field, UniformRand};
+use kimchi::circuits::domains::EvaluationDomains;
 use kimchi::circuits::expr::{ConstantExpr, Expr};
 use kimchi::circuits::expr::{ExprInner, Variable};
 use kimchi::circuits::gate::CurrOrNext;
+use rand::thread_rng;
 
 use crate::column::MSMColumn;
-use crate::{Fp, NUM_LIMBS};
+use crate::proof::{Witness, WitnessColumns};
+use crate::{Ff1, Fp, MsmBN254G1Affine, NUM_LIMBS};
 
 pub type MSMExpr<F> = Expr<ConstantExpr<F>, MSMColumn>;
 
@@ -36,22 +40,22 @@ pub fn make_mips_constraint() -> MSMExpr<Fp> {
     let mut limb_constraints: Vec<_> = vec![];
 
     for i in 0..NUM_LIMBS {
-        let a_1 = MSMExpr::Atom(ExprInner::<
+        let a_i = MSMExpr::Atom(ExprInner::<
             kimchi::circuits::expr::Operations<kimchi::circuits::expr::ConstantExprInner<Fp>>,
             MSMColumn,
         >::Cell(Variable {
             col: MSMColumn::A(i),
             row: CurrOrNext::Curr,
         }));
-        let b_1 = MSMExpr::Atom(ExprInner::Cell(Variable {
+        let b_i = MSMExpr::Atom(ExprInner::Cell(Variable {
             col: MSMColumn::B(i),
             row: CurrOrNext::Curr,
         }));
-        let c_1 = MSMExpr::Atom(ExprInner::Cell(Variable {
+        let c_i = MSMExpr::Atom(ExprInner::Cell(Variable {
             col: MSMColumn::C(i),
             row: CurrOrNext::Curr,
         }));
-        let limb_constraint = a_1 + b_1 - c_1;
+        let limb_constraint = a_i + b_i - c_i;
         limb_constraints.push(limb_constraint);
     }
 
@@ -60,4 +64,49 @@ pub fn make_mips_constraint() -> MSMExpr<Fp> {
 
     println!("{:?}", combined_constraint);
     combined_constraint
+}
+
+#[allow(dead_code)]
+fn limb_decompose(_input: Ff1) -> Vec<u8> {
+    // TODO see foreign_field.rs with from/to bigint conversion
+    unimplemented!()
+}
+
+pub fn make_mips_witness(_domain: EvaluationDomains<Fp>) -> Witness<MsmBN254G1Affine> {
+    let mut rng = thread_rng();
+    let a: Ff1 = Ff1::rand(&mut rng);
+    let b: Ff1 = Ff1::rand(&mut rng);
+
+    let a_limbs: Vec<u8> = limb_decompose(a);
+    let b_limbs: Vec<u8> = limb_decompose(b);
+    let c_limbs: Vec<u64> = a_limbs
+        .iter()
+        .zip(b_limbs.iter())
+        .map(|(ai, bi)| (*ai as u64) + (*bi as u64))
+        .collect();
+
+    let mut witness_a: Vec<MsmBN254G1Affine> = vec![];
+    let mut witness_b: Vec<MsmBN254G1Affine> = vec![];
+    let mut witness_c: Vec<MsmBN254G1Affine> = vec![];
+
+    let g: MsmBN254G1Affine = <MsmBN254G1Affine as ark_ec::AffineCurve>::prime_subgroup_generator();
+
+    for i in 0..NUM_LIMBS {
+        witness_a.push(From::from(g.mul(a_limbs[i] as u64)));
+        witness_b.push(From::from(g.mul(b_limbs[i] as u64)));
+        witness_c.push(From::from(g.mul(c_limbs[i])));
+    }
+
+    let _witness_columns = WitnessColumns {
+        a: witness_a.try_into().unwrap_or(panic!("Length mismatch")),
+        b: witness_b.try_into().unwrap_or(panic!("Length mismatch")),
+        c: witness_c.try_into().unwrap_or(panic!("Length mismatch")),
+    };
+
+    // TODO evaluate these on the domain
+    //let eval_a_1: MsmBn254G1Affine =
+
+    // TODO build witness
+
+    unimplemented!()
 }
--- msm/src/lib.rs
+++ msm/src/lib.rs
@@ -1,3 +1,4 @@
+use mina_curves::pasta::curves::pallas;
 use mina_poseidon::{
     constants::PlonkSpongeConstantsKimchi,
     sponge::{DefaultFqSponge, DefaultFrSponge},
@@ -21,7 +22,12 @@ pub const NUM_LOOKUP_M: usize = 8;
 pub type MsmBN254 = ark_ec::bn::Bn<ark_bn254::Parameters>;
 pub type MsmBN254G1Affine = <MsmBN254 as ark_ec::PairingEngine>::G1Affine;
 pub type MsmBN254G2Affine = <MsmBN254 as ark_ec::PairingEngine>::G2Affine;
+
+// THe native field we are working with
 pub type Fp = ark_bn254::Fr;
+// The foreign field we are emulating (one of the two)
+pub type Ff1 = mina_curves::pasta::Fp;
+pub type Ff2 = mina_curves::pasta::Fq;
 
 pub type SpongeParams = PlonkSpongeConstantsKimchi;
 pub type BaseSponge = DefaultFqSponge<ark_bn254::g1::Parameters, SpongeParams>;
