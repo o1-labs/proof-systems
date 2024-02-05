a77918528cb5ca7b99d7d99b5d6f058f63982905
Danny Willems <be.danny.willems@gmail.com>
2024-02-05 12:41:14 +0100

    Constraint: add combine

--- msm/src/constraint.rs
+++ msm/src/constraint.rs
@@ -4,6 +4,16 @@ use kimchi::circuits::expr::{ConstantExpr, Expr};
 
 pub type E<F> = Expr<ConstantExpr<F>, MSMColumn>;
 
+// t(X) = CONSTRAINT_1 * 1 + \
+//        CONSTRAINT_2 * \alpha + \
+//        CONSTRAINT_3 * \alpha^2
+//        ...
+pub fn combine<F: Field>(constraints: Vec<E<F>>) -> E<F> {
+    constraints
+        .reduce(|acc, x| Expr::constant(ConstantExpr::Alpha) * acc + x)
+        .unwrap_or(E::zero())
+}
+
 pub struct Env<F: Field> {
     // TODO
     pub(crate) _constraints: Vec<E<F>>,
