dd4d72a0cf9f66025fadff456ba420784dde5ea3
dannywillems <dannywillems@users.noreply.github.com>
2024-02-06 13:51:14 +0000

    deploy: ff587ea12577da26f497e2ff854707980f7b5497

--- rustdoc/kimchi_msm/sidebar-items.js
+++ rustdoc/kimchi_msm/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["DOMAIN_SIZE"],"mod":["columns","proof","prover","verifier"],"type":["BN254","BaseSponge","Fp","OpeningProof","ScalarSponge","SpongeParams"]};
+window.SIDEBAR_ITEMS = {"constant":["DOMAIN_SIZE"],"mod":["columns","constraint","proof","prover","verifier"],"type":["BN254","BaseSponge","Fp","OpeningProof","ScalarSponge","SpongeParams"]};
