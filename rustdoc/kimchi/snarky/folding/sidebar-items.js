18954e1240ca28f5aea2d006e461781e05f58c5e
dannywillems <dannywillems@users.noreply.github.com>
2024-02-14 11:12:00 +0000

    deploy: 13d14e51b9b0a6ab440b28437af406cc0c43418f

---
+++ rustdoc/kimchi/snarky/folding/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ForeignElement","FullChallenge","Private"]};
--- rustdoc/kimchi/snarky/sidebar-items.js
+++ rustdoc/kimchi/snarky/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["api","asm","boolean","constants","constraint_system","cvar","errors","poseidon","prelude","runner","snarky_type","union_find"]};
+window.SIDEBAR_ITEMS = {"mod":["api","asm","boolean","constants","constraint_system","cvar","errors","folding","poseidon","prelude","runner","snarky_type","union_find"]};
