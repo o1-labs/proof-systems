03b8802b4d1c0bdeca22e5afa085640de5b5859f
dannywillems <dannywillems@users.noreply.github.com>
2024-02-15 21:59:01 +0000

    deploy: 65f8381fa0818eff1c93f4daf3e4ff2965da9d16

--- rustdoc/kimchi_msm/precomputed_srs/sidebar-items.js
+++ rustdoc/kimchi_msm/precomputed_srs/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_and_store_srs","get_bn254_srs","get_bn254_srs_path","read_bn254_srs_from_disk","test_serialization"]};
+window.SIDEBAR_ITEMS = {"fn":["get_bn254_srs"]};
