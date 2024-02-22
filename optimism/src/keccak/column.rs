98ba031fecaf097cd0a3ce9f8e74a78e7739bce9
querolita <anais.querol@gmail.com>
2024-02-22 13:12:49 +0100

    implement generic iterators

--- optimism/src/keccak/column.rs
+++ optimism/src/keccak/column.rs
@@ -1,6 +1,9 @@
 //! This module defines the custom columns used in the Keccak witness, which
 //! are aliases for the actual Keccak witness columns also defined here.
-use super::{ZKVM_KECCAK_COLS_CURR, ZKVM_KECCAK_COLS_NEXT};
+use crate::{
+    keccak::{ZKVM_KECCAK_COLS_CURR, ZKVM_KECCAK_COLS_NEXT},
+    witness::Witness,
+};
 use ark_ff::{One, Zero};
 use kimchi::circuits::polynomials::keccak::constants::{
     CHI_SHIFTS_B_OFF, CHI_SHIFTS_SUM_OFF, KECCAK_COLS, PIRHO_DENSE_E_OFF, PIRHO_DENSE_ROT_E_OFF,
@@ -87,31 +90,43 @@ pub enum KeccakColumn {
 /// The Keccak circuit is split into two main modes: Sponge and Round.
 /// The columns are shared between the Sponge and Round steps.
 /// The hash and step indices are shared between both modes.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct KeccakWitness<T> {
-    pub hash_index: T,                        // Which hash this is inside the circuit
-    pub step_index: T,                        // Which step this is inside the hash
-    pub mode_flags: [T; MODE_FLAGS_COLS_LEN], // Round, Absorb, Squeeze
-    pub curr: [T; ZKVM_KECCAK_COLS_CURR], // Contains 1969 witnesses used in the current step including Input and RoundConstants
-    pub next: [T; ZKVM_KECCAK_COLS_NEXT], // Contains the Output
-}
+/// The row is split into the following entries:
+/// - hash_index: Which hash this is inside the circuit
+/// - step_index: Which step this is inside the hash
+/// - mode_flags: Round, Absorb, Squeeze
+/// - curr: Contains 1969 witnesses used in the current step including Input and RoundConstants
+/// - next: Contains the Output
+pub type KeccakWitness<T> = Witness<ZKVM_KECCAK_COLS_LENGTH, T>;
 
 impl<T: Clone> KeccakWitness<T> {
+    /// Returns the hash index
+    pub fn hash_index(&self) -> T {
+        self.row[0]
+    }
+
+    /// Returns the step index
+    pub fn step_index(&self) -> T {
+        self.row[1]
+    }
+
+    /// Returns the mode flags
+    pub fn mode_flags(&self) -> &[T] {
+        &self.row[2..5]
+    }
+
+    // Returns the `curr` witness columns
+    fn curr(&self) -> &[T] {
+        &self.row[5..5 + ZKVM_KECCAK_COLS_CURR]
+    }
+
+    // Returns the `next` witness columns
+    fn next(&self) -> &[T] {
+        &self.row[5 + ZKVM_KECCAK_COLS_CURR..]
+    }
+
     /// Returns a chunk of the `curr` witness columns
     pub fn chunk(&self, offset: usize, length: usize) -> &[T] {
-        &self.curr[offset..offset + length]
-    }
-}
-
-impl<T: Zero + One + Clone> Default for KeccakWitness<T> {
-    fn default() -> Self {
-        KeccakWitness {
-            hash_index: T::zero(),
-            step_index: T::zero(),
-            mode_flags: std::array::from_fn(|_| T::zero()), // Defaults are zero, but lookups will not be triggered
-            curr: std::array::from_fn(|_| T::zero()), // default zeros, but lookups will not be triggered always
-            next: std::array::from_fn(|_| T::zero()),
-        }
+        &self.curr()[offset..offset + length]
     }
 }
 
@@ -124,37 +139,37 @@ impl<T: Clone> Index<KeccakColumn> for KeccakWitness<T> {
     /// is used by intermediary values when executing the Round step.
     fn index(&self, index: KeccakColumn) -> &Self::Output {
         match index {
-            KeccakColumn::HashIndex => &self.hash_index,
-            KeccakColumn::StepIndex => &self.step_index,
-            KeccakColumn::FlagRound => &self.mode_flags[FLAG_ROUND_OFF],
-            KeccakColumn::FlagAbsorb => &self.mode_flags[FLAG_ABSORB_OFF],
-            KeccakColumn::FlagSqueeze => &self.mode_flags[FLAG_SQUEEZE_OFF],
-            KeccakColumn::FlagRoot => &self.curr[FLAG_ROOT_OFF],
-            KeccakColumn::PadLength => &self.curr[PAD_LEN_OFF],
-            KeccakColumn::InvPadLength => &self.curr[PAD_INV_OFF],
-            KeccakColumn::TwoToPad => &self.curr[PAD_TWO_OFF],
-            KeccakColumn::PadBytesFlags(idx) => &self.curr[PAD_BYTES_OFF + idx],
-            KeccakColumn::PadSuffix(idx) => &self.curr[PAD_SUFFIX_OFF + idx],
-            KeccakColumn::RoundConstants(idx) => &self.curr[ROUND_COEFFS_OFF + idx],
-            KeccakColumn::Input(idx) => &self.curr[idx],
-            KeccakColumn::ThetaShiftsC(idx) => &self.curr[THETA_SHIFTS_C_OFF + idx],
-            KeccakColumn::ThetaDenseC(idx) => &self.curr[THETA_DENSE_C_OFF + idx],
-            KeccakColumn::ThetaQuotientC(idx) => &self.curr[THETA_QUOTIENT_C_OFF + idx],
-            KeccakColumn::ThetaRemainderC(idx) => &self.curr[THETA_REMAINDER_C_OFF + idx],
-            KeccakColumn::ThetaDenseRotC(idx) => &self.curr[THETA_DENSE_ROT_C_OFF + idx],
-            KeccakColumn::ThetaExpandRotC(idx) => &self.curr[THETA_EXPAND_ROT_C_OFF + idx],
-            KeccakColumn::PiRhoShiftsE(idx) => &self.curr[PIRHO_SHIFTS_E_OFF + idx],
-            KeccakColumn::PiRhoDenseE(idx) => &self.curr[PIRHO_DENSE_E_OFF + idx],
-            KeccakColumn::PiRhoQuotientE(idx) => &self.curr[PIRHO_QUOTIENT_E_OFF + idx],
-            KeccakColumn::PiRhoRemainderE(idx) => &self.curr[PIRHO_REMAINDER_E_OFF + idx],
-            KeccakColumn::PiRhoDenseRotE(idx) => &self.curr[PIRHO_DENSE_ROT_E_OFF + idx],
-            KeccakColumn::PiRhoExpandRotE(idx) => &self.curr[PIRHO_EXPAND_ROT_E_OFF + idx],
-            KeccakColumn::ChiShiftsB(idx) => &self.curr[CHI_SHIFTS_B_OFF + idx],
-            KeccakColumn::ChiShiftsSum(idx) => &self.curr[CHI_SHIFTS_SUM_OFF + idx],
-            KeccakColumn::SpongeNewState(idx) => &self.curr[SPONGE_NEW_STATE_OFF + idx],
-            KeccakColumn::SpongeBytes(idx) => &self.curr[SPONGE_BYTES_OFF + idx],
-            KeccakColumn::SpongeShifts(idx) => &self.curr[SPONGE_SHIFTS_OFF + idx],
-            KeccakColumn::Output(idx) => &self.next[idx],
+            KeccakColumn::HashIndex => &self.hash_index(),
+            KeccakColumn::StepIndex => &self.step_index(),
+            KeccakColumn::FlagRound => &self.mode_flags()[FLAG_ROUND_OFF],
+            KeccakColumn::FlagAbsorb => &self.mode_flags()[FLAG_ABSORB_OFF],
+            KeccakColumn::FlagSqueeze => &self.mode_flags()[FLAG_SQUEEZE_OFF],
+            KeccakColumn::FlagRoot => &self.curr()[FLAG_ROOT_OFF],
+            KeccakColumn::PadLength => &self.curr()[PAD_LEN_OFF],
+            KeccakColumn::InvPadLength => &self.curr()[PAD_INV_OFF],
+            KeccakColumn::TwoToPad => &self.curr()[PAD_TWO_OFF],
+            KeccakColumn::PadBytesFlags(idx) => &self.curr()[PAD_BYTES_OFF + idx],
+            KeccakColumn::PadSuffix(idx) => &self.curr()[PAD_SUFFIX_OFF + idx],
+            KeccakColumn::RoundConstants(idx) => &self.curr()[ROUND_COEFFS_OFF + idx],
+            KeccakColumn::Input(idx) => &self.curr()[idx],
+            KeccakColumn::ThetaShiftsC(idx) => &self.curr()[THETA_SHIFTS_C_OFF + idx],
+            KeccakColumn::ThetaDenseC(idx) => &self.curr()[THETA_DENSE_C_OFF + idx],
+            KeccakColumn::ThetaQuotientC(idx) => &self.curr()[THETA_QUOTIENT_C_OFF + idx],
+            KeccakColumn::ThetaRemainderC(idx) => &self.curr()[THETA_REMAINDER_C_OFF + idx],
+            KeccakColumn::ThetaDenseRotC(idx) => &self.curr()[THETA_DENSE_ROT_C_OFF + idx],
+            KeccakColumn::ThetaExpandRotC(idx) => &self.curr()[THETA_EXPAND_ROT_C_OFF + idx],
+            KeccakColumn::PiRhoShiftsE(idx) => &self.curr()[PIRHO_SHIFTS_E_OFF + idx],
+            KeccakColumn::PiRhoDenseE(idx) => &self.curr()[PIRHO_DENSE_E_OFF + idx],
+            KeccakColumn::PiRhoQuotientE(idx) => &self.curr()[PIRHO_QUOTIENT_E_OFF + idx],
+            KeccakColumn::PiRhoRemainderE(idx) => &self.curr()[PIRHO_REMAINDER_E_OFF + idx],
+            KeccakColumn::PiRhoDenseRotE(idx) => &self.curr()[PIRHO_DENSE_ROT_E_OFF + idx],
+            KeccakColumn::PiRhoExpandRotE(idx) => &self.curr()[PIRHO_EXPAND_ROT_E_OFF + idx],
+            KeccakColumn::ChiShiftsB(idx) => &self.curr()[CHI_SHIFTS_B_OFF + idx],
+            KeccakColumn::ChiShiftsSum(idx) => &self.curr()[CHI_SHIFTS_SUM_OFF + idx],
+            KeccakColumn::SpongeNewState(idx) => &self.curr()[SPONGE_NEW_STATE_OFF + idx],
+            KeccakColumn::SpongeBytes(idx) => &self.curr()[SPONGE_BYTES_OFF + idx],
+            KeccakColumn::SpongeShifts(idx) => &self.curr()[SPONGE_SHIFTS_OFF + idx],
+            KeccakColumn::Output(idx) => &self.next()[idx],
         }
     }
 }
@@ -162,141 +177,37 @@ impl<T: Clone> Index<KeccakColumn> for KeccakWitness<T> {
 impl<T: Clone> IndexMut<KeccakColumn> for KeccakWitness<T> {
     fn index_mut(&mut self, index: KeccakColumn) -> &mut Self::Output {
         match index {
-            KeccakColumn::HashIndex => &mut self.hash_index,
-            KeccakColumn::StepIndex => &mut self.step_index,
-            KeccakColumn::FlagRound => &mut self.mode_flags[FLAG_ROUND_OFF],
-            KeccakColumn::FlagAbsorb => &mut self.mode_flags[FLAG_ABSORB_OFF],
-            KeccakColumn::FlagSqueeze => &mut self.mode_flags[FLAG_SQUEEZE_OFF],
-            KeccakColumn::FlagRoot => &mut self.curr[FLAG_ROOT_OFF],
-            KeccakColumn::PadLength => &mut self.curr[PAD_LEN_OFF],
-            KeccakColumn::InvPadLength => &mut self.curr[PAD_INV_OFF],
-            KeccakColumn::TwoToPad => &mut self.curr[PAD_TWO_OFF],
-            KeccakColumn::PadBytesFlags(idx) => &mut self.curr[PAD_BYTES_OFF + idx],
-            KeccakColumn::PadSuffix(idx) => &mut self.curr[PAD_SUFFIX_OFF + idx],
-            KeccakColumn::RoundConstants(idx) => &mut self.curr[ROUND_COEFFS_OFF + idx],
-            KeccakColumn::Input(idx) => &mut self.curr[idx],
-            KeccakColumn::ThetaShiftsC(idx) => &mut self.curr[THETA_SHIFTS_C_OFF + idx],
-            KeccakColumn::ThetaDenseC(idx) => &mut self.curr[THETA_DENSE_C_OFF + idx],
-            KeccakColumn::ThetaQuotientC(idx) => &mut self.curr[THETA_QUOTIENT_C_OFF + idx],
-            KeccakColumn::ThetaRemainderC(idx) => &mut self.curr[THETA_REMAINDER_C_OFF + idx],
-            KeccakColumn::ThetaDenseRotC(idx) => &mut self.curr[THETA_DENSE_ROT_C_OFF + idx],
-            KeccakColumn::ThetaExpandRotC(idx) => &mut self.curr[THETA_EXPAND_ROT_C_OFF + idx],
-            KeccakColumn::PiRhoShiftsE(idx) => &mut self.curr[PIRHO_SHIFTS_E_OFF + idx],
-            KeccakColumn::PiRhoDenseE(idx) => &mut self.curr[PIRHO_DENSE_E_OFF + idx],
-            KeccakColumn::PiRhoQuotientE(idx) => &mut self.curr[PIRHO_QUOTIENT_E_OFF + idx],
-            KeccakColumn::PiRhoRemainderE(idx) => &mut self.curr[PIRHO_REMAINDER_E_OFF + idx],
-            KeccakColumn::PiRhoDenseRotE(idx) => &mut self.curr[PIRHO_DENSE_ROT_E_OFF + idx],
-            KeccakColumn::PiRhoExpandRotE(idx) => &mut self.curr[PIRHO_EXPAND_ROT_E_OFF + idx],
-            KeccakColumn::ChiShiftsB(idx) => &mut self.curr[CHI_SHIFTS_B_OFF + idx],
-            KeccakColumn::ChiShiftsSum(idx) => &mut self.curr[CHI_SHIFTS_SUM_OFF + idx],
-            KeccakColumn::SpongeNewState(idx) => &mut self.curr[SPONGE_NEW_STATE_OFF + idx],
-            KeccakColumn::SpongeBytes(idx) => &mut self.curr[SPONGE_BYTES_OFF + idx],
-            KeccakColumn::SpongeShifts(idx) => &mut self.curr[SPONGE_SHIFTS_OFF + idx],
-            KeccakColumn::Output(idx) => &mut self.next[idx],
+            KeccakColumn::HashIndex => &mut self.hash_index(),
+            KeccakColumn::StepIndex => &mut self.step_index(),
+            KeccakColumn::FlagRound => &mut self.mode_flags()[FLAG_ROUND_OFF],
+            KeccakColumn::FlagAbsorb => &mut self.mode_flags()[FLAG_ABSORB_OFF],
+            KeccakColumn::FlagSqueeze => &mut self.mode_flags()[FLAG_SQUEEZE_OFF],
+            KeccakColumn::FlagRoot => &mut self.curr()[FLAG_ROOT_OFF],
+            KeccakColumn::PadLength => &mut self.curr()[PAD_LEN_OFF],
+            KeccakColumn::InvPadLength => &mut self.curr()[PAD_INV_OFF],
+            KeccakColumn::TwoToPad => &mut self.curr()[PAD_TWO_OFF],
+            KeccakColumn::PadBytesFlags(idx) => &mut self.curr()[PAD_BYTES_OFF + idx],
+            KeccakColumn::PadSuffix(idx) => &mut self.curr()[PAD_SUFFIX_OFF + idx],
+            KeccakColumn::RoundConstants(idx) => &mut self.curr()[ROUND_COEFFS_OFF + idx],
+            KeccakColumn::Input(idx) => &mut self.curr()[idx],
+            KeccakColumn::ThetaShiftsC(idx) => &mut self.curr()[THETA_SHIFTS_C_OFF + idx],
+            KeccakColumn::ThetaDenseC(idx) => &mut self.curr()[THETA_DENSE_C_OFF + idx],
+            KeccakColumn::ThetaQuotientC(idx) => &mut self.curr()[THETA_QUOTIENT_C_OFF + idx],
+            KeccakColumn::ThetaRemainderC(idx) => &mut self.curr()[THETA_REMAINDER_C_OFF + idx],
+            KeccakColumn::ThetaDenseRotC(idx) => &mut self.curr()[THETA_DENSE_ROT_C_OFF + idx],
+            KeccakColumn::ThetaExpandRotC(idx) => &mut self.curr()[THETA_EXPAND_ROT_C_OFF + idx],
+            KeccakColumn::PiRhoShiftsE(idx) => &mut self.curr()[PIRHO_SHIFTS_E_OFF + idx],
+            KeccakColumn::PiRhoDenseE(idx) => &mut self.curr()[PIRHO_DENSE_E_OFF + idx],
+            KeccakColumn::PiRhoQuotientE(idx) => &mut self.curr()[PIRHO_QUOTIENT_E_OFF + idx],
+            KeccakColumn::PiRhoRemainderE(idx) => &mut self.curr()[PIRHO_REMAINDER_E_OFF + idx],
+            KeccakColumn::PiRhoDenseRotE(idx) => &mut self.curr()[PIRHO_DENSE_ROT_E_OFF + idx],
+            KeccakColumn::PiRhoExpandRotE(idx) => &mut self.curr()[PIRHO_EXPAND_ROT_E_OFF + idx],
+            KeccakColumn::ChiShiftsB(idx) => &mut self.curr()[CHI_SHIFTS_B_OFF + idx],
+            KeccakColumn::ChiShiftsSum(idx) => &mut self.curr()[CHI_SHIFTS_SUM_OFF + idx],
+            KeccakColumn::SpongeNewState(idx) => &mut self.curr()[SPONGE_NEW_STATE_OFF + idx],
+            KeccakColumn::SpongeBytes(idx) => &mut self.curr()[SPONGE_BYTES_OFF + idx],
+            KeccakColumn::SpongeShifts(idx) => &mut self.curr()[SPONGE_SHIFTS_OFF + idx],
+            KeccakColumn::Output(idx) => &mut self.next()[idx],
         }
-    }
-}
-
-impl<F> IntoIterator for KeccakWitness<F> {
-    type Item = F;
-    type IntoIter = std::vec::IntoIter<F>;
-
-    /// Iterate over the columns in the Keccak circuit.
-    fn into_iter(self) -> Self::IntoIter {
-        let mut iter_contents = Vec::with_capacity(ZKVM_KECCAK_COLS_LENGTH);
-        iter_contents.push(self.hash_index);
-        iter_contents.push(self.step_index);
-        iter_contents.extend(self.mode_flags);
-        iter_contents.extend(self.curr);
-        iter_contents.extend(self.next);
-        iter_contents.into_iter()
-    }
-}
-
-impl<G> IntoParallelIterator for KeccakWitness<G>
-where
-    Vec<G>: IntoParallelIterator,
-{
-    type Iter = <Vec<G> as IntoParallelIterator>::Iter;
-    type Item = <Vec<G> as IntoParallelIterator>::Item;
-
-    /// Iterate over the columns in the Keccak circuit, in parallel.
-    fn into_par_iter(self) -> Self::Iter {
-        let mut iter_contents = Vec::with_capacity(ZKVM_KECCAK_COLS_LENGTH);
-        iter_contents.push(self.hash_index);
-        iter_contents.push(self.step_index);
-        iter_contents.extend(self.mode_flags);
-        iter_contents.extend(self.curr);
-        iter_contents.extend(self.next);
-        iter_contents.into_par_iter()
-    }
-}
-
-impl<G: Send + std::fmt::Debug> FromParallelIterator<G> for KeccakWitness<G> {
-    fn from_par_iter<I>(par_iter: I) -> Self
-    where
-        I: IntoParallelIterator<Item = G>,
-    {
-        let mut iter_contents = par_iter.into_par_iter().collect::<Vec<_>>();
-        let next = iter_contents
-            .drain(iter_contents.len() - ZKVM_KECCAK_COLS_NEXT..)
-            .collect::<Vec<G>>()
-            .try_into()
-            .unwrap();
-        let curr = iter_contents
-            .drain(iter_contents.len() - ZKVM_KECCAK_COLS_CURR..)
-            .collect::<Vec<G>>()
-            .try_into()
-            .unwrap();
-        let mode_flags = iter_contents
-            .drain(iter_contents.len() - MODE_FLAGS_COLS_LEN..)
-            .collect::<Vec<G>>()
-            .try_into()
-            .unwrap();
-        let step_index = iter_contents.pop().unwrap();
-        let hash_index = iter_contents.pop().unwrap();
-        KeccakWitness {
-            hash_index,
-            step_index,
-            mode_flags,
-            curr,
-            next,
-        }
-    }
-}
-
-impl<'data, G> IntoParallelIterator for &'data KeccakWitness<G>
-where
-    Vec<&'data G>: IntoParallelIterator,
-{
-    type Iter = <Vec<&'data G> as IntoParallelIterator>::Iter;
-    type Item = <Vec<&'data G> as IntoParallelIterator>::Item;
-
-    fn into_par_iter(self) -> Self::Iter {
-        let mut iter_contents = Vec::with_capacity(ZKVM_KECCAK_COLS_LENGTH);
-        iter_contents.push(&self.hash_index);
-        iter_contents.push(&self.step_index);
-        iter_contents.extend(&self.mode_flags);
-        iter_contents.extend(&self.curr);
-        iter_contents.extend(&self.next);
-        iter_contents.into_par_iter()
-    }
-}
-
-impl<'data, G> IntoParallelIterator for &'data mut KeccakWitness<G>
-where
-    Vec<&'data mut G>: IntoParallelIterator,
-{
-    type Iter = <Vec<&'data mut G> as IntoParallelIterator>::Iter;
-    type Item = <Vec<&'data mut G> as IntoParallelIterator>::Item;
-
-    fn into_par_iter(self) -> Self::Iter {
-        let mut iter_contents = Vec::with_capacity(ZKVM_KECCAK_COLS_LENGTH);
-        iter_contents.push(&mut self.hash_index);
-        iter_contents.push(&mut self.step_index);
-        iter_contents.extend(&mut self.mode_flags);
-        iter_contents.extend(&mut self.curr);
-        iter_contents.extend(&mut self.next);
-        iter_contents.into_par_iter()
     }
 }
