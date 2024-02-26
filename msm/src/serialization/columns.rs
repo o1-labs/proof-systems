7cee3d704908d8fda37e51730250e88582347f55
Danny Willems <be.danny.willems@gmail.com>
2024-02-26 17:46:34 +0100

    Sketch serialization subcircuit subdirectory

---
+++ msm/src/serialization/columns.rs
@@ -0,0 +1,34 @@
+use crate::columns::Column;
+use crate::columns::ColumnIndexer;
+use crate::LIMBS_NUM;
+use kimchi::circuits::expr::ConstantExpr;
+use kimchi::circuits::expr::Expr;
+
+/// Columns for the circuit splitting the bulletproof challenges in limbs used
+/// by the MSM.
+pub enum DecompositionColumnIndexer {
+    KimchiLimbs(usize),
+    MSMLimbs(usize),
+    IntermediateKimchiLimbs(usize),
+}
+
+impl ColumnIndexer for DecompositionColumnIndexer {
+    fn ix_to_column(self) -> Column {
+        match self {
+            DecompositionColumnIndexer::KimchiLimbs(i) => {
+                assert!(i < 3);
+                Column::X(i)
+            }
+            DecompositionColumnIndexer::MSMLimbs(i) => {
+                assert!(i < LIMBS_NUM);
+                Column::X(3 + i)
+            }
+            DecompositionColumnIndexer::IntermediateKimchiLimbs(i) => {
+                assert!(i < 3 + LIMBS_NUM);
+                Column::X(3 + LIMBS_NUM + i)
+            }
+        }
+    }
+}
+
+pub type DecompositionExpr<F> = Expr<ConstantExpr<F>, Column>;
