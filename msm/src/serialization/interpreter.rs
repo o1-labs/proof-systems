b5cdae335a8d28f5fefd95cae03ae84681f08e5a
Danny Willems <be.danny.willems@gmail.com>
2024-02-28 00:32:47 +0100

    Comments, change rangechecks

--- msm/src/serialization/interpreter.rs
+++ msm/src/serialization/interpreter.rs
@@ -3,9 +3,16 @@ pub trait InterpreterEnv {
 
     type Variable;
 
-    fn range_check64(&mut self, _value: &Self::Variable) {
+    /// Check that the value is in the range [0, 2^15-1]
+    fn range_check15(&mut self, _value: &Self::Variable) {
         // TODO
     }
 
+    /// Check that the value is in the range [0, 2^4-1]
+    fn range_check4(&mut self, _value: &Self::Variable) {
+        // TODO
+    }
+
+    /// Deserialize the next field element given as input
     fn deserialize_field_element(&mut self);
 }
