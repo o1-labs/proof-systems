c0968c381a0fb265e432a8f4a31d9efab7912ac1
Danny Willems <be.danny.willems@gmail.com>
2024-02-28 15:41:24 +0100

    Env: not parametrized by N

--- msm/src/serialization/witness.rs
+++ msm/src/serialization/witness.rs
@@ -5,11 +5,7 @@ use crate::serialization::interpreter::InterpreterEnv;
 use crate::LIMBS_NUM;
 
 /// Environment for the serializer interpreter
-/// It is parametrized by the number of field elements to be serialized and the
-/// field
-pub struct Env<const N: usize, Fp> {
-    pub step: usize,
-    pub kimchi_limbs: [[Fp; 3]; N],
+pub struct Env<Fp> {
     pub current_kimchi_limbs: [Fp; 3],
     /// The LIMB_NUM limbs that is used to encode a field element for the MSM
     pub msm_limbs: [Fp; LIMBS_NUM],
@@ -18,7 +14,7 @@ pub struct Env<const N: usize, Fp> {
     pub intermediate_limbs: [Fp; 19],
 }
 
-impl<const N: usize, Fp: Field> InterpreterEnv for Env<N, Fp> {
+impl<Fp: Field> InterpreterEnv for Env<Fp> {
     type Position = Column;
 
     // FIXME: is u128 ok? I think so, we only have 15 bits, 88 bits and 4 bits
@@ -51,7 +47,7 @@ impl<const N: usize, Fp: Field> InterpreterEnv for Env<N, Fp> {
     }
 }
 
-impl<const N: usize, Fp: Field> Env<N, Fp> {
+impl<Fp: Field> Env<Fp> {
     pub fn write_column(&mut self, position: Column, value: u128) {
         match position {
             Column::X(i) => {
@@ -69,11 +65,9 @@ impl<const N: usize, Fp: Field> Env<N, Fp> {
     }
 }
 
-impl<const N: usize, Fp: Field> Env<N, Fp> {
-    pub fn create(kimchi_limbs: [[Fp; 3]; N]) -> Self {
+impl<Fp: Field> Env<Fp> {
+    pub fn create() -> Self {
         Self {
-            step: 0,
-            kimchi_limbs,
             current_kimchi_limbs: [Fp::zero(); 3],
             msm_limbs: [Fp::zero(); LIMBS_NUM],
             intermediate_limbs: [Fp::zero(); 19],
