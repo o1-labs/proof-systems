27c63ae02c005b6205f62033a871325512d380f7
dannywillems <dannywillems@users.noreply.github.com>
2024-02-29 20:34:13 +0000

    deploy: c8816ab548cd4fdd95d8815fa4ba02797f2f7979

--- rustdoc/kimchi_msm/proof/sidebar-items.js
+++ rustdoc/kimchi_msm/proof/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Proof","Witness","WitnessColumns"]};
+window.SIDEBAR_ITEMS = {"struct":["Proof","ProofInputs"]};
--- rustdoc/serialization/sidebar-items.js
+++ rustdoc/serialization/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["main"]};
+window.SIDEBAR_ITEMS = {"constant":["N"],"fn":["main"]};
