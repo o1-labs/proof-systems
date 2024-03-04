93409c6ed54ae33dc47f12f522de2b23c27c728a
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-03-04 14:59:16 +0000

    [#1793] Rename MSMColumnIndexer to FFAColumnIndexer

--- msm/src/ffa/columns.rs
+++ msm/src/ffa/columns.rs
@@ -5,24 +5,24 @@ use crate::LIMBS_NUM;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 /// Column indexer for MSM columns
-pub enum MSMColumnIndexer {
+pub enum FFAColumnIndexer {
     A(usize),
     B(usize),
     C(usize),
     D(usize),
 }
 
-impl ColumnIndexer for MSMColumnIndexer {
+impl ColumnIndexer for FFAColumnIndexer {
     fn ix_to_column(self) -> Column {
         let to_column_inner = |offset, i| {
             assert!(i < LIMBS_NUM);
             Column::X(LIMBS_NUM * offset + i)
         };
         match self {
-            MSMColumnIndexer::A(i) => to_column_inner(0, i),
-            MSMColumnIndexer::B(i) => to_column_inner(1, i),
-            MSMColumnIndexer::C(i) => to_column_inner(2, i),
-            MSMColumnIndexer::D(i) => to_column_inner(3, i),
+            FFAColumnIndexer::A(i) => to_column_inner(0, i),
+            FFAColumnIndexer::B(i) => to_column_inner(1, i),
+            FFAColumnIndexer::C(i) => to_column_inner(2, i),
+            FFAColumnIndexer::D(i) => to_column_inner(3, i),
         }
     }
 }
