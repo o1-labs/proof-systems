90f3eba4ebffa6e5481f328cbe9ab65378f0a71d
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-03-05 18:57:50 +0000

    [#1791] Add minimal FFAInterpreterEnv

---
+++ msm/src/ffa/interpreter.rs
@@ -0,0 +1,17 @@
+use ark_ff::PrimeField;
+
+pub trait FFAInterpreterEnv<Fp: PrimeField> {
+    type Position;
+
+    type Variable: Clone
+        + std::ops::Add<Self::Variable, Output = Self::Variable>
+        + std::ops::Sub<Self::Variable, Output = Self::Variable>
+        + std::ops::Mul<Self::Variable, Output = Self::Variable>
+        + std::fmt::Debug;
+
+    fn add_constraint(&mut self, cst: Self::Variable);
+
+    fn copy(&mut self, x: &Self::Variable, position: Self::Position) -> Self::Variable;
+
+    fn constant(value: Fp) -> Self::Variable;
+}
