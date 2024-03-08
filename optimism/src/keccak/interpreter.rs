871d546fdf84692c3fd1bb5d5374cf4881d07169
querolita <anais.querol@gmail.com>
2024-03-08 18:51:07 +0100

    inline boolops inside keccak interpreter

--- optimism/src/keccak/interpreter.rs
+++ optimism/src/keccak/interpreter.rs
@@ -13,6 +13,48 @@ pub trait KeccakInterpreter<F: One + Debug + Zero> {
         + One
         + Zero;
 
+    ////////////////////////
+    // BOOLEAN OPERATIONS //
+    ////////////////////////
+
+    /// Degree-2 variable encoding whether the input is a boolean value (0 = yes)
+    fn is_boolean(x: Self::Variable) -> Self::Variable {
+        x.clone() * (x - Self::Variable::one())
+    }
+
+    /// Degree-1 variable encoding the negation of the input
+    /// Note: it only works as expected if the input is a boolean value
+    fn not(x: Self::Variable) -> Self::Variable {
+        Self::Variable::one() - x
+    }
+
+    /// Degree-1 variable encoding whether the input is the value one (0 = yes)
+    fn is_one(x: Self::Variable) -> Self::Variable {
+        Self::not(x)
+    }
+
+    /// Degree-2 variable encoding whether the first input is nonzero (0 = yes).
+    /// It requires the second input to be the multiplicative inverse of the first.
+    /// Note: if the first input is zero, there is no multiplicative inverse.
+    fn is_nonzero(x: Self::Variable, x_inv: Self::Variable) -> Self::Variable {
+        Self::is_one(x * x_inv)
+    }
+
+    /// Degree-1 variable encoding the XOR of two variables which should be boolean (1 = true)
+    fn xor(x: Self::Variable, y: Self::Variable) -> Self::Variable {
+        x.clone() + y.clone() - Self::constant(2) * x * y
+    }
+
+    /// Degree-1 variable encoding the OR of two variables, which should be boolean (1 = true)
+    fn or(x: Self::Variable, y: Self::Variable) -> Self::Variable {
+        x.clone() + y.clone() - x * y
+    }
+
+    /// Degree-2 variable encoding whether at least one of the two inputs is zero (0 = yes)
+    fn either_zero(x: Self::Variable, y: Self::Variable) -> Self::Variable {
+        x * y
+    }
+
     //////////////////////////
     // ARITHMETIC OPERATIONS //
     ///////////////////////////
