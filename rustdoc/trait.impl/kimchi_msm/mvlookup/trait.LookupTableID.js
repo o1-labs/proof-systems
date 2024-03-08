0565cc77dbbc1b0685c0bb8a3839ac43898dc6c4
volhovm <volhovm@users.noreply.github.com>
2024-03-08 14:55:02 +0000

    deploy: 36853bd853c462d7373dc21ff6a5fa7ed41b0d7d

--- rustdoc/trait.impl/kimchi_msm/mvlookup/trait.LookupTableID.js
+++ rustdoc/trait.impl/kimchi_msm/mvlookup/trait.LookupTableID.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "kimchi_msm":[],
-"kimchi_optimism":[["impl <a class=\"trait\" href=\"kimchi_msm/mvlookup/trait.LookupTableID.html\" title=\"trait kimchi_msm::mvlookup::LookupTableID\">LookupTableID</a> for <a class=\"enum\" href=\"kimchi_optimism/lookups/enum.LookupTableIDs.html\" title=\"enum kimchi_optimism::lookups::LookupTableIDs\">LookupTableIDs</a>"]]
+"kimchi_optimism":[["impl LookupTableID for <a class=\"enum\" href=\"kimchi_optimism/lookups/enum.LookupTableIDs.html\" title=\"enum kimchi_optimism::lookups::LookupTableIDs\">LookupTableIDs</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
