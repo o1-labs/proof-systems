cea3bd4b6d1a8ec2b4082565802bf42371ca482b
dannywillems <dannywillems@users.noreply.github.com>
2024-03-12 13:04:26 +0000

    deploy: dc4dc9cc88f30885615e9de63ea4c05cbe2e8e44

--- rustdoc/kimchi_msm/serialization/sidebar-items.js
+++ rustdoc/kimchi_msm/serialization/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["N_INTERMEDIATE_LIMBS"],"enum":["LookupTable"],"mod":["column","constraints","interpreter","witness"]};
+window.SIDEBAR_ITEMS = {"constant":["N_INTERMEDIATE_LIMBS"],"enum":["LookupTable"],"mod":["column","constraints","interpreter","witness"],"type":["Lookup"]};
