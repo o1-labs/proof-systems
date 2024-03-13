c944c3435cba3055d6008f1830e79f0c1f0a8bf7
Danny Willems <be.danny.willems@gmail.com>
2024-03-13 17:26:34 +0100

    Column: comment lookup aggregation

--- msm/src/columns.rs
+++ msm/src/columns.rs
@@ -9,6 +9,7 @@ pub enum Column {
     LookupPartialSum(usize),
     /// Multiplicities, indexed. This corresponds to the `m_i`
     LookupMultiplicity(u32),
+    /// The lookup aggregation, i.e. `phi`
     LookupAggregation,
     /// The fixed tables. The parameter is considered to the indexed table.
     /// u32 has been arbitrarily chosen as it seems to be already large enough
