de5786163e97a68c8840afa5b78c95d957aa3854
dannywillems <dannywillems@users.noreply.github.com>
2024-03-18 13:41:14 +0000

    deploy: 4c8e8866031e7efe3f43de2fa32667dfea48d48a

--- rustdoc/trait.impl/kimchi/circuits/expr/trait.ColumnEnvironment.js
+++ rustdoc/trait.impl/kimchi/circuits/expr/trait.ColumnEnvironment.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "kimchi":[],
-"kimchi_msm":[["impl&lt;'a, F: FftField&gt; ColumnEnvironment&lt;'a, F&gt; for <a class=\"struct\" href=\"kimchi_msm/column_env/struct.ColumnEnvironment.html\" title=\"struct kimchi_msm::column_env::ColumnEnvironment\">ColumnEnvironment</a>&lt;'a, F&gt;"]]
+"kimchi_msm":[["impl&lt;'a, const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>, F: FftField&gt; ColumnEnvironment&lt;'a, F&gt; for <a class=\"struct\" href=\"kimchi_msm/column_env/struct.ColumnEnvironment.html\" title=\"struct kimchi_msm::column_env::ColumnEnvironment\">ColumnEnvironment</a>&lt;'a, N, F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
