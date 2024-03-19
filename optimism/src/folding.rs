ff72791a4721bbe7b0414962746549565edbdb37
querolita <anais.querol@gmail.com>
2024-03-19 16:54:42 +0100

    switch to impl for concrete field and curve, include foldingitness

--- optimism/src/folding.rs
+++ optimism/src/folding.rs
@@ -1,10 +1,11 @@
 use ark_bn254;
+use ark_ec::short_weierstrass_jacobian::GroupAffine;
 use ark_ec::{AffineCurve, ProjectiveCurve};
 use ark_ff::{Field, One, UniformRand, Zero};
 use ark_poly::{EvaluationDomain, Evaluations, Radix2EvaluationDomain};
 use kimchi::folding::{
-    expressions::FoldingColumnTrait, BaseSponge, FoldingConfig, FoldingEnv, Instance, Side, Sponge,
-    Witness,
+    expressions::FoldingColumnTrait, Alphas, BaseSponge, FoldingConfig, FoldingEnv, Instance, Side,
+    Sponge, Witness,
 };
 use mina_poseidon::{
     constants::PlonkSpongeConstantsKimchi,
@@ -19,62 +20,20 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
+// Instantiate it with the desired group and field
 pub type Curve = ark_bn254::G1Affine;
 pub type Fp = ark_bn254::Fr;
 
-#[derive(Debug, Clone)]
-pub(crate) enum Alphas<F> {
-    Powers(F, Rc<AtomicUsize>),
-    Combinations(Vec<F>),
-}
-
-impl<F: Field> Alphas<F> {
-    pub(crate) fn new(alpha: F) -> Self {
-        Self::Powers(alpha, Rc::new(AtomicUsize::from(0)))
-    }
-    pub(crate) fn get(&self, i: usize) -> Option<F> {
-        match self {
-            Alphas::Powers(alpha, count) => {
-                let _ = count.fetch_max(i + 1, Ordering::Relaxed);
-                let i = [i as u64];
-                Some(alpha.pow(i))
-            }
-            Alphas::Combinations(alphas) => alphas.get(i).cloned(),
-        }
-    }
-    pub(crate) fn powers(self) -> Vec<F> {
-        match self {
-            Alphas::Powers(alpha, count) => {
-                let n = count.load(Ordering::Relaxed);
-                let alphas = successors(Some(F::one()), |last| Some(*last * alpha));
-                alphas.take(n).collect()
-            }
-            Alphas::Combinations(c) => c,
-        }
-    }
-    pub(crate) fn combine(a: Self, b: Self, challenge: F) -> Self {
-        let a = a.powers();
-        let b = b.powers();
-        assert_eq!(a.len(), b.len());
-        let comb = a
-            .into_iter()
-            .zip(b)
-            .map(|(a, b)| a + b * challenge)
-            .collect();
-        Self::Combinations(comb)
-    }
-}
-
 /// Folding instance containing the commitment to a witness of N columns, challenges for the proof, and the alphas
 #[derive(Debug, Clone)]
-pub(crate) struct FoldingInstance<const N: usize, G: AffineCurve> {
-    commitments: [G; N],
-    challenges: [<G as AffineCurve>::ScalarField; 3],
-    alphas: Alphas<<G as AffineCurve>::ScalarField>,
+pub(crate) struct FoldingInstance<const N: usize> {
+    commitments: [Curve; N],
+    challenges: [Fp; 3],
+    alphas: Alphas,
 }
 
-impl<const N: usize, G: CommitmentCurve> Instance<G> for FoldingInstance<N, G> {
-    fn combine(a: Self, b: Self, challenge: <G as AffineCurve>::ScalarField) -> Self {
+impl<const N: usize> Instance<Curve> for FoldingInstance<N> {
+    fn combine(a: Self, b: Self, challenge: Fp) -> Self {
         FoldingInstance {
             commitments: array::from_fn(|i| {
                 a.commitments[i] + b.commitments[i].mul(challenge).into_affine()
@@ -86,5 +45,20 @@ impl<const N: usize, G: CommitmentCurve> Instance<G> for FoldingInstance<N, G> {
             ],
             alphas: Alphas::combine(a.alphas, b.alphas, challenge),
         }
+    }
+}
+
+pub(crate) struct FoldingWitness<const N: usize> {
+    witness: [Evaluations<Fp, Radix2EvaluationDomain<Fp>>; N],
+}
+
+impl<const N: usize> Witness<Curve> for FoldingWitness<N> {
+    fn combine(mut a: Self, b: Self, challenge: Fp) -> Self {
+        for (a, b) in a.witness.iter_mut().zip(b.witness) {
+            for (a, b) in a.evals.iter_mut().zip(b.evals) {
+                *a += challenge * b;
+            }
+        }
+        a
     }
 }
