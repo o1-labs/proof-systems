0b6e78ecca298b812fbe881bf911a75b968df4b3
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-03-20 15:31:15 +0000

    [#1793] Fix the total number of columns

--- msm/src/fec/columns.rs
+++ msm/src/fec/columns.rs
@@ -1,4 +1,4 @@
 use crate::N_LIMBS;
 
 /// Number of columns in the FEC circuits.
-pub const FEC_N_COLUMNS: usize = 34 * N_LIMBS + 42 * N_LIMBS; // 42 LIMBS is for debugging
+pub const FEC_N_COLUMNS: usize = 12 * N_LIMBS + 29;
