ca2e5f08f9bc820540d800defc07003721d9482d
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-03-20 16:01:20 +0000

    [#1792] Rename serialization columns

--- msm/src/serialization/column.rs
+++ msm/src/serialization/column.rs
@@ -7,16 +7,15 @@ use crate::{
 /// Total number of columns in the serialization circuit
 pub const SER_N_COLUMNS: usize = 6 * N_LIMBS + N_INTERMEDIATE_LIMBS + 9;
 
-/// Column used by the serialization subcircuit
-/// It is not used yet.
+/// Columns used by the serialization subcircuit.
 pub enum SerializationColumn {
-    /// 3 88 bits inputs. For the row #i this represents the IPA challenge xi_{log(i)}.
-    KimchiLimb(usize),
-    /// N_INTERMEDIATE_LIMBS intermediate values, 4 bits long.
-    IntermediateLimb(usize),
-    /// N_LIMBS values, representing the limbs used by the MSM
-    MSMLimb(usize),
-    /// Previous C_j, this one is looked up. For the row i, the expected
+    /// 3 88-bit inputs. For the row #i this represents the IPA challenge xi_{log(i)}.
+    ChalKimchi(usize),
+    /// N_INTERMEDIATE_LIMBS intermediate values, 4 bits long. Represent parts of the IPA challenge.
+    ChalIntermediate(usize),
+    /// N_LIMBS values, representing the converted IPA challenge.
+    ChalConverted(usize),
+    /// Previous coefficient C_j, this one is looked up. For the row i, the expected
     /// value is (C_i >> 1).
     CoeffInput(usize),
     /// Trusted (for range) foreign field modulus, in 4 big limbs.
@@ -32,15 +31,15 @@ pub enum SerializationColumn {
 impl ColumnIndexer for SerializationColumn {
     fn ix_to_column(self) -> Column {
         match self {
-            Self::KimchiLimb(j) => {
+            Self::ChalKimchi(j) => {
                 assert!(j < 3);
                 Column::X(j)
             }
-            Self::IntermediateLimb(j) => {
+            Self::ChalIntermediate(j) => {
                 assert!(j < N_INTERMEDIATE_LIMBS);
                 Column::X(3 + j)
             }
-            Self::MSMLimb(j) => {
+            Self::ChalConverted(j) => {
                 assert!(j < N_LIMBS);
                 Column::X(N_INTERMEDIATE_LIMBS + 3 + j)
             }
