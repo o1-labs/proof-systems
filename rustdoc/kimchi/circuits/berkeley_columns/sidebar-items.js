c72b30dc4c4b72ae49fabe56c37e0125be648caa
mrmr1993 <mrmr1993@users.noreply.github.com>
2024-04-08 23:01:51 +0000

    deploy: 23e3efaac5e5ca807d4da480ce83ac7d4028a77a

--- rustdoc/kimchi/circuits/berkeley_columns/sidebar-items.js
+++ rustdoc/kimchi/circuits/berkeley_columns/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Column"]};
+window.SIDEBAR_ITEMS = {"enum":["Column"],"fn":["coeff","constant","index","witness","witness_curr","witness_next"],"struct":["Environment","LookupEnvironment"],"type":["E"]};
