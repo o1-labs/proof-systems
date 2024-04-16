7e0b85f645e51684162d5d757ac45530f834b046
volhovm <volhovm@users.noreply.github.com>
2024-04-16 13:17:04 +0000

    deploy: 4fb098a54945a992be4a12595123cb113fd50b59

--- rustdoc/type.impl/ark_bn254/fields/fr/type.Fr.js
+++ rustdoc/type.impl/ark_bn254/fields/fr/type.Fr.js
@@ -1,4 +1,3 @@
 (function() {var type_impls = {
-"kimchi_msm":[],
-"kimchi_optimism":[]
+"kimchi_msm":[]
 };if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
