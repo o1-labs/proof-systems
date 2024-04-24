e589d524b3572371ea0d3adb5987eb3328dc503c
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-04-24 11:33:24 +0100

    [#2060] [#2037] Add composition for interpreters/gadgets

--- msm/src/circuit_design/mod.rs
+++ msm/src/circuit_design/mod.rs
@@ -1,10 +1,12 @@
 pub mod capabilities;
+pub mod composition;
 pub mod constraints;
 pub mod witness;
 
 // Reexport main types
 pub use crate::circuit_design::{
     capabilities::{ColAccessCap, ColWriteCap, LookupCap},
+    composition::{MPrism, SubEnvColumn, SubEnvLookup},
     constraints::ConstraintBuilderEnv,
     witness::WitnessBuilderEnv,
 };
