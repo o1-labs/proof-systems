2ba12a0833cdd79a6ee38af67e969a11a3f9fb3b
querolita <querolita@users.noreply.github.com>
2024-04-25 10:43:38 +0000

    deploy: 1e5f63e0e79e4fbfcba017f893302fa633e4c144

--- rustdoc/trait.impl/kimchi/circuits/expr/trait.FormattedOutput.js
+++ rustdoc/trait.impl/kimchi/circuits/expr/trait.FormattedOutput.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "kimchi":[],
-"kimchi_msm":[["impl <a class=\"trait\" href=\"kimchi/circuits/expr/trait.FormattedOutput.html\" title=\"trait kimchi::circuits::expr::FormattedOutput\">FormattedOutput</a> for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>"]]
+"kimchi_msm":[["impl FormattedOutput for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
