41f41c88b3ce47b21648f96a9816f6d73609a65e
dannywillems <dannywillems@users.noreply.github.com>
2024-04-29 13:42:05 +0000

    deploy: d51d42e999dfd62de4e488d0d8f9de992d9ce54b

--- rustdoc/trait.impl/mina_poseidon/constants/trait.SpongeConstants.js
+++ rustdoc/trait.impl/mina_poseidon/constants/trait.SpongeConstants.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
+"ivc":[["impl SpongeConstants for <a class=\"struct\" href=\"ivc/poseidon/params/struct.PlonkSpongeConstantsIVC.html\" title=\"struct ivc::poseidon::params::PlonkSpongeConstantsIVC\">PlonkSpongeConstantsIVC</a>"]],
 "mina_poseidon":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
