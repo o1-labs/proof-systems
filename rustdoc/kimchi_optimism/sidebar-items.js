37caa4d13890060ce905330f003faea0b659cfb5
dannywillems <dannywillems@users.noreply.github.com>
2024-05-06 14:01:12 +0000

    deploy: c16fa5c6e70839d280abff34e6f5afc840b7cb0f

--- rustdoc/kimchi_optimism/sidebar-items.js
+++ rustdoc/kimchi_optimism/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["DOMAIN_SIZE"],"mod":["cannon","cannon_cli","folding","keccak","lookups","mips","preimage_oracle","proof","ramlookup","trace"]};
+window.SIDEBAR_ITEMS = {"constant":["DOMAIN_SIZE"],"mod":["cannon","cannon_cli","folding","keccak","lookups","mips","preimage_oracle","proof","ramlookup","trace"],"type":["BaseSponge","Curve","Fp","OpeningProof","ScalarSponge","SpongeParams"]};
