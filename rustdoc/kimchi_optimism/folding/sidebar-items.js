22083926d70ec18f46fdbf26e6d2f002b8f93bae
dannywillems <dannywillems@users.noreply.github.com>
2024-05-06 18:55:42 +0000

    deploy: 81243446a6d22a8e1a2aa87b3dcc79ab84541595

--- rustdoc/kimchi_optimism/folding/sidebar-items.js
+++ rustdoc/kimchi_optimism/folding/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Alphas","Challenge"],"struct":["BaseSponge","ChallengeIter","FoldingEnvironment","FoldingInstance","FoldingWitness"]};
+window.SIDEBAR_ITEMS = {"enum":["Challenge"],"struct":["BaseSponge","ChallengeIter","FoldingEnvironment","FoldingInstance","FoldingWitness"]};
