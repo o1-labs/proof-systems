9fa0288667ad2b5a17045ae4236a013b77e7f837
dannywillems <dannywillems@users.noreply.github.com>
2024-05-09 14:48:04 +0000

    deploy: bc321ce15206e29fcb3d2d27dde88c4527b4ce36

--- rustdoc/implementors/folding/instance_witness/trait.Instance.js
+++ rustdoc/implementors/folding/instance_witness/trait.Instance.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"o1vm":[["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.usize.html\">usize</a>, G: CommitmentCurve&gt; Instance&lt;G&gt; for <a class=\"struct\" href=\"o1vm/folding/struct.FoldingInstance.html\" title=\"struct o1vm::folding::FoldingInstance\">FoldingInstance</a>&lt;N, G&gt;"]]
+"kimchi_optimism":[["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.usize.html\">usize</a>, G: CommitmentCurve&gt; Instance&lt;G&gt; for <a class=\"struct\" href=\"kimchi_optimism/folding/struct.FoldingInstance.html\" title=\"struct kimchi_optimism::folding::FoldingInstance\">FoldingInstance</a>&lt;N, G&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rustdoc/implementors/kimchi_optimism/trace/trait.Tracer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kimchi_optimism":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
