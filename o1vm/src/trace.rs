acc5a57ecca033df56b53f1f0d899fc6d0e5099f
querolita <anais.querol@gmail.com>
2024-05-14 18:45:52 +0200

    simplify traces interfaces

--- o1vm/src/trace.rs
+++ o1vm/src/trace.rs
@@ -11,14 +11,15 @@ use crate::{
     E,
 };
 use ark_ff::{One, Zero};
-use ark_poly::{EvaluationDomain, Evaluations, Radix2EvaluationDomain};
-use folding::{Alphas, FoldingConfig};
+use ark_poly::{Evaluations, Radix2EvaluationDomain as D};
+use folding::{expressions::FoldingCompatibleExpr, Alphas, FoldingConfig};
 use itertools::Itertools;
-use kimchi_msm::witness::Witness;
+use kimchi::circuits::expr::ChallengeTerm;
+use kimchi_msm::{columns::Column, witness::Witness};
 use mina_poseidon::sponge::FqSponge;
-use poly_commitment::{PolyComm, SRS as _};
+use poly_commitment::{commitment::absorb_commitment, PolyComm, SRS as _};
 use rayon::{iter::ParallelIterator, prelude::IntoParallelIterator};
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, ops::Index};
 
 /// Returns the index of the witness column in the trace.
 pub trait Indexer {
@@ -65,8 +66,7 @@ impl<const N: usize, C: FoldingConfig> ProvableTrace for Trace<N, C> {
 /// - `F`: the type of the witness data.
 #[allow(clippy::type_complexity)]
 #[derive(Clone)]
-pub struct DecomposedTrace<const N: usize, const N_REL: usize, const N_SEL: usize, C: FoldingConfig>
-{
+pub struct DecomposedTrace<const N: usize, C: FoldingConfig> {
     /// The domain size of the circuit (should coincide with that of the traces)
     pub domain_size: usize,
     /// The traces are indexed by the selector
@@ -76,17 +76,23 @@ pub struct DecomposedTrace<const N: usize, const N_REL: usize, const N_SEL: usiz
     pub trace: BTreeMap<C::Selector, Trace<N, C>>,
 }
 
+// Implementation of [Index] using [C::Selector] as the index for [DecomposedTrace] to access the trace directly.
+impl<const N: usize, C: FoldingConfig> Index<C::Selector> for DecomposedTrace<N, C> {
+    type Output = Trace<N, C>;
+
+    fn index(&self, index: C::Selector) -> &Self::Output {
+        &self.trace[&index]
+    }
+}
+
 // Any decomposable trace is provable.
-impl<const N: usize, const N_REL: usize, const N_SEL: usize, C: FoldingConfig> ProvableTrace
-    for DecomposedTrace<N, N_REL, N_SEL, C>
-{
+impl<const N: usize, C: FoldingConfig> ProvableTrace for DecomposedTrace<N, C> {
     fn domain_size(&self) -> usize {
         self.domain_size
     }
 }
 
-impl<const N: usize, const N_REL: usize, const N_SEL: usize, C: FoldingConfig>
-    DecomposedTrace<N, N_REL, N_SEL, C>
+impl<const N: usize, C: FoldingConfig> DecomposedTrace<N, C>
 where
     C::Selector: Indexer,
 {
@@ -96,7 +102,7 @@ where
     /// selector columns are only instantiated at the very end, so their length
     /// could be zero most times.
     pub fn number_of_rows(&self, opcode: C::Selector) -> usize {
-        self.trace[&opcode].witness.cols[0].len()
+        self[opcode].witness.cols[0].len()
     }
 
     /// Returns a boolean indicating whether the witness for the given selector
@@ -119,7 +125,11 @@ where
     }
 
     /// Sets the selector column to all ones, and the rest to all zeros
-    pub fn set_selector_column(&mut self, selector: C::Selector, number_of_rows: usize) {
+    pub fn set_selector_column<const N_REL: usize>(
+        &mut self,
+        selector: C::Selector,
+        number_of_rows: usize,
+    ) {
         (N_REL..N).for_each(|i| {
             if i == selector.ix() {
                 self.trace.get_mut(&selector).unwrap().witness.cols[i]
@@ -137,7 +147,7 @@ where
 /// instance and a folding witness.
 /// It is specialized for the [DecomposedTrace] struct for now and is expected
 /// to fold individual instructions, selected with a specific [C::Selector].
-pub(crate) trait Foldable<const N: usize, C: FoldingConfig, Sponge> {
+pub trait Foldable<const N: usize, C: FoldingConfig, Sponge> {
     /// Returns the witness for the given selector as a folding witness and
     /// folding instance pair.
     /// Note that this function will also absorb all commitments to the columns
@@ -145,33 +155,38 @@ pub(crate) trait Foldable<const N: usize, C: FoldingConfig, Sponge> {
     fn to_folding_pair(
         &self,
         selector: C::Selector,
+        fq_sponge: &mut Sponge,
+        domain: D<ScalarField<C>>,
         srs: &poly_commitment::srs::SRS<C::Curve>,
-        sponge: &mut Sponge,
     ) -> (
         FoldingInstance<N, C::Curve>,
         FoldingWitness<N, ScalarField<C>>,
     );
+
+    /// Returns a map of constraints that are compatible with folding for each selector
+    fn folding_constraints(&self) -> BTreeMap<C::Selector, Vec<FoldingCompatibleExpr<C>>>;
 }
 
 /// Implement the trait Foldable for the structure [DecomposedTrace]
-impl<const N: usize, const N_REL: usize, const N_SEL: usize, C: FoldingConfig, Sponge>
-    Foldable<N, C, Sponge> for DecomposedTrace<N, N_REL, N_SEL, C>
+impl<const N: usize, C: FoldingConfig<Column = Column>, Sponge> Foldable<N, C, Sponge>
+    for DecomposedTrace<N, C>
 where
     C::Selector: Indexer,
     Sponge: FqSponge<BaseField<C>, C::Curve, ScalarField<C>>,
+    <C as FoldingConfig>::Challenge: From<ChallengeTerm>,
 {
     fn to_folding_pair(
         &self,
         selector: C::Selector,
-        srs: &poly_commitment::srs::SRS<C::Curve>,
         fq_sponge: &mut Sponge,
+        domain: D<ScalarField<C>>,
+        srs: &poly_commitment::srs::SRS<C::Curve>,
     ) -> (
         FoldingInstance<N, C::Curve>,
         FoldingWitness<N, ScalarField<C>>,
     ) {
-        let domain = Radix2EvaluationDomain::<ScalarField<C>>::new(self.domain_size).unwrap();
         let folding_witness = FoldingWitness {
-            witness: (&self.trace[&selector].witness)
+            witness: (&self[selector].witness)
                 .into_par_iter()
                 .map(|w| Evaluations::from_vec_and_domain(w.to_vec(), domain))
                 .collect(),
@@ -181,6 +196,12 @@ where
             .into_par_iter()
             .map(|w| srs.commit_evaluations_non_hiding(domain, w))
             .collect();
+
+        // Absorbing commitments
+        (&commitments)
+            .into_iter()
+            .for_each(|c| absorb_commitment(fq_sponge, c));
+
         let commitments: [C::Curve; N] = commitments
             .into_iter()
             .map(|c| c.elems[0])
@@ -204,57 +225,117 @@ where
 
         (instance, folding_witness)
     }
+
+    fn folding_constraints(&self) -> BTreeMap<C::Selector, Vec<FoldingCompatibleExpr<C>>> {
+        self.trace
+            .iter()
+            .map(|(k, instr)| {
+                (
+                    *k,
+                    instr
+                        .constraints
+                        .iter()
+                        .map(|x| FoldingCompatibleExpr::from(x.clone()))
+                        .collect(),
+                )
+            })
+            .collect()
+    }
 }
 
 /// Tracer builds traces for some program executions.
-/// The constant type `N` is defined as the maximum number of columns the trace
-/// can use per row.
 /// The constant type `N_REL` is defined as the maximum number of relation
 /// columns the trace can use per row.
-/// The constant type `N_SEL` is defined as the number of selector columns the
-/// trace can use per row.
-/// The type `Selector` encodes the information of the kind of information the
-/// trace encodes. Examples:
+/// The type `C` encodes the folding configuration, from which the selector,
+/// and scalar field are derived. Examples of selectors are:
 /// - For Keccak, `Step` encodes the row being performed at a time: round,
 /// squeeze, padding, etc...
 /// - For MIPS, `Instruction` encodes the CPU instruction being executed: add,
 /// sub, load, store, etc...
-/// The type parameter `F` is the type the data points in the trace are encoded
-/// into. It can be a field or a native type (u64).
-pub trait DecomposableTracer<
-    const N: usize,
-    const N_REL: usize,
-    const N_SEL: usize,
-    C: FoldingConfig,
-    Env,
->
-{
-    /// Create a new circuit
-    fn new(domain_size: usize, env: &mut Env) -> Self;
+pub trait Tracer<const N_REL: usize, C: FoldingConfig, Env> {
+    type Selector;
+
+    /// Initialize a new trace with the given domain size, selector, and environment.
+    fn init(domain_size: usize, selector: C::Selector, env: &mut Env) -> Self;
 
     /// Add a witness row to the circuit (only for relation columns)
-    fn push_row(&mut self, opcode: C::Selector, row: &[ScalarField<C>; N_REL]);
+    fn push_row(&mut self, selector: Self::Selector, row: &[ScalarField<C>; N_REL]);
 
     /// Pad the rows of one opcode with the given row until
     /// reaching the domain size if needed.
     /// Returns the number of rows that were added.
     /// It does not add selector columns.
-    fn pad_with_row(&mut self, opcode: C::Selector, row: &[ScalarField<C>; N_REL]) -> usize;
+    fn pad_with_row(&mut self, selector: Self::Selector, row: &[ScalarField<C>; N_REL]) -> usize;
 
     /// Pads the rows of one opcode with zero rows until
     /// reaching the domain size if needed.
     /// Returns the number of rows that were added.
     /// It does not add selector columns.
-    fn pad_with_zeros(&mut self, opcode: C::Selector) -> usize;
+    fn pad_with_zeros(&mut self, selector: Self::Selector) -> usize;
 
     /// Pad the rows of one opcode with the first row until
     /// reaching the domain size if needed.
     /// It only tries to pad witnesses which are non empty.
     /// Returns the number of rows that were added.
     /// It does not add selector columns.
-    fn pad_dummy(&mut self, opcode: C::Selector) -> usize;
+    /// - Use `None` for single traces
+    /// - Use `Some(selector)` for multi traces
+    fn pad_dummy(&mut self, selector: Self::Selector) -> usize;
+}
+
+/// DecomposableTracer builds traces for some program executions.
+/// The constant type `N_REL` is defined as the maximum number of relation
+/// columns the trace can use per row.
+/// The type `C` encodes the folding configuration, from which the selector,
+/// and scalar field are derived. Examples of selectors are:
+/// - For Keccak, `Step` encodes the row being performed at a time: round,
+/// squeeze, padding, etc...
+/// - For MIPS, `Instruction` encodes the CPU instruction being executed: add,
+/// sub, load, store, etc...
+pub trait DecomposableTracer<Env> {
+    /// Create a new decomposable trace with the given domain size, and environment.
+    fn new(domain_size: usize, env: &mut Env) -> Self;
 
     /// Pads the rows of the witnesses until reaching the domain size using the first
     /// row repeatedly. It does not add selector columns.
     fn pad_witnesses(&mut self);
+}
+
+/// Generic implementation of the [Tracer] trait for the [DecomposedTrace] struct.
+/// It requires the [DecomposedTrace] to implement the [DecomposableTracer] trait,
+/// and the [Trace] struct to implement the [Tracer] trait with Selector set to (),
+/// and the [C::Selector] to implement the [Indexer] trait.
+impl<const N: usize, const N_REL: usize, C: FoldingConfig, Env> Tracer<N_REL, C, Env>
+    for DecomposedTrace<N, C>
+where
+    DecomposedTrace<N, C>: DecomposableTracer<Env>,
+    Trace<N, C>: Tracer<N_REL, C, Env, Selector = ()>,
+    <C as FoldingConfig>::Selector: Indexer,
+{
+    type Selector = C::Selector;
+
+    fn init(domain_size: usize, _selector: C::Selector, env: &mut Env) -> Self {
+        <Self as DecomposableTracer<Env>>::new(domain_size, env)
+    }
+
+    fn push_row(&mut self, selector: Self::Selector, row: &[ScalarField<C>; N_REL]) {
+        self.trace.get_mut(&selector).unwrap().push_row((), row);
+    }
+
+    fn pad_with_row(&mut self, selector: Self::Selector, row: &[ScalarField<C>; N_REL]) -> usize {
+        self.trace.get_mut(&selector).unwrap().pad_with_row((), row)
+    }
+
+    fn pad_with_zeros(&mut self, selector: Self::Selector) -> usize {
+        self.trace.get_mut(&selector).unwrap().pad_with_zeros(())
+    }
+
+    fn pad_dummy(&mut self, selector: Self::Selector) -> usize {
+        // We only want to pad non-empty witnesses.
+        if !self.in_circuit(selector) {
+            0
+        } else {
+            self.trace.get_mut(&selector).unwrap().pad_dummy(())
+        }
+    }
 }
