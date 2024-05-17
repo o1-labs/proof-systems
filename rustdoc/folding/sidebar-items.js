99796d9382d14fd396e32d1d6c67c628030975ec
dannywillems <dannywillems@users.noreply.github.com>
2024-05-17 17:28:13 +0000

    deploy: dc43c7d3606621ac24bda194ff4097fd51123cda

--- rustdoc/folding/sidebar-items.js
+++ rustdoc/folding/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Alphas","Side"],"mod":["checker","columns","decomposable_folding","expressions","quadraticization"],"struct":["FoldingScheme","RelaxedInstance","RelaxedWitness"],"trait":["FoldingConfig","FoldingEnv","Instance","Witness"]};
+window.SIDEBAR_ITEMS = {"enum":["Alphas","Side"],"mod":["checker","columns","decomposable_folding","expressions","quadraticization"],"struct":["FoldingOutput","FoldingScheme","RelaxedInstance","RelaxedWitness"],"trait":["FoldingConfig","FoldingEnv","Instance","Witness"]};
