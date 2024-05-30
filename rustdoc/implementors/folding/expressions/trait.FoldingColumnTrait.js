2764d0d2903f507b9e7f91029607df715d850974
dannywillems <dannywillems@users.noreply.github.com>
2024-05-30 14:30:55 +0000

    deploy: b60e6c2735e2aa33947d0c6c5f40fb7fd7ad8548

--- rustdoc/implementors/folding/expressions/trait.FoldingColumnTrait.js
+++ rustdoc/implementors/folding/expressions/trait.FoldingColumnTrait.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "folding":[],
 "kimchi_msm":[["impl FoldingColumnTrait for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>"]],
-"o1vm":[["impl FoldingColumnTrait for <a class=\"enum\" href=\"o1vm/keccak/column/enum.ColumnAlias.html\" title=\"enum o1vm::keccak::column::ColumnAlias\">KeccakColumn</a>"],["impl FoldingColumnTrait for <a class=\"enum\" href=\"o1vm/mips/column/enum.ColumnAlias.html\" title=\"enum o1vm::mips::column::ColumnAlias\">MIPSColumn</a>"]]
+"o1vm":[["impl FoldingColumnTrait for <a class=\"enum\" href=\"o1vm/mips/column/enum.ColumnAlias.html\" title=\"enum o1vm::mips::column::ColumnAlias\">MIPSColumn</a>"],["impl FoldingColumnTrait for <a class=\"enum\" href=\"o1vm/keccak/column/enum.ColumnAlias.html\" title=\"enum o1vm::keccak::column::ColumnAlias\">KeccakColumn</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
