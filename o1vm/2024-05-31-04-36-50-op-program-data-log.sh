d94d44489402134cbb05b58a87564f88e5207e11
querolita <anais.querol@gmail.com>
2024-06-03 16:23:55 +0200

    reorganize tests in modules and fix opcode of load32

---
+++ o1vm/2024-05-31-04-36-50-op-program-data-log.sh
@@ -0,0 +1,5 @@
+export L2_HEAD=0x2b40c8aab3fa9ddd0f0c71612c6ebc0740ddc258bb42638393d082522fc28cab
+export L2_BLOCK_NUMBER=12685435
+export OP_PROGRAM_DATA_DIR=~/WORKSPACE/proof-systems/o1vm/op-program-db-sepolia-2024-05-31-04-36-50
+export L1RPC=https://ethereum-sepolia-rpc.publicnode.com
+export L2RPC=http://162.55.83.132:8545/
