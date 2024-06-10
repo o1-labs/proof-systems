9e43c02dc9ce53144cc39f820126cb8fbaa23abd
querolita <anais.querol@gmail.com>
2024-06-10 19:56:06 +0200

    fix call to the oracle to use the effective address including the offset (otherwise the word alignment was not being respected)

--- o1vm/src/mips/interpreter.rs
+++ o1vm/src/mips/interpreter.rs
@@ -900,7 +900,8 @@ pub trait InterpreterEnv {
     fn report_exit(&mut self, exit_code: &Self::Variable);
 
     /// Request the preimage oracle for `len` bytes and store the bytes starting
-    /// from `addr`, and it returns the number of bytes actually read.
+    /// from the effective address `addr` (already contains offset), and it
+    /// returns the number of bytes actually read.
     /// The number of bytes actually read will be set into `pos`.
     /// The first 8 bytes will be the length of the preimage, encoded as an
     /// unsigned 64bits, and the rest will be the preimage.
@@ -1094,9 +1095,21 @@ pub fn interpret_rtype<Env: InterpreterEnv>(env: &mut Env, instr: RTypeInstructi
             let length = env.read_register(&Env::constant(6));
             let preimage_offset =
                 env.read_register(&Env::constant(REGISTER_PREIMAGE_OFFSET as u32));
+
+            // Calculate the effective address to start storing the oracle bytes
+            let effective_addr = {
+                let res_scratch = env.alloc_scratch();
+                let overflow_scratch = env.alloc_scratch();
+                let (res, _overflow) = unsafe {
+                    env.add_witness(&addr, &preimage_offset, res_scratch, overflow_scratch)
+                };
+                // FIXME: Requires a range check
+                res
+            };
+
             let read_length = {
                 let pos = env.alloc_scratch();
-                env.request_preimage_write(&addr, &length, pos)
+                env.request_preimage_write(&effective_addr, &length, pos)
             };
             env.write_register(
                 &Env::constant(REGISTER_PREIMAGE_OFFSET as u32),
