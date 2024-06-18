1cbebb0be95a20c4abbaa714471e53bd733a7f5b
Danny Willems <be.danny.willems@gmail.com>
2024-06-18 18:20:37 +0200

    IVC: assert the domain is large enough to handle the IVC circuit
    We should move this into an appropriate environment later

--- ivc/tests/add.rs
+++ ivc/tests/add.rs
@@ -88,7 +88,7 @@ pub fn interpreter_simple_add<
 #[test]
 pub fn test_simple_add() {
     let mut rng = o1_utils::tests::make_test_rng(None);
-    let domain_size: usize = 1 << 5;
+    let domain_size: usize = 1 << 15;
     let domain = EvaluationDomains::<Fp>::create(domain_size).unwrap();
 
     let mut srs = SRS::<Curve>::create(domain_size);
