f5eaf69a8ea2c7053db03e37ec69968699540e22
dannywillems <dannywillems@users.noreply.github.com>
2024-06-26 18:16:42 +0000

    deploy: ab147a2bcd3289423be03f8c908455b2a722df16

--- rustdoc/ivc/ivc/columns/sidebar-items.js
+++ rustdoc/ivc/ivc/columns/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["IVC_NB_TOTAL_FIXED_SELECTORS","N_BLOCKS"],"enum":["IVCColumn"],"fn":["block_height"],"struct":["IVCFECLens","IVCHashLens"]};
+window.SIDEBAR_ITEMS = {"constant":["IVC_NB_TOTAL_FIXED_SELECTORS","N_BLOCKS"],"enum":["IVCColumn"],"fn":["block_height","total_height"],"struct":["IVCFECLens","IVCHashLens"]};
