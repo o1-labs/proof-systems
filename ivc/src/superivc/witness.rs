bac1b0f4b42c071807002b4b5ffec31511484d9a
Danny Willems <be.danny.willems@gmail.com>
2024-07-13 08:57:31 +0200

    Define environments

--- ivc/src/superivc/witness.rs
+++ ivc/src/superivc/witness.rs
@@ -1,1 +1,12 @@
+use std::collections::BTreeMap;
 
+use folding::{FoldingConfig, FoldingOutput};
+use mina_poseidon::poseidon::SpongeState;
+
+pub struct Env<C: FoldingConfig> {
+    /// Accumulators, indexed by natural numbers, but it should be an
+    /// instruction or an enum representing a list of accepting "functions".
+    pub accumulators: BTreeMap<usize, FoldingOutput<C>>,
+
+    pub sponges: BTreeMap<usize, SpongeState>,
+}
