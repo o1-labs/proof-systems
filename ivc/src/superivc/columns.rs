fd5652b5f7bc163937545b8129f42a71f239eebd
Danny Willems <be.danny.willems@gmail.com>
2024-07-13 11:14:22 +0200

    SuperIVC: comment about the block computing the hash commitments

--- ivc/src/superivc/columns.rs
+++ ivc/src/superivc/columns.rs
@@ -3,6 +3,10 @@ pub enum SuperIVCColumn {
     /// Selector to compute the ECC addition
     BlockAppECCAdd,
     /// Selector to compute the hash of the commitments (APP + IVC)
+    /// The block will also be responsible to compute the "output" of the
+    /// selected application "f_j". The block will use the state of the sponge
+    /// for the specific app, and the final state of this sponge at the end of
+    /// the fold will be used as a public input for the next instance.
     BlockAppHashCommitments,
     /// Selector to split the foreign field elements into chunks of 150 bits and
     /// 75 bits that can be used for the EC ADD
