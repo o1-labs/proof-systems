d8fffeea2801f8593a0db54b207e5444d3c4de1c
Danny Willems <be.danny.willems@gmail.com>
2024-07-13 11:07:08 +0200

    Split the witness environment into IVC and App accumulators

--- ivc/src/superivc/witness.rs
+++ ivc/src/superivc/witness.rs
@@ -8,10 +8,20 @@ use mina_poseidon::poseidon::SpongeState;
 /// instance k, including the sponges.
 /// For each fold instance k, the prover will pick one accumulator to merge with
 /// the current instance.
-pub struct Env<C: FoldingConfig> {
-    /// Accumulators, indexed by natural numbers, but it should be an
+/// We split the accumulators between the ones used by the applications and the one
+/// for the IVC.
+/// It is parametrized by:
+/// - FCAPP: a folding configuration specific for the application
+/// - FCIVC: a folding configuration specific for the IVC circuit only
+/// - N_APP_COL: the number of columns used by the applications. It does suppose
+/// all applications use the same number of columns
+pub struct Env<FCApp: FoldingConfig, FCIVC: FoldingConfig, const N_APP_COL: usize> {
+    /// IVC accumulators, indexed by natural numbers, but it should be an
     /// instruction or an enum representing a list of accepting "functions".
-    pub accumulators: BTreeMap<usize, FoldingOutput<C>>,
+    pub ivc_accumulators: BTreeMap<usize, FoldingOutput<FCIVC>>,
+
+    /// Accumulators of the applications
+    pub app_accumulators: BTreeMap<usize, FoldingOutput<FCApp>>,
 
     /// Sponges, index by natural numbers. The natural numbers should be the
     /// instruction.
