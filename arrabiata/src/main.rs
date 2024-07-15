6d6b834ff0f870111e95f459342eb2d305c82ead
Danny Willems <be.danny.willems@gmail.com>
2024-07-15 21:18:14 +0200

    Arrabiata/main: run interpreter N times

--- arrabiata/src/main.rs
+++ arrabiata/src/main.rs
@@ -1,4 +1,4 @@
-use arrabiata::witness::Env;
+use arrabiata::{interpreter, witness::Env};
 use mina_curves::pasta::{Fp, Fq, Pallas, Vesta};
 // FIXME: use other parameters, like one with the partial rounds
 use mina_poseidon::constants::PlonkSpongeConstantsKimchi;
@@ -10,14 +10,23 @@ pub fn main() {
     let cmd = clap::Command::new("cargo")
         .bin_name("cargo")
         .subcommand_required(true)
-        .subcommand(clap::Command::new("square-root").arg(arg_n));
+        .subcommand(
+            clap::Command::new("square-root")
+                .arg(arg_n)
+                .arg_required_else_help(true),
+        );
     let matches = cmd.get_matches();
     let matches = match matches.subcommand() {
         Some(("square-root", matches)) => matches,
         _ => unreachable!("clap should ensure we don't get here"),
     };
-    let n_iteration = matches.get_one::<u64>("n");
-    println!("{n_iteration:?}");
+    let n_iteration = matches.get_one::<u64>("n").unwrap();
 
-    let _env = Env::<Fp, Fq, PlonkSpongeConstantsKimchi, Vesta, Pallas>::new();
+    let mut env = Env::<Fp, Fq, PlonkSpongeConstantsKimchi, Vesta, Pallas>::new();
+
+    while env.current_iteration < *n_iteration {
+        println!("Run iteration: {}", env.current_iteration);
+        interpreter::run(&mut env);
+        env.current_iteration += 1;
+    }
 }
