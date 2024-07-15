20ec0791b708871927c89f233fdc037d4e5ce4a1
Danny Willems <be.danny.willems@gmail.com>
2024-07-15 19:03:15 +0200

    Arrabiata: alias for expressions

--- arrabiata/src/columns.rs
+++ arrabiata/src/columns.rs
@@ -1,4 +1,8 @@
+use kimchi::circuits::expr::{ConstantExpr, Expr};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Column {
     X(usize),
 }
+
+pub type E<Fp> = Expr<ConstantExpr<Fp>, Column>;
--- arrabiata/src/constraints.rs
+++ arrabiata/src/constraints.rs
@@ -1,15 +1,20 @@
 use ark_ff::Field;
 
-use kimchi::circuits::expr::{ConstantExpr, Expr};
+use crate::columns::E;
 
 use super::{columns::Column, interpreter::InterpreterEnv};
 
 pub struct Env<Fp: Field> {
-    pub constraints: Vec<Expr<ConstantExpr<Fp>, Column>>,
+    pub constraints: Vec<E<Fp>>,
 }
 
+/// An environment to build constraints.
+/// The constraint environment is mostly useful when we want to perform a Nova
+/// proof.
+/// The constraint environment must be instantiated only once, at the last step
+/// of the computation.
 impl<Fp: Field> InterpreterEnv for Env<Fp> {
     type Position = Column;
 
-    type Variable = Expr<ConstantExpr<Fp>, Column>;
+    type Variable = E<Fp>;
 }
