968b524ba8e4171c0e1eb3690f374838d3d6fb0e
Danny Willems <be.danny.willems@gmail.com>
2024-07-18 17:58:26 +0200

    Arrabiata/columns: allow formatting the cols

--- arrabiata/src/columns.rs
+++ arrabiata/src/columns.rs
@@ -1,4 +1,6 @@
-use kimchi::circuits::expr::{ConstantExpr, Expr};
+use std::collections::HashMap;
+
+use kimchi::circuits::expr::{CacheId, ConstantExpr, Expr, FormattedOutput};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Column {
@@ -6,3 +8,28 @@ pub enum Column {
 }
 
 pub type E<Fp> = Expr<ConstantExpr<Fp>, Column>;
+
+// Code to allow for pretty printing of the expressions
+impl FormattedOutput for Column {
+    fn latex(&self, _cache: &mut HashMap<CacheId, Self>) -> String {
+        match self {
+            Column::X(i) => format!("x_{{{i}}}"),
+        }
+    }
+
+    fn text(&self, _cache: &mut HashMap<CacheId, Self>) -> String {
+        match self {
+            Column::X(i) => format!("x[{i}]"),
+        }
+    }
+
+    fn ocaml(&self, _cache: &mut HashMap<CacheId, Self>) -> String {
+        // FIXME
+        unimplemented!("Not used at the moment")
+    }
+
+    fn is_alpha(&self) -> bool {
+        // FIXME
+        unimplemented!("Not used at the moment")
+    }
+}
