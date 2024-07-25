6801c75d9118b02c248431d52ceea20abde0978e
Danny Willems <be.danny.willems@gmail.com>
2024-07-25 21:55:43 +0200

    Arrabiata/Test: test constrain_boolean uses field reduction

--- arrabiata/tests/utils.rs
+++ arrabiata/tests/utils.rs
@@ -2,6 +2,7 @@ use arrabiata::{interpreter::InterpreterEnv, witness::Env, POSEIDON_STATE_SIZE};
 use mina_curves::pasta::{Fp, Fq, Pallas, Vesta};
 use num_bigint::BigInt;
 use num_integer::Integer;
+use o1_utils::FieldHelpers;
 
 // Testing that modulo on negative numbers gives a positive value.
 // It is extensively used in the witness generation, therefore checking this
@@ -54,4 +55,33 @@ fn test_read_bit_of_folding_combiner() {
     assert_eq!(res, zero_bi);
     let res = unsafe { env.read_bit_of_folding_combiner(pos, 8) };
     assert_eq!(res, zero_bi);
+}
+
+#[test]
+#[should_panic]
+fn test_constrain_boolean_witness_negative_value() {
+    let srs_log2_size = 2;
+    let mut env = {
+        let z0 = BigInt::from(1u64);
+        let sponge_e1: [BigInt; POSEIDON_STATE_SIZE] = std::array::from_fn(|_i| BigInt::from(0u64));
+        Env::<Fp, Fq, Vesta, Pallas>::new(srs_log2_size, z0, sponge_e1.clone(), sponge_e1.clone())
+    };
+
+    env.constrain_boolean(BigInt::from(-42));
+}
+
+#[test]
+fn test_constrain_boolean_witness_positive_and_negative_modulus() {
+    let srs_log2_size = 2;
+    let mut env = {
+        let z0 = BigInt::from(1u64);
+        let sponge_e1: [BigInt; POSEIDON_STATE_SIZE] = std::array::from_fn(|_i| BigInt::from(0u64));
+        Env::<Fp, Fq, Vesta, Pallas>::new(srs_log2_size, z0, sponge_e1.clone(), sponge_e1.clone())
+    };
+
+    let modulus: BigInt = Fp::modulus_biguint().into();
+    env.constrain_boolean(modulus.clone());
+    env.constrain_boolean(modulus.clone() + BigInt::from(1u64));
+    env.constrain_boolean(-modulus.clone());
+    env.constrain_boolean(-modulus.clone() + BigInt::from(1u64));
 }
