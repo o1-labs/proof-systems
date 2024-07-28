21dd97705ae712e527891f24145712597a9bdd0b
mrmr1993 <mr_1993@hotmail.co.uk>
2024-07-28 14:12:24 +0100

    Re-order prover

--- groth16-plus-lookups/src/prover.rs
+++ groth16-plus-lookups/src/prover.rs
@@ -24,39 +24,8 @@ pub fn prove<F: PrimeField, Rng: rand::RngCore, Pair: PairingEngine<Fr = F>>(
         }
         Evaluations::<F, D<F>>::from_vec_and_domain(values, circuit_layout.domain)
     };
-    let a_values = compute_contributions(&circuit_layout.a_contributions);
-    let b_values = compute_contributions(&circuit_layout.b_contributions);
-    let c_values = compute_contributions(&circuit_layout.c_contributions);
 
-    let a_poly = a_values.interpolate();
-    let b_poly = b_values.interpolate();
-
-    let quotient_poly = {
-        let mut a_values_d2 = a_poly.evaluate_over_domain_by_ref(circuit_layout.domain_d2);
-        let b_values_d2 = b_poly.evaluate_over_domain_by_ref(circuit_layout.domain_d2);
-        // TODO: Wasteful
-        let c_values_d2 = c_values
-            .interpolate()
-            .evaluate_over_domain_by_ref(circuit_layout.domain_d2);
-        for (a, (b, c)) in a_values_d2.evals.iter_mut().zip(
-            b_values_d2
-                .evals
-                .into_iter()
-                .zip(c_values_d2.evals.into_iter()),
-        ) {
-            *a *= b;
-            *a -= c;
-        }
-        let (quotient, res) = a_values_d2
-            .interpolate()
-            .divide_by_vanishing_poly(circuit_layout.domain)
-            .unwrap();
-        if !res.is_zero() {
-            panic!("Division wasn't zero :'(");
-        }
-        quotient
-    };
-
+    let a_poly = compute_contributions(&circuit_layout.a_contributions).interpolate();
     let a = {
         let initial = trusted_setup_outputs
             .output_fixed_randomizer
@@ -72,6 +41,8 @@ pub fn prove<F: PrimeField, Rng: rand::RngCore, Pair: PairingEngine<Fr = F>>(
         .into_affine()
             + initial
     };
+
+    let b_poly = compute_contributions(&circuit_layout.b_contributions).interpolate();
     let b = {
         let initial = trusted_setup_outputs.right_fixed_randomizer.1
             + trusted_setup_outputs
@@ -89,6 +60,7 @@ pub fn prove<F: PrimeField, Rng: rand::RngCore, Pair: PairingEngine<Fr = F>>(
         };
         initial + values_commitment
     };
+
     let c = {
         let witness_commitment = {
             let private_witness: Vec<_> = witness[circuit_layout.public_input_size..]
@@ -102,6 +74,32 @@ pub fn prove<F: PrimeField, Rng: rand::RngCore, Pair: PairingEngine<Fr = F>>(
             .into_affine()
         };
         let quotient_commitment = {
+            let quotient_poly = {
+                let mut a_values_d2 = a_poly.evaluate_over_domain_by_ref(circuit_layout.domain_d2);
+                let b_values_d2 = b_poly.evaluate_over_domain_by_ref(circuit_layout.domain_d2);
+                // TODO: Wasteful
+                let c_values_d2 = compute_contributions(&circuit_layout.c_contributions)
+                    .interpolate()
+                    .evaluate_over_domain_by_ref(circuit_layout.domain_d2);
+                for (a, (b, c)) in a_values_d2.evals.iter_mut().zip(
+                    b_values_d2
+                        .evals
+                        .into_iter()
+                        .zip(c_values_d2.evals.into_iter()),
+                ) {
+                    *a *= b;
+                    *a -= c;
+                }
+                let (quotient, res) = a_values_d2
+                    .interpolate()
+                    .divide_by_vanishing_poly(circuit_layout.domain)
+                    .unwrap();
+                if !res.is_zero() {
+                    panic!("Division wasn't zero :'(");
+                }
+                quotient
+            };
+
             let coefficients: Vec<_> = quotient_poly.iter().map(|x| x.into_repr()).collect();
             VariableBaseMSM::multi_scalar_mul(
                 &trusted_setup_outputs.quotient_commitments,
