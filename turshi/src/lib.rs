654e80cd60fad5102ecfa5ff658c65e3b142ee5f
Danny Willems <be.danny.willems@gmail.com>
2024-07-30 00:37:01 +0200

    Turshi: remove old macro annocation

--- turshi/src/lib.rs
+++ turshi/src/lib.rs
@@ -1,9 +1,11 @@
-#![warn(missing_docs)]
-//! This module contains the code that executes a compiled Cairo program and generates the memory.
+//! This module contains the code that executes a compiled Cairo program and
+//! generates the memory.
 //! The Cairo runner includes code to execute a bytecode compiled Cairo program,
 //! and obtain a memory instantiation after the execution. It uses some code to
-//! represent Cairo instructions and their decomposition, together with their logic
-//! which is represented as steps of computation making up the full program.
+//! represent Cairo instructions and their decomposition, together with their
+//! logic which is represented as steps of computation making up the full
+//! program.
+
 pub mod flags;
 pub mod helper;
 pub mod memory;
