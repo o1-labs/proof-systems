6bafa00191e7a33aa52db98fdc2d192d44e61f4e
Danny Willems <be.danny.willems@gmail.com>
2024-07-30 00:21:38 +0200

    Utils: move math related tests in subdirectory

--- utils/src/math.rs
+++ utils/src/math.rs
@@ -21,30 +21,3 @@ pub fn ceil_log2(d: usize) -> usize {
 pub fn div_ceil(a: usize, b: usize) -> usize {
     (a + b - 1) / b
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_log2() {
-        let tests = [
-            (1, 0),
-            (2, 1),
-            (3, 2),
-            (9, 4),
-            (15, 4),
-            (16, 4),
-            (17, 5),
-            (15430, 14),
-            (usize::MAX, 64),
-        ];
-        for (d, expected_res) in tests.iter() {
-            let res = ceil_log2(*d);
-            assert_eq!(
-                res, *expected_res,
-                "ceil(log2({d})) = {res}, expected = {expected_res}"
-            )
-        }
-    }
-}
