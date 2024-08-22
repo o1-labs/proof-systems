09a7c61aed4019a2ef79a5de5bf32d6beeef662a
Danny Willems <be.danny.willems@gmail.com>
2024-08-22 10:33:45 -0700

    MVPoly: move doc + rename prime::MVPoly in prime::Dense

--- mvpoly/src/prime.rs
+++ mvpoly/src/prime.rs
@@ -1,5 +1,145 @@
-//! Multivariate polynomial
-//! See [these notes](https://hackmd.io/@dannywillems/SyHar7p5A) for more context.
+//! Multivariate polynomial dense representation using prime numbers
+//!
+//! First, we start by attributing a different prime number for each variable.
+//! For instance, for `F^{<=2}[X_{1}, X_{2}]`, we assign `X_{1}` to `2`
+//! and $X_{2}$ to $3$.
+//! From there, we note `X_{1} X_{2}` as the value `6`, `X_{1}^2` as `4`, `X_{2}^2`
+//! as 9. The constant is `1`.
+//!
+//! From there, we represent our polynomial coefficients in a sparse list. Some
+//! cells, noted `NA`, won't be used for certain vector spaces.
+//!
+//! For instance, `X_{1} + X_{2}` will be represented as:
+//! ```text
+//! [0,   1,   1,   0,    0,   0,    0,    0,    0]
+//!  |    |    |    |     |    |     |     |     |
+//!  1    2    3    4     5    6     7     8     9
+//!  |    |    |    |     |    |     |     |     |
+//!  cst  X1  X2   X1^2   NA  X1*X2  NA   NA    X2^2
+//! ```
+//!
+//! and the polynomial `42 X_{1} + 3 X_{1} X_{2} + 14 X_{2}^2` will be represented
+//! as
+//!
+//! ```text
+//! [0,  42,   1,   0,    0,   3,    0,    0,    14]
+//!  |    |    |    |     |    |     |     |     |
+//!  1    2    3    4     5    6     7     8     9
+//!  |    |    |    |     |    |     |     |     |
+//!  cst  X1  X2   X1^2   NA  X1*X2  NA   NA    X2^2
+//! ```
+//!
+//! Adding two polynomials in this base is pretty straightforward: we simply add the
+//! coefficients of the two lists.
+//!
+//! Multiplication is not more complicated.
+//! To compute the result of $P_{1} * P_{2}$, the value of index $i$ will be the sum
+//! of the decompositions.
+//!
+//! For instance, if we take `P_{1}(X_{1}) = 2 X_{1} + X_{2}` and `P_{2}(X_{1},
+//! X_{2}) = X_{2} + 3`, the expected product is
+//! `P_{3}(X_{1}, X_{2}) = (2 X_{1} + X_{2}) * (X_{2} + 3) = 2 X_{1} X_{2} + 6
+//! X_{1} + 3 X_{2} + X_{2}^2`
+//!
+//! Given in the representation above, we have:
+//!
+//! ```text
+//! For P_{1}:
+//!
+//! [0,   2,   1,   0,    0,   0,    0,    0,    0]
+//!  |    |    |    |     |    |     |     |     |
+//!  1    2    3    4     5    6     7     8     9
+//!  |    |    |    |     |    |     |     |     |
+//!  cst  X1  X2   X1^2   NA  X1*X2  NA   NA    X2^2
+//!
+//! ```
+//!
+//! ```text
+//! For P_{2}:
+//!
+//! [3,   0,   1,   0,    0,   0,    0,    0,    0]
+//!  |    |    |    |     |    |     |     |     |
+//!  1    2    3    4     5    6     7     8     9
+//!  |    |    |    |     |    |     |     |     |
+//!  cst  X1  X2   X1^2   NA  X1*X2  NA   NA    X2^2
+//!
+//! ```
+//!
+//!
+//! ```text
+//! For P_{3}:
+//!
+//! [0,   6,   3,   0,    0,   2,    0,    0,    1]
+//!  |    |    |    |     |    |     |     |     |
+//!  1    2    3    4     5    6     7     8     9
+//!  |    |    |    |     |    |     |     |     |
+//!  cst  X1  X2   X1^2   NA  X1*X2  NA   NA    X2^2
+//!
+//! ```
+//!
+//! To compute `P_{3}`, we get iterate over an empty list of $9$ elements which will
+//! define `P_{3}`.
+//!
+//! For index `1`, we multiply `P_{1}[1]` and `P_{1}[1]`.
+//!
+//! FOr index $2$, the only way to get this index is by fetching $2$ in each list.
+//! Therefore, we do `P_{1}[2] P_{2}[1] + P_{2}[2] * P_{1}[1] = 2 * 3 + 0 * 0 = 6`.
+//!
+//! For index `3`, same than for `2`.
+//!
+//! For index `4`, we have `4 = 2 * 2`, therefore, we multiply `P_{1}[2]` and `P_{2}[2]`
+//!
+//! For index `6`, we have `6 = 2 * 3` and `6 = 3 * 2`, which are the prime
+//! decompositions of $6$. Therefore we sum `P_{1}[2] * P_{2}[3]` and `P_{2}[2] *
+//! P_{1}[3]`.
+//!
+//! For index $9$, we have $9 = 3 * 3$, therefore we do the same than for $4$.
+//!
+//! This can be generalized.
+//!
+//! The algorithm is as follow:
+//! - for each cell `j`:
+//!     - if `j` is prime, compute `P_{1}[j] P_{2}[1] + P_{2}[j] P_{1}[1]`
+//!     - else:
+//!         - take the prime decompositions of `j` (and their permutations).
+//!         - for each decomposition, compute the product
+//!         - sum
+//!
+//!
+//! #### Other examples degree $2$ with 3 variables.
+//!
+//! ```math
+//! \begin{align}
+//! $\mathbb{F}^{\le 2}[X_{1}, X_{2}, X_{3}] = \{
+//!         & \, a_{0} + \\
+//!         & \, a_{1} X_{1} + \\
+//!         & \, a_{2} X_{2} + \\
+//!         & \, a_{3} X_{3} + \\
+//!         & \, a_{4} X_{1} X_{2} + \\
+//!         & \, a_{5} X_{2} X_{3} + \\
+//!         & \, a_{6} X_{1} X_{3} + \\
+//!         & \, a_{7} X_{1}^2 + \\
+//!         & \, a_{8} X_{2}^2 + \\
+//!         & \, a_{9} X_{3}^2 \, | \, a_{i} \in \mathbb{F}
+//!         \}
+//! \end{align}
+//! ```
+//!
+//! We assign:
+//!
+//! - `X_{1} = 2`
+//! - `X_{2} = 3`
+//! - `X_{3} = 5`
+//!
+//! And therefore, we have:
+//! - `X_{1}^2 = 4`
+//! - `X_{1} X_{2} = 6`
+//! - `X_{1} X_{3} = 10`
+//! - `X_{2}^2 = 9`
+//! - `X_{2} X_{3} = 15`
+//! - `X_{3}^2 = 25`
+//!
+//! We have an array with 25 indices, even though we need 10 elements only.
 
 use std::{
     collections::HashMap,
@@ -16,18 +156,24 @@ use crate::utils::{
 };
 
 /// Represents a multivariate polynomial of degree less than `D` in `N` variables.
-/// The natural representation is the coefficients given in the monomial basis.
-pub struct MVPoly<F: PrimeField, const N: usize, const D: usize> {
+/// The representation is dense, i.e., all coefficients are stored.
+/// The polynomial is represented as a vector of coefficients, where the index
+/// of the coefficient corresponds to the index of the monomial.
+/// A mapping between the index and the prime decomposition of the monomial is
+/// stored in `normalized_indices`.
+pub struct Dense<F: PrimeField, const N: usize, const D: usize> {
     coeff: Vec<F>,
     // keeping track of the indices of the monomials that are normalized
     // to avoid recomputing them
+    // FIXME: this should be stored somewhere else; we should not have it for
+    // each polynomial
     normalized_indices: Vec<usize>,
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> Dense<F, N, D> {
     pub fn new() -> Self {
         let normalized_indices = Self::compute_normalized_indices();
-        MVPoly {
+        Dense {
             coeff: vec![F::zero(); Self::dimension()],
             normalized_indices,
         }
@@ -39,7 +185,7 @@ impl<F: PrimeField, const N: usize, const D: usize> MVPoly<F, N, D> {
 
     pub fn from_coeffs(coeff: Vec<F>) -> Self {
         let normalized_indices = Self::compute_normalized_indices();
-        MVPoly {
+        Dense {
             coeff,
             normalized_indices,
         }
@@ -102,18 +248,18 @@ impl<F: PrimeField, const N: usize, const D: usize> MVPoly<F, N, D> {
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> Default for MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> Default for Dense<F, N, D> {
     fn default() -> Self {
-        MVPoly::new()
+        Dense::new()
     }
 }
 
 // Addition
-impl<F: PrimeField, const N: usize, const D: usize> Add for MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> Add for Dense<F, N, D> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        let mut result = MVPoly::new();
+        let mut result = Dense::new();
         for i in 0..self.coeff.len() {
             result.coeff[i] = self.coeff[i] + other.coeff[i];
         }
@@ -121,11 +267,11 @@ impl<F: PrimeField, const N: usize, const D: usize> Add for MVPoly<F, N, D> {
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> Add<&MVPoly<F, N, D>> for MVPoly<F, N, D> {
-    type Output = MVPoly<F, N, D>;
+impl<F: PrimeField, const N: usize, const D: usize> Add<&Dense<F, N, D>> for Dense<F, N, D> {
+    type Output = Dense<F, N, D>;
 
-    fn add(self, other: &MVPoly<F, N, D>) -> MVPoly<F, N, D> {
-        let mut result = MVPoly::new();
+    fn add(self, other: &Dense<F, N, D>) -> Dense<F, N, D> {
+        let mut result = Dense::new();
         for i in 0..self.coeff.len() {
             result.coeff[i] = self.coeff[i] + other.coeff[i];
         }
@@ -133,11 +279,11 @@ impl<F: PrimeField, const N: usize, const D: usize> Add<&MVPoly<F, N, D>> for MV
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> Add<MVPoly<F, N, D>> for &MVPoly<F, N, D> {
-    type Output = MVPoly<F, N, D>;
+impl<F: PrimeField, const N: usize, const D: usize> Add<Dense<F, N, D>> for &Dense<F, N, D> {
+    type Output = Dense<F, N, D>;
 
-    fn add(self, other: MVPoly<F, N, D>) -> MVPoly<F, N, D> {
-        let mut result = MVPoly::new();
+    fn add(self, other: Dense<F, N, D>) -> Dense<F, N, D> {
+        let mut result = Dense::new();
         for i in 0..self.coeff.len() {
             result.coeff[i] = self.coeff[i] + other.coeff[i];
         }
@@ -145,11 +291,11 @@ impl<F: PrimeField, const N: usize, const D: usize> Add<MVPoly<F, N, D>> for &MV
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> Add<&MVPoly<F, N, D>> for &MVPoly<F, N, D> {
-    type Output = MVPoly<F, N, D>;
+impl<F: PrimeField, const N: usize, const D: usize> Add<&Dense<F, N, D>> for &Dense<F, N, D> {
+    type Output = Dense<F, N, D>;
 
-    fn add(self, other: &MVPoly<F, N, D>) -> MVPoly<F, N, D> {
-        let mut result = MVPoly::new();
+    fn add(self, other: &Dense<F, N, D>) -> Dense<F, N, D> {
+        let mut result = Dense::new();
         for i in 0..self.coeff.len() {
             result.coeff[i] = self.coeff[i] + other.coeff[i];
         }
@@ -158,7 +304,7 @@ impl<F: PrimeField, const N: usize, const D: usize> Add<&MVPoly<F, N, D>> for &M
 }
 
 // Multiplication
-impl<F: PrimeField, const N: usize, const D: usize> Mul<MVPoly<F, N, D>> for MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> Mul<Dense<F, N, D>> for Dense<F, N, D> {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
@@ -193,15 +339,15 @@ impl<F: PrimeField, const N: usize, const D: usize> Mul<MVPoly<F, N, D>> for MVP
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> PartialEq for MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> PartialEq for Dense<F, N, D> {
     fn eq(&self, other: &Self) -> bool {
         self.coeff == other.coeff
     }
 }
 
-impl<F: PrimeField, const N: usize, const D: usize> Eq for MVPoly<F, N, D> {}
+impl<F: PrimeField, const N: usize, const D: usize> Eq for Dense<F, N, D> {}
 
-impl<F: PrimeField, const N: usize, const D: usize> Debug for MVPoly<F, N, D> {
+impl<F: PrimeField, const N: usize, const D: usize> Debug for Dense<F, N, D> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         let mut prime_gen = PrimeNumberGenerator::new();
         self.coeff.iter().enumerate().for_each(|(i, c)| {
