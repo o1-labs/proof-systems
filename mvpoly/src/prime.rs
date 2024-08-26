b71dec299e3351bb8f487cb6c91d7cee9b9750a0
Danny Willems <be.danny.willems@gmail.com>
2024-08-26 16:55:47 -0700

    MVPoly: add double
    To match expr interface

--- mvpoly/src/prime.rs
+++ mvpoly/src/prime.rs
@@ -281,6 +281,14 @@ impl<F: PrimeField, const N: usize, const D: usize> Dense<F, N, D> {
             });
         is_homogeneous
     }
+
+    pub fn double(&self) -> Self {
+        let mut result = Dense::zero();
+        for i in 0..self.coeff.len() {
+            result.coeff[i] = self.coeff[i].double();
+        }
+        result
+    }
 }
 
 impl<F: PrimeField, const N: usize, const D: usize> Default for Dense<F, N, D> {
