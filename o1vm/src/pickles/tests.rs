4ddcafc3a68f0687d9a10119b27be50ab49ce089
Danny Willems <be.danny.willems@gmail.com>
2024-09-24 23:18:24 +0200

    o1vm/pickles: unit tests from ethereum-optimism

---
+++ o1vm/src/pickles/tests.rs
@@ -0,0 +1,4 @@
+// There are tests in
+// ${PROJECT_ROOT}/ethereum-optimism/cannon/mipsevm/open_mips_tests/test/bin
+// Let's read them, and make a proof of them and verify.
+// It will be our unit tests for each instruction
