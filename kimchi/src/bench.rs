21baded2e86e6466b5423f8c7741e05df05470dd
Danny Willems <be.danny.willems@gmail.com>
2024-09-25 22:46:41 +0200

    Kimchi/Bench: remove duplicated test_bench.
    It is already in the directory benches

--- kimchi/src/bench.rs
+++ kimchi/src/bench.rs
@@ -115,30 +115,3 @@ impl BenchmarkCtx {
         .unwrap();
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use std::time::Instant;
-
-    use super::*;
-
-    #[test]
-    fn test_bench() {
-        // context created in 21.2235 ms
-        let start = Instant::now();
-        let srs_size = 4;
-        let ctx = BenchmarkCtx::new(srs_size);
-        println!("testing bench code for SRS of size {srs_size}");
-        println!("context created in {}s", start.elapsed().as_secs());
-
-        // proof created in 7.1227 ms
-        let start = Instant::now();
-        let (proof, public_input) = ctx.create_proof();
-        println!("proof created in {}s", start.elapsed().as_secs());
-
-        // proof verified in 1.710 ms
-        let start = Instant::now();
-        ctx.batch_verification(&vec![(proof, public_input)]);
-        println!("proof verified in {}", start.elapsed().as_secs());
-    }
-}
