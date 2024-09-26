dac23e12af546645991570e101a161e8576bb668
dannywillems <dannywillems@users.noreply.github.com>
2024-09-26 20:40:58 +0000

    deploy: 347899ada9c6a1a8568dca3fff022c349346892c

--- rustdoc/o1_utils/field_helpers/sidebar-items.js
+++ rustdoc/o1_utils/field_helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["FieldHelpersError"],"fn":["i32_to_field","pows","product"],"trait":["BigUintFieldHelpers","FieldHelpers","RandomField","Two"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["FieldHelpersError"],"fn":["i32_to_field","inner_prod","pows","product"],"trait":["BigUintFieldHelpers","FieldHelpers","RandomField","Two"],"type":["Result"]};
