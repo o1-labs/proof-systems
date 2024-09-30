90a374a3cc5eb658fe0f5ffb43db11bae6e4b637
querolita <querolita@users.noreply.github.com>
2024-09-30 15:30:50 +0000

    deploy: 4f8682dc488dfc42ac8df45340f12c1ca64752b2

--- rustdoc/kimchi/circuits/constraints/sidebar-items.js
+++ rustdoc/kimchi/circuits/constraints/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["GateError"],"fn":["selector_polynomial","zk_rows_strict_lower_bound"],"struct":["Builder","ColumnEvaluations","ConstraintSystem","EvaluatedColumnCoefficients","FeatureFlags"]};
+window.SIDEBAR_ITEMS = {"constant":["NUM_CHUNKS_BY_DEFAULT","ZK_ROWS_BY_DEFAULT"],"enum":["GateError"],"fn":["selector_polynomial","zk_rows_strict_lower_bound"],"struct":["Builder","ColumnEvaluations","ConstraintSystem","EvaluatedColumnCoefficients","FeatureFlags"]};
