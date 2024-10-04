fb4593d7d8a582c996790cc5306ceb08f2671c54
Danny Willems <be.danny.willems@gmail.com>
2024-10-04 22:20:05 +0200

    o1vm/pickles: using new instead of elems
    elems has been renamed in chunks previously, to map the actual meaning of the
    field of the structure

--- o1vm/src/pickles/prover.rs
+++ o1vm/src/pickles/prover.rs
@@ -209,9 +209,7 @@ where
             (
                 DensePolynomialOrEvaluations::DensePolynomial(poly),
                 // We do not have any blinder, therefore we set to 0.
-                PolyComm {
-                    elems: vec![G::ScalarField::zero()],
-                },
+                PolyComm::new(vec![G::ScalarField::zero()]),
             )
         })
         .collect();
