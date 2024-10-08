b9932835d2f4410d96c086154ff40be8d6320bb3
Matt Walker <matt.g.d.walker@gmail.com>
2024-10-08 12:27:13 -0400

    Add dynamic selectors to column_env.rs

--- o1vm/src/pickles/column_env.rs
+++ o1vm/src/pickles/column_env.rs
@@ -40,10 +40,7 @@ impl<'a, F: FftField> TColumnEnvironment<'a, F, BerkeleyChallengeTerm, BerkeleyC
     // We do not want to keep kimchi_msm/generic prover
     type Column = kimchi_msm::columns::Column;
 
-    fn get_column(
-        &self,
-        col: &Self::Column,
-    ) -> Option<&'a Evaluations<F, Radix2EvaluationDomain<F>>> {
+    fn get_column(&self, col: &Self::Column) -> Option<&'a Evals<F>> {
         match *col {
             Self::Column::Relation(i) => {
                 if i < SCRATCH_SIZE {
@@ -59,9 +56,13 @@ impl<'a, F: FftField> TColumnEnvironment<'a, F, BerkeleyChallengeTerm, BerkeleyC
                     panic!("We should not have that many relation columns");
                 }
             }
-            Self::Column::DynamicSelector(_i) => {
-                // FIXME: add selectors
-                panic!("Not implemented yet");
+            Self::Column::DynamicSelector(i) => {
+                assert!(
+                    i < N_MIPS_SEL_COLS,
+                    "We do not have that many dynamic selector columns"
+                );
+                let res = &self.witness.selector[i];
+                Some(res)
             }
             _ => {
                 panic!("We should not have any other type of columns")
