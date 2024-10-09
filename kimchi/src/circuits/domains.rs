bde970479d842d57e8d77466f34cfbb099ece257
Danny Willems <be.danny.willems@gmail.com>
2024-10-09 12:12:50 +0200

    Kimchi/test: move test_create_domain into subdirectory tests
    Respect Rust convention to move integration tests in a different subdirectory

--- kimchi/src/circuits/domains.rs
+++ kimchi/src/circuits/domains.rs
@@ -58,23 +58,3 @@ impl<F: FftField> EvaluationDomains<F> {
         Ok(EvaluationDomains { d1, d2, d4, d8 })
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ark_ff::Field;
-    use mina_curves::pasta::Fp;
-
-    #[test]
-    fn test_create_domain() {
-        if let Ok(d) = EvaluationDomains::<Fp>::create(usize::MAX) {
-            assert!(d.d4.group_gen.pow([4]) == d.d1.group_gen);
-            assert!(d.d8.group_gen.pow([2]) == d.d4.group_gen);
-            println!("d8 = {:?}", d.d8.group_gen);
-            println!("d8^2 = {:?}", d.d8.group_gen.pow([2]));
-            println!("d4 = {:?}", d.d4.group_gen);
-            println!("d4 = {:?}", d.d4.group_gen.pow([4]));
-            println!("d1 = {:?}", d.d1.group_gen);
-        }
-    }
-}
