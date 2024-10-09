bd49d628689a404d0bd765e00fcd2f04f6072aed
Fizzixnerd <Fizzixnerd@users.noreply.github.com>
2024-10-09 20:27:55 +0000

    deploy: 09cf803486b00929b050f708cd704b039918148f

--- rustdoc/o1vm/pickles/sidebar-items.js
+++ rustdoc/o1vm/pickles/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["column_env","proof","prover"]};
+window.SIDEBAR_ITEMS = {"constant":["DEGREE_QUOTIENT_POLYNOMIAL","MAXIMUM_DEGREE_CONSTRAINTS","TOTAL_NUMBER_OF_CONSTRAINTS"],"mod":["column_env","proof","prover"]};
