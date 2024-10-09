ccba0204668198c7cc11df53d9096d94fcaf5832
dannywillems <dannywillems@users.noreply.github.com>
2024-10-09 12:41:16 +0000

    deploy: 53f3e453858b329216d389525f9ba925c44fcc7f

--- rustdoc/implementors/kimchi/circuits/expr/trait.AlphaChallengeTerm.js
+++ rustdoc/implementors/kimchi/circuits/expr/trait.AlphaChallengeTerm.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
+"arrabiata":[["impl&lt;'a&gt; AlphaChallengeTerm&lt;'a&gt; for <a class=\"enum\" href=\"arrabiata/columns/enum.ChallengeTerm.html\" title=\"enum arrabiata::columns::ChallengeTerm\">ChallengeTerm</a>"]],
 "kimchi":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
