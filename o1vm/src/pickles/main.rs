69179f5442eb1449e82375d19c407aa5d391af7d
Danny Willems <be.danny.willems@gmail.com>
2024-10-09 16:09:32 +0200

    o1vm/pickles: remove old FIXME - selectors are now added

--- o1vm/src/pickles/main.rs
+++ o1vm/src/pickles/main.rs
@@ -102,7 +102,6 @@ pub fn main() -> ExitCode {
     let mut curr_proof_inputs: ProofInputs<Vesta> = ProofInputs::new(DOMAIN_SIZE);
     while !mips_wit_env.halt {
         let _instr: Instruction = mips_wit_env.step(&configuration, &meta, &start);
-        // FIXME: add selectors
         for (scratch, scratch_chunk) in mips_wit_env
             .scratch_state
             .iter()
