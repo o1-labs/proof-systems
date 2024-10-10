cea212c037e96ee69b4d4432dc9927b7388f8e6d
marcbeunardeau88 <marc@o1labs.org>
2024-10-10 12:44:55 +0200

    o1vm/pickles/main : add constraints together the constraints from the different instructions were put one next to another when gatherting them in main, and aggregated in the prover. Instead we add them together.

--- o1vm/src/pickles/main.rs
+++ o1vm/src/pickles/main.rs
@@ -1,4 +1,4 @@
-use ark_ff::UniformRand;
+use ark_ff::{UniformRand, Zero};
 use kimchi::circuits::domains::EvaluationDomains;
 use kimchi_msm::expr::E;
 use log::debug;
@@ -83,7 +83,7 @@ pub fn main() -> ExitCode {
         let mut mips_con_env = mips_constraints::Env::<Fp>::default();
         let mut constraints = Instruction::iter()
             .flat_map(|instr_typ| instr_typ.into_iter())
-            .fold(vec![], |mut acc, instr| {
+            .fold(vec![], |acc, instr| {
                 interpreter::interpret_instruction(&mut mips_con_env, instr);
                 let selector = mips_con_env.get_selector();
                 let constraints_with_selector: Vec<E<Fp>> = mips_con_env
@@ -91,9 +91,20 @@ pub fn main() -> ExitCode {
                     .into_iter()
                     .map(|c| selector.clone() * c)
                     .collect();
-                acc.extend(constraints_with_selector);
+                // we want to iterate on the vector of the biggest length.
+                let (big, small) = if constraints_with_selector.len() > acc.len() {
+                    (constraints_with_selector, acc)
+                } else {
+                    (acc, constraints_with_selector)
+                };
+                // add the constraints together
+                let res = big
+                    .into_iter()
+                    .enumerate()
+                    .map(|(i, cst)| cst + small.get(i).unwrap_or(&E::<Fp>::zero()).clone())
+                    .collect();
                 mips_con_env.reset();
-                acc
+                res
             });
         constraints.extend(mips_con_env.get_selector_constraints());
         constraints
