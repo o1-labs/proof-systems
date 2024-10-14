c14aa1c24cc3eeffd5f07b60ad14e8a738e7844c
Danny Willems <be.danny.willems@gmail.com>
2024-10-14 10:38:16 +0200

    Kimchi: modest additional documentation

--- kimchi/src/circuits/domains.rs
+++ kimchi/src/circuits/domains.rs
@@ -1,3 +1,6 @@
+//! This module describes the evaluation domains that can be used by the
+//! polynomials.
+
 use ark_ff::FftField;
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
 use serde::{Deserialize, Serialize};
@@ -5,6 +8,8 @@ use serde_with::serde_as;
 
 use crate::error::DomainCreationError;
 
+/// The different multiplicaive domain sizes that can be used by the polynomials.
+/// We do support up to 8 times the size of the original domain for now.
 #[derive(Clone, Copy, Debug, PartialEq, FromPrimitive, ToPrimitive)]
 pub enum Domain {
     D1 = 1,
