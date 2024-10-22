ee90f2bb338dd7148243efb38416e402a64cc2f2
marcbeunardeau88 <marc@o1labs.org>
2024-10-22 10:37:11 +0200

    bugfix sel + remove stupid assert + better print

--- o1vm/src/pickles/proof.rs
+++ o1vm/src/pickles/proof.rs
@@ -30,27 +30,24 @@ impl<G: KimchiCurve> ProofInputs<G> {
         let l = vec![16, 18, 19, 20, 21];
         let mut count = 0;
         for i in 0..self.evaluations.selector.len() {
-            if !(G::ScalarField::from(1 as u64) == self.evaluations.selector[i]) {
-                assert!(G::ScalarField::from(0 as u64) == self.evaluations.selector[i]);
-            }
-            if G::ScalarField::from(1 as u64) == self.evaluations.selector[i] && count < 5 {
+            if G::ScalarField::from(17 as u64) == self.evaluations.selector[i] && count < 5 {
                 count += 1;
                 for j in &l {
                     println!("scratch[{}]= {}", j, self.evaluations.scratch[*j][i])
                 }
+                // second equal
+
+                let x = self.evaluations.scratch[16][i]
+    // 0xcd0f
+- G::ScalarField::from((13 + 12*16 + 15 * 256 * 16) as u64);
+                let inv_or_zero = self.evaluations.scratch[19][i];
+                println!("x*x_inv cd0f cst={}", x * inv_or_zero);
+
                 let x = self.evaluations.scratch[16][i]
             // 0x1810
                 - G::ScalarField::from((8 + 16 + 16 * 256) as u64);
                 let inv_or_zero = self.evaluations.scratch[21][i];
-                println!("x*x_inv={}", x * inv_or_zero);
-
-                // secind equal
-                println!("second equality");
-                let x = self.evaluations.scratch[16][i]
-                // 0xcd0f
-    - G::ScalarField::from((13 + 12*16 + 15 * 256 * 16) as u64);
-                let inv_or_zero = self.evaluations.scratch[19][i];
-                println!("x*x_inv={}", x * inv_or_zero)
+                println!("x*x_inv 1810cst={}", x * inv_or_zero);
             }
         }
     }
