9f739c073a2fded25c122f73eed8ce11d8d0fe1f
Danny Willems <be.danny.willems@gmail.com>
2024-10-22 13:13:21 +0200

    o1vm: MISC format

--- o1vm/src/interpreters/mod.rs
+++ o1vm/src/interpreters/mod.rs
@@ -5,6 +5,5 @@ pub mod keccak;
 pub mod mips;
 
 /// An interpreter for the RISC-V 32I instruction set, following the specification
-/// on
-/// [riscv.org](https://riscv.org/wp-content/uploads/2019/12/riscv-spec-20191213.pdf).
+/// on [riscv.org](https://riscv.org/wp-content/uploads/2019/12/riscv-spec-20191213.pdf).
 pub mod riscv32i;
