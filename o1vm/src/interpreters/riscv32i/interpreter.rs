e4572013a34530d19f33f3e780a317563c60dd96
Danny Willems <be.danny.willems@gmail.com>
2024-10-22 22:09:45 +0200

    o1vm/riscv32i: implement iterator over the instructions

--- o1vm/src/interpreters/riscv32i/interpreter.rs
+++ o1vm/src/interpreters/riscv32i/interpreter.rs
@@ -1,3 +1,4 @@
+use strum::{EnumCount, IntoEnumIterator};
 use strum_macros::{EnumCount, EnumIter};
 
 use crate::lookups::{Lookup, LookupTableIDs};
@@ -96,6 +97,57 @@ pub enum JInstruction {
     Jump, // jalr
 }
 
+impl IntoIterator for Instruction {
+    type Item = Instruction;
+    type IntoIter = std::vec::IntoIter<Instruction>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        match self {
+            Instruction::RType(_) => {
+                let mut iter_contents = Vec::with_capacity(RInstruction::COUNT);
+                for rtype in RInstruction::iter() {
+                    iter_contents.push(Instruction::RType(rtype));
+                }
+                iter_contents.into_iter()
+            }
+            Instruction::IType(_) => {
+                let mut iter_contents = Vec::with_capacity(IInstruction::COUNT);
+                for itype in IInstruction::iter() {
+                    iter_contents.push(Instruction::IType(itype));
+                }
+                iter_contents.into_iter()
+            }
+            Instruction::SType(_) => {
+                let mut iter_contents = Vec::with_capacity(SInstruction::COUNT);
+                for stype in SInstruction::iter() {
+                    iter_contents.push(Instruction::SType(stype));
+                }
+                iter_contents.into_iter()
+            }
+            Instruction::BType(_) => {
+                let mut iter_contents = Vec::with_capacity(BInstruction::COUNT);
+                for btype in BInstruction::iter() {
+                    iter_contents.push(Instruction::BType(btype));
+                }
+                iter_contents.into_iter()
+            }
+            Instruction::UType(_) => {
+                let mut iter_contents = Vec::with_capacity(UInstruction::COUNT);
+                for utype in UInstruction::iter() {
+                    iter_contents.push(Instruction::UType(utype));
+                }
+                iter_contents.into_iter()
+            }
+            Instruction::JType(_) => {
+                let mut iter_contents = Vec::with_capacity(JInstruction::COUNT);
+                for jtype in JInstruction::iter() {
+                    iter_contents.push(Instruction::JType(jtype));
+                }
+                iter_contents.into_iter()
+            }
+        }
+    }
+}
 pub trait InterpreterEnv {
     /// A position can be seen as an indexed variable
     type Position;
