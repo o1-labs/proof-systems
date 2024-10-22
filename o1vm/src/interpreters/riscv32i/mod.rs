9cc1100d07218a07370ffb554771e1453b6f0fa9
Danny Willems <be.danny.willems@gmail.com>
2024-10-22 09:50:42 +0200

    o1vm/riscv32i: add SCRATCH_STATE + expose interpreter mod

--- o1vm/src/interpreters/riscv32i/mod.rs
+++ o1vm/src/interpreters/riscv32i/mod.rs
@@ -1,2 +1,7 @@
 pub mod column;
+pub mod interpreter;
 pub mod registers;
+
+/// The minimal number of columns required for the VM
+// FIXME: this is not correct
+pub const SCRATCH_STATE: usize = 80;
