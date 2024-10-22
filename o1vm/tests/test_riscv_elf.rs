df5b68f26b523ec43e3b193da4eebc1e2f85d02d
Danny Willems <be.danny.willems@gmail.com>
2024-10-22 20:20:45 +0200

    o1vm/riscv32i: read ELF in a test

---
+++ o1vm/tests/test_riscv_elf.rs
@@ -0,0 +1,21 @@
+use elf::{
+    endian::{AnyEndian, LittleEndian},
+    note::{Note, NoteGnuBuildId},
+    section::SectionHeader,
+    ElfBytes,
+};
+
+#[test]
+fn test_elf() {
+    let curr_dir = std::env::current_dir().unwrap();
+    println!("Path: {:?}", curr_dir);
+    let path = curr_dir.join(std::path::PathBuf::from(
+        "resources/programs/riscv32i/basic.elf",
+    ));
+    println!("Path: {:?}", path);
+    let file_data = std::fs::read(path).expect("Could not read file.");
+    let slice = file_data.as_slice();
+    let file = ElfBytes::<LittleEndian>::minimal_parse(slice).expect("Open test1");
+
+    println!("File: {:?}", file);
+}
