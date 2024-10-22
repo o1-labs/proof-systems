8fa75730f70b929ea6a6ebd325d25ceb842109dd
marcbeunardeau88 <marc@o1labs.org>
2024-10-22 10:07:23 +0200

    debug 5 cst, simplify print, assert sel = 0 or 1

--- o1vm/src/pickles/proof.rs
+++ o1vm/src/pickles/proof.rs
@@ -10,7 +10,6 @@ pub struct WitnessColumns<G, S> {
     pub error: G,
     pub selector: S,
 }
-
 pub struct ProofInputs<G: KimchiCurve> {
     pub evaluations: WitnessColumns<Vec<G::ScalarField>, Vec<G::ScalarField>>,
 }
@@ -28,54 +27,29 @@ impl<G: KimchiCurve> ProofInputs<G> {
     }
 
     pub fn witness17(&self) {
-        let mut met = false;
-        let mut fst_met = 666;
-        for i in 0..self.evaluations.selector.len() {
-            if !met {
-                met = <G::ScalarField as Field>::ONE == self.evaluations.selector[i];
-                if met {
-                    fst_met = i
-                }
-            }
-        }
         let l = vec![16, 18, 19, 20, 21];
-        if !met {
-            // println!("no instr 17 found")
-        } else {
-            println!("fst instr 17 is {}", fst_met);
-            for i in l {
-                println!("scratch[{}]= {}", i, self.evaluations.scratch[i][fst_met])
+        let mut count = 0;
+        for i in 0..self.evaluations.selector.len() {
+            if !(G::ScalarField::from(1 as u64) == self.evaluations.selector[i]) {
+                assert!(G::ScalarField::from(0 as u64) == self.evaluations.selector[i]);
             }
-            let x = self.evaluations.scratch[16][fst_met]
+            if G::ScalarField::from(1 as u64) == self.evaluations.selector[i] && count < 5 {
+                count += 1;
+                for j in &l {
+                    println!("scratch[{}]= {}", j, self.evaluations.scratch[*j][i])
+                }
+                let x = self.evaluations.scratch[16][i]
             // 0x1810
                 - G::ScalarField::from((8 + 16 + 16 * 256) as u64);
-            let inv_or_zero = self.evaluations.scratch[21][fst_met];
-            if x * inv_or_zero == -G::ScalarField::ONE {
-                println!("!!!!!!!!")
-            }
-            if x * inv_or_zero == G::ScalarField::ONE {
-                println!("111111111111111")
-            }
-            if x * inv_or_zero == G::ScalarField::ZERO {
-                println!("000000000000")
-            } else {
-                println!("x*x_inv={}", x * inv_or_zero)
-            }
-            // secind equal
-            println!("second equality");
-            let x = self.evaluations.scratch[16][fst_met]
-// 0xcd0f
+                let inv_or_zero = self.evaluations.scratch[21][i];
+                println!("x*x_inv={}", x * inv_or_zero);
+
+                // secind equal
+                println!("second equality");
+                let x = self.evaluations.scratch[16][i]
+                // 0xcd0f
     - G::ScalarField::from((13 + 12*16 + 15 * 256 * 16) as u64);
-            let inv_or_zero = self.evaluations.scratch[19][fst_met];
-            if x * inv_or_zero == -G::ScalarField::ONE {
-                println!("!!!!!!!!")
-            }
-            if x * inv_or_zero == G::ScalarField::ONE {
-                println!("111111111111111")
-            }
-            if x * inv_or_zero == G::ScalarField::ZERO {
-                println!("000000000000")
-            } else {
+                let inv_or_zero = self.evaluations.scratch[19][i];
                 println!("x*x_inv={}", x * inv_or_zero)
             }
         }
