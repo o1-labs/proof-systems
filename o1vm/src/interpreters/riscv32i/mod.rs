76a466c6167b176353cb15f3be5c2a9a0eca70eb
Danny Willems <be.danny.willems@gmail.com>
2024-10-22 19:31:34 +0200

    o1vm/riscv32i: basic interface for the witness

--- o1vm/src/interpreters/riscv32i/mod.rs
+++ o1vm/src/interpreters/riscv32i/mod.rs
@@ -10,3 +10,7 @@ pub const SCRATCH_SIZE: usize = 80;
 
 // FIXME: I don't know yet how many instructions do exist in the RiscV32i ISA
 pub const INSTRUCTION_SET_SIZE: usize = 90;
+
+pub const PAGE_ADDRESS_SIZE: u32 = 12;
+pub const PAGE_SIZE: u32 = 1 << PAGE_ADDRESS_SIZE;
+pub const PAGE_ADDRESS_MASK: u32 = PAGE_SIZE - 1;
