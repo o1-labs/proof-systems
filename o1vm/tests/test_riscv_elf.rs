4d1b576be55e371c9e39799344637bd2d6bf5f59
Danny Willems <be.danny.willems@gmail.com>
2024-10-27 11:24:34 +0100

    o1vm/riscv32i: test correctly the to_string method

--- o1vm/tests/test_riscv_elf.rs
+++ o1vm/tests/test_riscv_elf.rs
@@ -1,3 +1,4 @@
+use o1vm::interpreters::riscv32i::interpreter::{IInstruction, Instruction, RInstruction};
 use std::collections::HashMap;
 
 use elf::{endian::LittleEndian, section::SectionHeader, ElfBytes};
@@ -96,4 +97,22 @@ fn test_elf() {
     //     .unwrap();
 
     // println!("Executable code: {:?}", executable_code);
+}
+
+#[test]
+// Checking an instruction can be converted into a string.
+// It is mostly because we would want to use it to debug or write better error
+// messages.
+fn test_instruction_can_be_converted_into_string() {
+    let instruction = Instruction::RType(RInstruction::Add);
+    assert_eq!(instruction.to_string(), "add");
+
+    let instruction = Instruction::RType(RInstruction::Sub);
+    assert_eq!(instruction.to_string(), "sub");
+
+    let instruction = Instruction::IType(IInstruction::LoadByte);
+    assert_eq!(instruction.to_string(), "lb");
+
+    let instruction = Instruction::IType(IInstruction::LoadHalf);
+    assert_eq!(instruction.to_string(), "lh");
 }
