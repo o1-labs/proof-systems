c15793124428c4f7decd21593711e12ac0585cd1
Danny Willems <be.danny.willems@gmail.com>
2024-10-27 11:24:00 +0100

    o1vm/riscv32i: reimplement display to get the string repr

--- o1vm/src/interpreters/riscv32i/interpreter.rs
+++ o1vm/src/interpreters/riscv32i/interpreter.rs
@@ -1,14 +1,12 @@
 use strum::{EnumCount, IntoEnumIterator};
-use strum_macros::{Display, EnumCount, EnumIter};
+use strum_macros::{EnumCount, EnumIter};
 
 use crate::lookups::{Lookup, LookupTableIDs};
 use ark_ff::{One, Zero};
 
 use super::registers::{REGISTER_CURRENT_IP, REGISTER_HEAP_POINTER, REGISTER_NEXT_IP};
 
-#[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Hash, Ord, PartialOrd, Display,
-)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Hash, Ord, PartialOrd)]
 pub enum Instruction {
     RType(RInstruction),
     IType(IInstruction),
@@ -20,9 +18,8 @@ pub enum Instruction {
 
 // See https://www.cs.cornell.edu/courses/cs3410/2024fa/assignments/cpusim/riscv-instructions.pdf for the order
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
-
 pub enum RInstruction {
     #[default]
     Add, // add
@@ -40,7 +37,7 @@ pub enum RInstruction {
 }
 
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
 pub enum IInstruction {
     #[default]
@@ -65,7 +62,7 @@ pub enum IInstruction {
 }
 
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
 pub enum SInstruction {
     #[default]
@@ -75,7 +72,7 @@ pub enum SInstruction {
 }
 
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
 pub enum SBInstruction {
     #[default]
@@ -88,16 +85,17 @@ pub enum SBInstruction {
 }
 
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
 pub enum UInstruction {
     #[default]
     LoadUpperImmediate, // lui
+    // Add upper immediate to PC
     AddUpperImmediate, // auipc
 }
 
 #[derive(
-    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd, Display,
+    Debug, Clone, Copy, Eq, PartialEq, EnumCount, EnumIter, Default, Hash, Ord, PartialOrd,
 )]
 pub enum UJInstruction {
     #[default]
@@ -155,6 +153,101 @@ impl IntoIterator for Instruction {
         }
     }
 }
+
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::RType(rtype) => write!(f, "{}", rtype),
+            Instruction::IType(itype) => write!(f, "{}", itype),
+            Instruction::SType(stype) => write!(f, "{}", stype),
+            Instruction::SBType(sbtype) => write!(f, "{}", sbtype),
+            Instruction::UType(utype) => write!(f, "{}", utype),
+            Instruction::UJType(ujtype) => write!(f, "{}", ujtype),
+        }
+    }
+}
+
+impl std::fmt::Display for RInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RInstruction::Add => write!(f, "add"),
+            RInstruction::Sub => write!(f, "sub"),
+            RInstruction::ShiftLeftLogical => write!(f, "sll"),
+            RInstruction::SetLessThan => write!(f, "slt"),
+            RInstruction::SetLessThanUnsigned => write!(f, "sltu"),
+            RInstruction::Xor => write!(f, "xor"),
+            RInstruction::ShiftRightLogical => write!(f, "srl"),
+            RInstruction::ShiftRightArithmetic => write!(f, "sra"),
+            RInstruction::Or => write!(f, "or"),
+            RInstruction::And => write!(f, "and"),
+            RInstruction::Fence => write!(f, "fence"),
+            RInstruction::FenceI => write!(f, "fence.i"),
+        }
+    }
+}
+
+impl std::fmt::Display for IInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IInstruction::LoadByte => write!(f, "lb"),
+            IInstruction::LoadHalf => write!(f, "lh"),
+            IInstruction::LoadWord => write!(f, "lw"),
+            IInstruction::LoadByteUnsigned => write!(f, "lbu"),
+            IInstruction::LoadHalfUnsigned => write!(f, "lhu"),
+            IInstruction::ShiftLeftLogicalImmediate => write!(f, "slli"),
+            IInstruction::ShiftRightLogicalImmediate => write!(f, "srli"),
+            IInstruction::ShiftRightArithmeticImmediate => write!(f, "srai"),
+            IInstruction::SetLessThanImmediate => write!(f, "slti"),
+            IInstruction::SetLessThanImmediateUnsigned => write!(f, "sltiu"),
+            IInstruction::AddImmediate => write!(f, "addi"),
+            IInstruction::XorImmediate => write!(f, "xori"),
+            IInstruction::OrImmediate => write!(f, "ori"),
+            IInstruction::AndImmediate => write!(f, "andi"),
+            IInstruction::JumpAndLinkRegister => write!(f, "jalr"),
+        }
+    }
+}
+
+impl std::fmt::Display for SInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SInstruction::StoreByte => write!(f, "sb"),
+            SInstruction::StoreHalf => write!(f, "sh"),
+            SInstruction::StoreWord => write!(f, "sw"),
+        }
+    }
+}
+
+impl std::fmt::Display for SBInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SBInstruction::BranchEq => write!(f, "beq"),
+            SBInstruction::BranchNeq => write!(f, "bne"),
+            SBInstruction::BranchLessThan => write!(f, "blt"),
+            SBInstruction::BranchGreaterThanEqual => write!(f, "bge"),
+            SBInstruction::BranchLessThanUnsigned => write!(f, "bltu"),
+            SBInstruction::BranchGreaterThanEqualUnsigned => write!(f, "bgeu"),
+        }
+    }
+}
+
+impl std::fmt::Display for UInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UInstruction::LoadUpperImmediate => write!(f, "lui"),
+            UInstruction::AddUpperImmediate => write!(f, "auipc"),
+        }
+    }
+}
+
+impl std::fmt::Display for UJInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UJInstruction::JumpAndLink => write!(f, "jal"),
+        }
+    }
+}
+
 pub trait InterpreterEnv {
     /// A position can be seen as an indexed variable
     type Position;
