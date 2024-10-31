295c19f4bd70dda2285d3cf6dc0cd42ee72c0ef8
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-10-31 14:27:31 +0800

    Remove duplicate tests

--- curves/src/pasta/curves/tests.rs
+++
@@ -1,5 +0,0 @@
-use crate::pasta::{ProjectivePallas, ProjectiveVesta};
-use ark_algebra_test_templates::*;
-
-test_group!(g1; ProjectivePallas; sw);
-test_group!(g2; ProjectiveVesta; sw);
--- curves/src/pasta/fields/tests.rs
+++
@@ -1,5 +0,0 @@
-use crate::pasta::fields::{Fp as Fr, Fq};
-use ark_algebra_test_templates::*;
-
-test_field!(fq; Fq; mont_prime_field);
-test_field!(fr; Fr; mont_prime_field);
