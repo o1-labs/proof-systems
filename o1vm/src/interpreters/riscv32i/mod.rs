89d9af9faf3a7de75d56b8572e9116c2502bda49
Danny Willems <be.danny.willems@gmail.com>
2024-11-04 11:45:39 +0100

    o1vm/riscv32i: introduce column module and instruction set size

--- o1vm/src/interpreters/riscv32i/mod.rs
+++ o1vm/src/interpreters/riscv32i/mod.rs
@@ -3,6 +3,11 @@
 // implemented. Using a small value for now.
 pub const SCRATCH_SIZE: usize = 20;
 
+/// Number of instructions in the ISA
+// FIXME: the value might not be correct. It will be updated when the
+// interpreter is fully implemented.
+pub const INSTRUCTION_SET_SIZE: usize = 47;
+
 /// List all columns used by the interpreter
 pub mod column;
 
