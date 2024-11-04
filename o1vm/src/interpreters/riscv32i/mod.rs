2d222e6133f31b30b588f58a1c49ae5c916fed85
Danny Willems <be.danny.willems@gmail.com>
2024-11-04 11:36:10 +0100

    o1vm/riscv32i: introduce SCRATCH_SIZE and column module

--- o1vm/src/interpreters/riscv32i/mod.rs
+++ o1vm/src/interpreters/riscv32i/mod.rs
@@ -1,2 +1,10 @@
+/// The minimal number of columns required for the VM
+// FIXME: the value will be updated when the interpreter is fully
+// implemented. Using a small value for now.
+pub const SCRATCH_SIZE: usize = 20;
+
+/// List all columns used by the interpreter
+pub mod column;
+
 /// All the registers used by the ISA
 pub mod registers;
