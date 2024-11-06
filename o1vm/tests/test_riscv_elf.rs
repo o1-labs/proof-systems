e5be63f0a6b0265dab18df32056edf9ace71be57
Danny Willems <be.danny.willems@gmail.com>
2024-11-06 12:45:22 +0100

    o1vm/test: testing expected interpreter behavior for no-action

--- o1vm/tests/test_riscv_elf.rs
+++ o1vm/tests/test_riscv_elf.rs
@@ -76,3 +76,30 @@ fn test_instruction_can_be_converted_into_string() {
     let instruction = Instruction::IType(IInstruction::LoadHalf);
     assert_eq!(instruction.to_string(), "lh");
 }
+
+#[test]
+fn test_no_action() {
+    let curr_dir = std::env::current_dir().unwrap();
+    let path = curr_dir.join(std::path::PathBuf::from(
+        "resources/programs/riscv32i/no-action",
+    ));
+    let state = o1vm::elf_loader::parse_riscv32i(&path).unwrap();
+    let mut witness = Env::<Fp>::create(PAGE_SIZE.try_into().unwrap(), state);
+    // This is the output we get by running objdump -d no-action
+    assert_eq!(witness.registers.current_instruction_pointer, 69844);
+    assert_eq!(witness.registers.next_instruction_pointer, 69848);
+
+    (0..=7).for_each(|_| {
+        let instr = witness.step();
+        // li is addi, li is a pseudo instruction
+        assert_eq!(instr, Instruction::IType(IInstruction::AddImmediate))
+    });
+    assert_eq!(witness.registers.general_purpose[10], 0);
+    assert_eq!(witness.registers.general_purpose[11], 0);
+    assert_eq!(witness.registers.general_purpose[12], 0);
+    assert_eq!(witness.registers.general_purpose[13], 0);
+    assert_eq!(witness.registers.general_purpose[14], 0);
+    assert_eq!(witness.registers.general_purpose[15], 0);
+    assert_eq!(witness.registers.general_purpose[16], 0);
+    assert_eq!(witness.registers.general_purpose[17], 42);
+}
