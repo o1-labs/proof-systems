9522828310adca95774c9a30763c48313fc7c4be
dannywillems <dannywillems@users.noreply.github.com>
2024-11-14 14:55:51 +0000

    deploy: 81872e278f7806ee0602311ef4501f0adb71ba23

---
+++ rustdoc/o1vm/interpreters/riscv32i/column/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Column"],"type":["E"]};
--- rustdoc/o1vm/interpreters/riscv32i/sidebar-items.js
+++ rustdoc/o1vm/interpreters/riscv32i/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["registers"]};
+window.SIDEBAR_ITEMS = {"constant":["INSTRUCTION_SET_SIZE","SCRATCH_SIZE"],"mod":["column","registers"]};
