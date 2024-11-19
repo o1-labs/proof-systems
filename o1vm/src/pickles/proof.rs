c3dc7f2514f6c71638ee11f69821e7657824172c
marcbeunardeau88 <marc@o1labs.org>
2024-11-19 10:06:12 +0100

    o1vm/proof: introduce not inversed proof input

--- o1vm/src/pickles/proof.rs
+++ o1vm/src/pickles/proof.rs
@@ -1,7 +1,9 @@
-use kimchi::{curve::KimchiCurve, proof::PointEvaluations};
-use poly_commitment::{ipa::OpeningProof, PolyComm};
-
 use crate::interpreters::mips::{column::N_MIPS_SEL_COLS, witness::SCRATCH_SIZE};
+use ark_ff::Field;
+use kimchi::curve::KimchiCurve;
+use kimchi::proof::PointEvaluations;
+use poly_commitment::{ipa::OpeningProof, PolyComm};
+use std::collections::{HashMap, HashSet};
 
 pub struct WitnessColumns<G, S> {
     pub scratch: [G; SCRATCH_SIZE],
@@ -24,6 +26,62 @@ impl<F: Field> ProofInputs<F> {
                 selector: Vec::with_capacity(domain_size),
             },
         }
+    }
+}
+
+pub struct NotInversedProofInputs<F: Field> {
+    pub evaluations: WitnessColumns<Vec<F>, Vec<F>>,
+    pub idx_to_inverse: Vec<HashSet<usize>>,
+}
+
+impl<F: Field> NotInversedProofInputs<F> {
+    pub fn new(domain_size: usize) -> Self {
+        NotInversedProofInputs {
+            evaluations: ProofInputs::new(domain_size).evaluations,
+            idx_to_inverse: Vec::with_capacity(domain_size),
+        }
+    }
+}
+
+impl<F: Field> From<NotInversedProofInputs<F>> for ProofInputs<F> {
+    fn from(not_inversed: NotInversedProofInputs<F>) -> ProofInputs<F> {
+        // Initialising the result
+        let mut res = ProofInputs::new(not_inversed.evaluations.scratch[1].len());
+        res.evaluations = not_inversed.evaluations;
+        // Collecting the values to inverse
+        let to_inverse_map: &mut HashMap<_, F> = &mut HashMap::new();
+        for (i, set_i) in not_inversed.idx_to_inverse.iter().enumerate() {
+            for &j in set_i.iter() {
+                assert!(res.evaluations.scratch[j][i] != F::zero());
+                // the result is none as this key has not been inserted yet
+                let _none = HashMap::insert(to_inverse_map, (i, j), res.evaluations.scratch[j][i]);
+            }
+        }
+        // Perform the inversion
+        let mut to_inverse_vec: Vec<F> = Vec::with_capacity(to_inverse_map.len());
+        for v in to_inverse_map.values() {
+            to_inverse_vec.push(*v);
+        }
+        for v in &to_inverse_vec {
+            assert!(v != &F::zero());
+        }
+        let to_inverse_slice = to_inverse_vec.as_mut_slice();
+
+        ark_ff::batch_inversion::<F>(to_inverse_slice);
+        // Collecting the inverses and putting them back in the map
+        // we need to create a second map to collect the inverses.
+        // IMPROVEME: We should not need to do this
+        let mut inversed_map = HashMap::new();
+        for (i, (k, _)) in to_inverse_map.iter().enumerate() {
+            // We ignore the old value
+            let _ = inversed_map.insert(*k, to_inverse_slice[i]);
+        }
+
+        // Writting the inversed values in res
+        for (&(i, j), &inv) in inversed_map.iter() {
+            res.evaluations.scratch[j][i] = inv;
+        }
+        res
     }
 }
 
