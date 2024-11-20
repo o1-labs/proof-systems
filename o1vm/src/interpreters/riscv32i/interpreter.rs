4719c61bba087e5bc2fe37763a55b75593ec474e
svv232 <saidcooldude@gmail.com>
2024-11-19 22:50:31 -0500

    adding implementation for stype instructions

--- o1vm/src/interpreters/riscv32i/interpreter.rs
+++ o1vm/src/interpreters/riscv32i/interpreter.rs
@@ -1227,8 +1227,169 @@ pub fn interpret_itype<Env: InterpreterEnv>(env: &mut Env, instr: IInstruction) 
     };
 }
 
-pub fn interpret_stype<Env: InterpreterEnv>(_env: &mut Env, _instr: SInstruction) {
-    unimplemented!("TODO")
+pub fn interpret_stype<Env: InterpreterEnv>(env: &mut Env, instr: SInstruction) {
+    /* fetch instruction pointer from the program state */
+    let instruction_pointer = env.get_instruction_pointer();
+    /* compute the next instruction ptr and add one, as well record raml lookup */
+    let next_instruction_pointer = env.get_next_instruction_pointer();
+    /* read instruction from ip address */
+    let instruction = {
+        let v0 = env.read_memory(&instruction_pointer);
+        let v1 = env.read_memory(&(instruction_pointer.clone() + Env::constant(1)));
+        let v2 = env.read_memory(&(instruction_pointer.clone() + Env::constant(2)));
+        let v3 = env.read_memory(&(instruction_pointer.clone() + Env::constant(3)));
+        (v3 * Env::constant(1 << 24))
+            + (v2 * Env::constant(1 << 16))
+            + (v1 * Env::constant(1 << 8))
+            + v0
+    };
+
+    /* fetch opcode from instruction bit 0 - 6 for a total len of 7 */
+    let opcode = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 7, 0, pos) }
+    };
+    /* verify opcode is 7 bits */
+    env.range_check8(&opcode, 7);
+
+    let imm0_4 = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 12, 7, pos) }
+        // bytes 7-11
+    };
+    env.range_check8(&imm0_4, 5);
+    let funct3 = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 15, 12, pos) }
+    };
+    env.range_check8(&funct3, 3);
+
+    let rs1 = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 20, 15, pos) }
+    };
+    env.range_check8(&rs1, 5);
+    let rs2 = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 25, 20, pos) }
+    };
+    env.range_check8(&rs2, 5);
+
+    let imm5_11 = {
+        let pos = env.alloc_scratch();
+        unsafe { env.bitmask(&instruction, 31, 25, pos) }
+        // bytes 25-31
+    };
+    env.range_check8(&imm5_11, 6);
+
+    // check correctness of decomposition of S type function
+    env.add_constraint(
+        instruction
+           - (opcode.clone() * Env::constant(1 << 0))    // opcode at bits 0-6
+           - (imm0_4.clone() * Env::constant(1 << 7))    // imm0_4 at bits 7-11
+           - (funct3.clone() * Env::constant(1 << 12))   // funct3 at bits 12-14
+           - (rs1.clone() * Env::constant(1 << 15))      // rs1 at bits 15-19
+           - (rs2.clone() * Env::constant(1 << 20))      // rs2 at bits 20-24
+           - (imm5_11.clone() * Env::constant(1 << 25)), // imm5_11 at bits 25-31
+    );
+
+    let local_rs1 = env.read_register(&rs1);
+    let local_imm0_4 = env.sign_extend(&imm0_4, 5);
+    let local_imm5_11 = env.sign_extend(&imm5_11, 7);
+    let local_imm0_11 = {
+        let pos = env.alloc_scratch();
+        let shift_pos = env.alloc_scratch();
+        let shifted_imm5_11 =
+            unsafe { env.shift_left(&local_imm5_11, &Env::constant(5), shift_pos) };
+        let local_imm0_11 = unsafe { env.or_witness(&shifted_imm5_11, &local_imm0_4, pos) };
+        env.sign_extend(&local_imm0_11, 11)
+    };
+    let address = {
+        let address_scratch = env.alloc_scratch();
+        let overflow_scratch = env.alloc_scratch();
+        let (address, _overflow) = unsafe {
+            env.add_witness(
+                &local_rs1,
+                &local_imm0_11,
+                address_scratch,
+                overflow_scratch,
+            )
+        };
+        address
+    };
+    let local_rs2 = env.read_register(&rs2);
+
+    match instr {
+        SInstruction::StoreByte => {
+            // sb: M[x[rs1] + sext(offset)] = x[rs2][7:0]
+            let v0 = {
+                let value_scratch = env.alloc_scratch();
+                unsafe { env.bitmask(&local_rs2, 8, 0, value_scratch) }
+            };
+
+            env.lookup_8bits(&v0);
+            env.write_memory(&address, v0);
+
+            env.set_instruction_pointer(next_instruction_pointer.clone());
+            env.set_next_instruction_pointer(next_instruction_pointer + Env::constant(4u32));
+        }
+        SInstruction::StoreHalf => {
+            // sh: M[x[rs1] + sext(offset)] = x[rs2][15:0]
+            let [v0, v1] = [
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 8, 0, value_scratch) }
+                },
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 16, 8, value_scratch) }
+                },
+            ];
+
+            env.lookup_8bits(&v0);
+            env.lookup_8bits(&v1);
+
+            env.write_memory(&address, v0);
+            env.write_memory(&(address.clone() + Env::constant(1u32)), v1);
+
+            env.set_instruction_pointer(next_instruction_pointer.clone());
+            env.set_next_instruction_pointer(next_instruction_pointer + Env::constant(4u32));
+        }
+        SInstruction::StoreWord => {
+            // sw: M[x[rs1] + sext(offset)] = x[rs2][31:0]
+            let [v0, v1, v2, v3] = [
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 32, 24, value_scratch) }
+                },
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 24, 16, value_scratch) }
+                },
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 16, 8, value_scratch) }
+                },
+                {
+                    let value_scratch = env.alloc_scratch();
+                    unsafe { env.bitmask(&local_rs2, 8, 0, value_scratch) }
+                },
+            ];
+
+            env.lookup_8bits(&v0);
+            env.lookup_8bits(&v1);
+            env.lookup_8bits(&v2);
+            env.lookup_8bits(&v3);
+
+            env.write_memory(&address, v0);
+            env.write_memory(&(address.clone() + Env::constant(1u32)), v1);
+            env.write_memory(&(address.clone() + Env::constant(2u32)), v2);
+            env.write_memory(&(address.clone() + Env::constant(3u32)), v3);
+
+            env.set_instruction_pointer(next_instruction_pointer.clone());
+            env.set_next_instruction_pointer(next_instruction_pointer + Env::constant(4u32));
+        }
+    };
 }
 
 pub fn interpret_sbtype<Env: InterpreterEnv>(_env: &mut Env, _instr: SBInstruction) {
