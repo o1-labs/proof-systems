840066e8613d15a3bd7cfeab8c88ba8aece0eb8f
Danny Willems <be.danny.willems@gmail.com>
2024-11-20 16:24:28 +0100

    o1vm/riscv32: implement mul_lo

--- o1vm/src/interpreters/riscv32im/witness.rs
+++ o1vm/src/interpreters/riscv32im/witness.rs
@@ -505,6 +505,21 @@ impl<Fp: Field> InterpreterEnv for Env<Fp> {
         res
     }
 
+    unsafe fn mul_lo(
+        &mut self,
+        x: &Self::Variable,
+        y: &Self::Variable,
+        position: Self::Position,
+    ) -> Self::Variable {
+        let x: u32 = (*x).try_into().unwrap();
+        let y: u32 = (*y).try_into().unwrap();
+        let res = (x as u64) * (y as u64);
+        let res = (res & ((1 << 32) - 1)) as u32;
+        let res = res as u64;
+        self.write_column(position, res);
+        res
+    }
+
     unsafe fn mul_hi_lo(
         &mut self,
         x: &Self::Variable,
