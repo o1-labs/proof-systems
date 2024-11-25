b0aabaa7ad2c7728983d93e22c1845d6f8ad6e91
dannywillems <dannywillems@users.noreply.github.com>
2024-11-25 18:07:17 +0000

    deploy: 403763e2c824b030b3de5c25950c116984311d9b

--- rustdoc/o1vm/interpreters/mips/column/sidebar-items.js
+++ rustdoc/o1vm/interpreters/mips/column/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["N_MIPS_COLS","N_MIPS_REL_COLS","N_MIPS_SEL_COLS"],"enum":["ColumnAlias"],"type":["MIPSWitness"]};
+window.SIDEBAR_ITEMS = {"constant":["N_MIPS_COLS","N_MIPS_REL_COLS","N_MIPS_SEL_COLS","SCRATCH_SIZE","SCRATCH_SIZE_INVERSE"],"enum":["ColumnAlias"],"type":["MIPSWitness"]};
