db6c0f3047b4fa37e963c67255d168159e2f8ad3
Danny Willems <be.danny.willems@gmail.com>
2024-11-25 15:49:36 +0100

    o1vm/riscv32im: test decoding mul

--- o1vm/src/interpreters/riscv32im/tests.rs
+++ o1vm/src/interpreters/riscv32im/tests.rs
@@ -1,5 +1,5 @@
 use super::{registers::Registers, witness::Env, INSTRUCTION_SET_SIZE, PAGE_SIZE, SCRATCH_SIZE};
-use crate::interpreters::riscv32im::interpreter::{Instruction, RInstruction};
+use crate::interpreters::riscv32im::interpreter::{Instruction, MInstruction, RInstruction};
 use ark_ff::Zero;
 use mina_curves::pasta::Fp;
 use rand::{CryptoRng, Rng, RngCore};
@@ -250,6 +250,29 @@ pub fn generate_random_and_instruction<RNG: RngCore + CryptoRng>(rng: &mut RNG) 
     ]
 }
 
+pub fn generate_random_mul_instruction<RNG: RngCore + CryptoRng>(rng: &mut RNG) -> [u8; 4] {
+    let opcode = 0b0110011;
+    let rd = rng.gen_range(0..32);
+    let funct3 = 0b000;
+    let rs1 = rng.gen_range(0..32);
+    let rs2 = rng.gen_range(0..32);
+    let funct2 = 0b01;
+    let funct5 = 0b00000;
+    let instruction = opcode
+        | (rd << 7)
+        | (funct3 << 12)
+        | (rs1 << 15)
+        | (rs2 << 20)
+        | (funct2 << 25)
+        | (funct5 << 27);
+    [
+        instruction as u8,
+        (instruction >> 8) as u8,
+        (instruction >> 16) as u8,
+        (instruction >> 24) as u8,
+    ]
+}
+
 #[test]
 pub fn test_instruction_decoding_add() {
     let mut env: Env<Fp> = dummy_env();
@@ -384,4 +407,17 @@ pub fn test_instruction_decoding_and() {
     env.memory[0].1[3] = instruction[3];
     let (opcode, _instruction) = env.decode_instruction();
     assert_eq!(opcode, Instruction::RType(RInstruction::And));
+}
+
+#[test]
+pub fn test_instruction_decoding_mul() {
+    let mut env: Env<Fp> = dummy_env();
+    let mut rng = o1_utils::tests::make_test_rng(None);
+    let instruction = generate_random_mul_instruction(&mut rng);
+    env.memory[0].1[0] = instruction[0];
+    env.memory[0].1[1] = instruction[1];
+    env.memory[0].1[2] = instruction[2];
+    env.memory[0].1[3] = instruction[3];
+    let (opcode, _instruction) = env.decode_instruction();
+    assert_eq!(opcode, Instruction::MType(MInstruction::Mul));
 }
