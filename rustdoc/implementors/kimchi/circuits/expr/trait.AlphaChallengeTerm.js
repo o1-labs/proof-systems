ad7f22206bdd8ed8f5524a0340c210efd188bc9e
dannywillems <dannywillems@users.noreply.github.com>
2024-12-11 19:34:07 +0000

    deploy: 644bf15791cdd22840dcdd61abc112e75aaff828

--- rustdoc/implementors/kimchi/circuits/expr/trait.AlphaChallengeTerm.js
+++ rustdoc/implementors/kimchi/circuits/expr/trait.AlphaChallengeTerm.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"arrabiata":[["impl&lt;'a&gt; <a class=\"trait\" href=\"kimchi/circuits/expr/trait.AlphaChallengeTerm.html\" title=\"trait kimchi::circuits::expr::AlphaChallengeTerm\">AlphaChallengeTerm</a>&lt;'a&gt; for <a class=\"enum\" href=\"arrabiata/columns/enum.ChallengeTerm.html\" title=\"enum arrabiata::columns::ChallengeTerm\">ChallengeTerm</a>"]],
+"arrabbiata":[["impl&lt;'a&gt; <a class=\"trait\" href=\"kimchi/circuits/expr/trait.AlphaChallengeTerm.html\" title=\"trait kimchi::circuits::expr::AlphaChallengeTerm\">AlphaChallengeTerm</a>&lt;'a&gt; for <a class=\"enum\" href=\"arrabbiata/columns/enum.ChallengeTerm.html\" title=\"enum arrabbiata::columns::ChallengeTerm\">ChallengeTerm</a>"]],
 "kimchi":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
