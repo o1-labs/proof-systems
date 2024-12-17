94cf0f7d9651ca4c72a02bb2038f759063295b69
martyall <martyall@protonmail.com>
2024-12-16 17:00:06 -0800

    define ocaml conversion types

--- o1vm/src/pickles/proof.rs
+++ o1vm/src/pickles/proof.rs
@@ -39,3 +39,120 @@ pub struct Proof<G: KimchiCurve> {
     /// IPA opening proof
     pub opening_proof: OpeningProof<G>,
 }
+
+#[cfg(feature = "ocaml_types")]
+pub mod caml {
+    use super::{WitnessColumns, ProofInputs, Proof};
+    use ark_ec::AffineRepr;
+    use ocaml;
+
+    #[derive(ocaml::IntoValue, ocaml::FromValue, ocaml_gen::Struct)]
+    pub struct CamlWitnessColumn<G, S> {
+      pub scratch: [G; SCRATCH_SIZE],
+      pub scratch_inverse: [G; SCRATCH_SIZE_INVERSE],
+      pub instruction_counter: G,
+      pub error: G,
+      pub selector: S,
+    }
+
+    impl<CamlG, CamlS> From<WitnessColumns<G>> for CamlWitnessColumns<CamlG, CamlF>
+    where
+        CamlG: From<G>,
+        CamlS: From<S>,
+    {
+        fn from(witness_columns: WitnessColumns<G,S>) -> Self {
+            Self {
+                scratch: witness_columns.scratch.into_iter().map(CamlG::from).collect(),
+                scratch_inverse: witness_columns.scratch_inverse.into_iter().map(CamlG::from).collect(),
+                instruction_counter: CamlG::from(witness_columns.instruction_counter),
+                error: CamlG::from(witness_columns.error),
+                selector: CamlS::from(witness_columns.selector),
+            }
+        }
+    }
+
+    impl<CamlG, CamlS> From<CamlWitnessColumns<CamlG, CamlS>> for WitnessColumns<G,S>
+    where
+        G: From<CamlG>,
+        S: From<CamlS>,
+    {
+        fn from(caml_witness_columns: CamlWitnessColumns<CamlG, CamlS>) -> Self {
+            Self {
+                scratch: caml_witness_columns.scratch.into_iter().map(G::from).collect(),
+                scratch_inverse: caml_witness_columns.scratch_inverse.into_iter().map(G::from).collect(),
+                instruction_counter: G::from(caml_witness_columns.instruction_counter),
+                error: G::from(caml_witness_columns.error),
+                selector: S::from(caml_witness_columns.selector),
+            }
+        }
+    }
+
+    #[derive(ocaml::IntoValue, ocaml::FromValue, ocaml_gen::Struct)]
+    pub struct CamlProofInputs<G> {
+        pub evaluations: CamlWitnessColumns<Vec<G::ScalarField>, Vec<G::ScalarField>>,
+    }
+
+    impl<CamlG> From<ProofInputs<G>> for CamlProofInputs<CamlG>
+    where
+        CamlG: From<G>,
+    {
+        fn from(proof_inputs: ProofInputs<G>) -> Self {
+            Self {
+                evaluations: CamlWitnessColumns::from(proof_inputs.evaluations),
+            }
+        }
+    }
+
+    impl<CamlG> From<CamlProofInputs<CamlG>> for ProofInputs<G>
+    where
+        G: From<CamlG>,
+    {
+        fn from(caml_proof_inputs: CamlProofInputs<CamlG>) -> Self {
+            Self {
+                evaluations: WitnessColumns::from(caml_proof_inputs.evaluations),
+            }
+        }
+    }
+
+    #[derive(ocaml::IntoValue, ocaml::FromValue, ocaml_gen::Struct)]
+    pub struct CamlProof<G> {
+        pub commitments: CamlWitnessColumns<PolyComm<G>, [PolyComm<G>; N_MIPS_SEL_COLS]>,
+        pub zeta_evaluations: CamlWitnessColumns<G::ScalarField, [G::ScalarField; N_MIPS_SEL_COLS]>,
+        pub zeta_omega_evaluations: CamlWitnessColumns<G::ScalarField, [G::ScalarField; N_MIPS_SEL_COLS]>,
+        pub quotient_commitment: PolyComm<G>,
+        pub quotient_evaluations: PointEvaluations<Vec<G::ScalarField>>,
+        pub opening_proof: OpeningProof<G>,
+    }
+
+    impl<CamlG> From<Proof<G>> for CamlProof<CamlG>
+    where
+        CamlG: From<G>,
+    { fn from(proof: Proof<G>) ->
+        Self {
+            Self {
+                commitments: CamlWitnessColumns::from(proof.commitments),
+                zeta_evaluations: CamlWitnessColumns::from(proof.zeta_evaluations),
+                zeta_omega_evaluations: CamlWitnessColumns::from(proof.zeta_omega_evaluations),
+                quotient_commitment: proof.quotient_commitment,
+                quotient_evaluations: proof.quotient_evaluations,
+                opening_proof: proof.opening_proof,
+            }
+        }
+    }
+
+    impl<CamlG> From<CamlProof<CamlG>> for Proof<G>
+    where
+        G: From<CamlG>,
+    {
+        fn from(caml_proof: CamlProof<CamlG>) -> Self {
+            Self {
+                commitments: WitnessColumns::from(caml_proof.commitments),
+                zeta_evaluations: WitnessColumns::from(caml_proof.zeta_evaluations),
+                zeta_omega_evaluations: WitnessColumns::from(caml_proof.zeta_omega_evaluations),
+                quotient_commitment: caml_proof.quotient_commitment,
+                quotient_evaluations: caml_proof.quotient_evaluations,
+                opening_proof: caml_proof.opening_proof,
+            }
+        }
+    }
+}
