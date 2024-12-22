e2b64e30fb06e4c7d8df5b2543a6922007d6aa6d
Danny Willems <be.danny.willems@gmail.com>
2024-12-22 15:25:27 +0100

    o1vm/riscv32: sign_extend - add runtime assert for input invariants

--- o1vm/src/interpreters/riscv32im/interpreter.rs
+++ o1vm/src/interpreters/riscv32im/interpreter.rs
@@ -1388,6 +1388,7 @@ pub trait InterpreterEnv {
     /// Given a variable `x`, this function extends it to a signed integer of
     /// `bitlength` bits.
     fn sign_extend(&mut self, x: &Self::Variable, bitlength: u32) -> Self::Variable {
+        assert!(bitlength <= 32);
         // FIXME: Constrain `high_bit`
         let high_bit = {
             let pos = self.alloc_scratch();
