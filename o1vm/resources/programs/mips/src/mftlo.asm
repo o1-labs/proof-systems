49235d338ff233dc29a2809b0d179f603b657301
Danny Willems <be.danny.willems@gmail.com>
2024-12-23 18:46:20 +0100

    Run make build-mips-programs

---
+++ o1vm/resources/programs/mips/src/mftlo.asm
@@ -0,0 +1,39 @@
+###############################################################################
+# File         : mftlo.asm
+# Project      : MIPS32 MUX
+# Author:      : Grant Ayers (ayers@cs.stanford.edu)
+#
+# Standards/Formatting:
+#   MIPS gas, soft tab, 80 column
+#
+# Description:
+#   Test the functionality of the 'mtlo' and 'mflo' instructions.
+#
+###############################################################################
+
+
+.section .text
+.global __start
+__start:
+    lui     $s0, 0xbfff         # Load the base address 0xbffffff0
+    ori     $s0, 0xfff0
+    ori     $s1, $0, 1          # Prepare the 'done' status
+
+    #### Test code start ####
+
+    lui     $t0, 0xdeaf
+    ori     $t0, 0xbeef
+    mtlo    $t0
+    mflo    $t1
+    subu    $v1, $t0, $t1
+    sltiu   $v0, $v1, 1
+
+    #### Test code end ####
+
+    sw      $v0, 8($s0)         # Set the test result
+    sw      $s1, 4($s0)         # Set 'done'
+
+$done:
+    jr      $ra
+    nop
+
---
+++ o1vm/resources/programs/mips/src/slti.asm
@@ -0,0 +1,37 @@
+###############################################################################
+# File         : slti.asm
+# Project      : MIPS32 MUX
+# Author:      : Grant Ayers (ayers@cs.stanford.edu)
+#
+# Standards/Formatting:
+#   MIPS gas, soft tab, 80 column
+#
+# Description:
+#   Test the functionality of the 'slti' instruction.
+#
+###############################################################################
+
+
+.section .text
+.global __start
+__start:
+    lui     $s0, 0xbfff         # Load the base address 0xbffffff0
+    ori     $s0, 0xfff0
+    ori     $s1, $0, 1          # Prepare the 'done' status
+
+    #### Test code start ####
+
+    lui     $t0, 0x8000
+    slti    $v0, $t0, 0xffff
+    slti    $v1, $t0, 0
+    and     $v0, $v0, $v1
+
+    #### Test code end ####
+
+    sw      $v0, 8($s0)         # Set the test result
+    sw      $s1, 4($s0)         # Set 'done'
+
+$done:
+    jr      $ra
+    nop
+
