9d209f8ba05aaea62235287ec66c4312f86ce2d6
dannywillems <dannywillems@users.noreply.github.com>
2024-12-23 20:01:45 +0000

    deploy: 6c81d899daa5970e404eeaaa98e265866e046a38

--- rustdoc/o1vm/interpreters/riscv32im/registers/sidebar-items.js
+++ rustdoc/o1vm/interpreters/riscv32im/registers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["N_GP_REGISTERS","REGISTER_CURRENT_IP","REGISTER_HEAP_POINTER","REGISTER_NEXT_IP"],"struct":["Registers"]};
+window.SIDEBAR_ITEMS = {"constant":["N_GP_REGISTERS","REGISTER_CURRENT_IP","REGISTER_HEAP_POINTER","REGISTER_NEXT_IP"],"enum":["RegisterAlias"],"struct":["Registers"]};
