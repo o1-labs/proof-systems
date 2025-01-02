1abb3ecc201dba3b8bfd94d88ae96eba76ec909b
martyall <martyall@users.noreply.github.com>
2025-01-02 20:07:21 +0000

    deploy: eb80434af9b4b250aa0ef8d0276b1b35e19e3586

--- rustdoc/o1vm/preimage_oracle/sidebar-items.js
+++ rustdoc/o1vm/preimage_oracle/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_bidirectional_channel"],"struct":["PreImageOracle","RW","ReadWrite"],"trait":["PreImageOracleT"]};
+window.SIDEBAR_ITEMS = {"fn":["create_bidirectional_channel"],"struct":["NullPreImageOracle","PreImageOracle","RW","ReadWrite"],"trait":["PreImageOracleT"]};
