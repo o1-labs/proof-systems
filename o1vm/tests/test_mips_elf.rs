367517d1f7e1f3eaabcd100ad7e6ca0b0f3d07c0
martyall <martyall@protonmail.com>
2025-01-06 13:28:59 -0800

    run the open mips tests that we can manage

--- o1vm/tests/test_mips_elf.rs
+++ o1vm/tests/test_mips_elf.rs
@@ -6,71 +6,105 @@ use o1vm::{
     interpreters::mips::witness::{self},
     preimage_oracle::{NullPreImageOracle, PreImageOracleT},
 };
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
-fn parse_state(fp: String) -> State {
-    let curr_dir = std::env::current_dir().unwrap();
-    let path = curr_dir.join(std::path::PathBuf::from(fp));
-    o1vm::elf_loader::parse_elf(Architecture::Mips, &path).unwrap()
+struct MipsTest {
+    bin_file: PathBuf,
 }
 
-fn read_word<Fp: Field, T: PreImageOracleT>(env: &mut witness::Env<Fp, T>, addr: u32) -> u32 {
-    let bytes: [u8; 4] = [
-        env.get_memory_direct(addr),
-        env.get_memory_direct(addr + 1),
-        env.get_memory_direct(addr + 2),
-        env.get_memory_direct(addr + 3),
-    ];
-    u32::from_be_bytes(bytes)
+// currently excluding any oracle based tests and a select group of tests that are failing
+fn is_test_excluded(bin_file: &Path) -> bool {
+    let file_name = bin_file.file_name().unwrap().to_str().unwrap();
+    let untested_programs = ["exit_group", "jal", "mul", "jalr"];
+    file_name.starts_with("oracle") || untested_programs.contains(&file_name)
 }
 
-fn test_single_register_result(prgoram_name: &str, expected: u32) {
-    let base_dir = "resources/programs/mips/bin/";
-    // this choice was taken from the cannon state_test.go, it is arbitrary
-    let halt_address = 0xa7ef00d0_u32;
-    let bin_file = format!("{}{}", base_dir, prgoram_name);
-    let mut state = parse_state(bin_file);
-    // each of the open mips tests end execution with the instruciton 'jr $ra'. As in cannon, we can
-    // set $ra to a specific value and test the program counter to signal the program has terminated.
-    state.registers[31] = halt_address;
-    let start = cannon::Start::create(state.step as usize);
-    let configuration = VmConfiguration {
-        halt_address: Some(halt_address),
-        ..Default::default()
-    };
-    let mut witness = witness::Env::<Fp, Box<dyn PreImageOracleT>>::create(
-        cannon::PAGE_SIZE as usize,
-        state,
-        Box::new(NullPreImageOracle),
-    );
-    while !witness.halt {
-        witness.step(&configuration, &None, &start);
+impl MipsTest {
+    fn parse_state(&self) -> State {
+        let curr_dir = std::env::current_dir().unwrap();
+        let path = curr_dir.join(&self.bin_file);
+        o1vm::elf_loader::parse_elf(Architecture::Mips, &path).unwrap()
     }
-    // If you look at the tests, this is always used as a base address to find the registers for
-    // signaling the program exit flag and the return result
-    let return_register = 0xbffffff0_u32;
 
-    let done_register = return_register + 4;
-    assert_eq!(
-        read_word(&mut witness, return_register + 4),
-        1,
-        "Expected done register to be set to 1, got {}",
-        done_register
-    );
+    fn read_word<Fp: Field, T: PreImageOracleT>(env: &mut witness::Env<Fp, T>, addr: u32) -> u32 {
+        let bytes: [u8; 4] = [
+            env.get_memory_direct(addr),
+            env.get_memory_direct(addr + 1),
+            env.get_memory_direct(addr + 2),
+            env.get_memory_direct(addr + 3),
+        ];
+        u32::from_be_bytes(bytes)
+    }
 
-    let result_register = return_register + 8;
-    assert_eq!(
-        read_word(&mut witness, result_register),
-        expected,
-        "Program {} failure: expected result register to be {}, got {}",
-        prgoram_name,
-        expected,
-        result_register
-    );
-    println!("Program {} passed", prgoram_name);
+    fn run(&self) -> Result<(), String> {
+        let mut state = self.parse_state();
+        let halt_address = 0xa7ef00d0_u32;
+        state.registers[31] = halt_address;
+
+        let start = cannon::Start::create(state.step as usize);
+        let configuration = VmConfiguration {
+            halt_address: Some(halt_address),
+            ..Default::default()
+        };
+
+        let mut witness = witness::Env::<Fp, Box<dyn PreImageOracleT>>::create(
+            cannon::PAGE_SIZE as usize,
+            state,
+            Box::new(NullPreImageOracle),
+        );
+
+        while !witness.halt {
+            witness.step(&configuration, &None, &start);
+        }
+
+        let return_register = 0xbffffff0_u32;
+        let done_register = return_register + 4;
+        let result_register = return_register + 8;
+
+        let done_value = Self::read_word(&mut witness, done_register);
+        if done_value != 1 {
+            return Err(format!(
+                "Expected done register to be set to 1, got {:#x}",
+                done_value
+            ));
+        }
+
+        let result_value = Self::read_word(&mut witness, result_register);
+        if result_value != 1 {
+            return Err(format!(
+                "Program {:?} failure: expected result register to contain 1, got {:#x}",
+                self.bin_file, result_value
+            ));
+        }
+
+        Ok(())
+    }
 }
 
-#[test]
-fn open_mips_tests_execution() {
-    test_single_register_result("nor", 0x1);
-    test_single_register_result("jr", 0x1);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg_attr(not(feature = "open_mips"), ignore)]
+    fn open_mips_tests() {
+        let test_dir = "resources/programs/mips/bin";
+        let test_files: Vec<MipsTest> = fs::read_dir(test_dir)
+            .unwrap_or_else(|_| panic!("Error reading directory {}", test_dir))
+            .filter_map(|entry| entry.ok())
+            .map(|entry| entry.path())
+            .filter(|f| f.is_file() && f.extension().is_none() && !is_test_excluded(f))
+            .map(|f| MipsTest { bin_file: f })
+            .collect();
+
+        for test in test_files {
+            let test_name = test.bin_file.file_name().unwrap().to_str().unwrap();
+            if let Err(err) = test.run() {
+                panic!("Test '{}' failed: {}", test_name, err);
+            }
+        }
+    }
 }
