05a6e0f01a38d96cf996914aff2934d2e0e0b824
dannywillems <dannywillems@users.noreply.github.com>
2025-01-08 16:28:03 +0000

    deploy: 74cc7c887e1357e301f6c4659b1082cc80c9d471

--- rustdoc/implementors/folding/expressions/trait.FoldingColumnTrait.js
+++ rustdoc/implementors/folding/expressions/trait.FoldingColumnTrait.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "folding":[],
-"kimchi_msm":[["impl <a class=\"trait\" href=\"folding/expressions/trait.FoldingColumnTrait.html\" title=\"trait folding::expressions::FoldingColumnTrait\">FoldingColumnTrait</a> for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>"]]
+"kimchi_msm":[["impl FoldingColumnTrait for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
