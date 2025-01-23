06d33dbb88b6ff73d6b4092adedc100b983ae80b
martyall <martyall@protonmail.com>
2025-01-23 14:46:25 -0800

    add fold_commitments

--- saffron/src/commitment.rs
+++ saffron/src/commitment.rs
@@ -1,9 +1,17 @@
-use ark_poly::{Evaluations, Radix2EvaluationDomain as D};
-use poly_commitment::{commitment::CommitmentCurve, ipa::SRS, PolyComm, SRS as _};
-use rayon::prelude::*;
-
 use crate::blob::FieldBlob;
+use ark_ec::AffineRepr;
+use ark_ff::One;
+use ark_poly::{Evaluations, Radix2EvaluationDomain as D};
+use mina_poseidon::FqSponge;
+use poly_commitment::{
+    commitment::{absorb_commitment, CommitmentCurve},
+    ipa::SRS,
+    PolyComm, SRS as _,
+};
+use rayon::prelude::*;
+use tracing::instrument;
 
+#[instrument(skip_all)]
 pub fn commit_to_field_elems<G: CommitmentCurve>(
     srs: &SRS<G>,
     domain: D<G::ScalarField>,
@@ -18,15 +26,39 @@ pub fn commit_to_field_elems<G: CommitmentCurve>(
         .collect()
 }
 
+#[instrument(skip_all)]
 pub fn commit_to_blob<G: CommitmentCurve>(
     srs: &SRS<G>,
-    blob: FieldBlob<G::ScalarField>,
+    blob: &FieldBlob<G::ScalarField>,
 ) -> Vec<PolyComm<G>> {
     let num_chunks = 1;
     blob.data
         .par_iter()
         .map(|p| srs.commit_non_hiding(p, num_chunks))
         .collect()
+}
+
+#[instrument(skip_all)]
+pub fn fold_commitments<
+    G: AffineRepr,
+    EFqSponge: Clone + FqSponge<G::BaseField, G, G::ScalarField>,
+>(
+    sponge: &mut EFqSponge,
+    commitments: &[PolyComm<G>],
+) -> PolyComm<G> {
+    for commitment in commitments {
+        absorb_commitment(sponge, commitment)
+    }
+    let challenge = sponge.challenge();
+    let powers: Vec<G::ScalarField> = commitments
+        .iter()
+        .scan(G::ScalarField::one(), |acc, _| {
+            let res = *acc;
+            *acc *= challenge;
+            Some(res)
+        })
+        .collect::<Vec<_>>();
+    PolyComm::multi_scalar_mul(&commitments.iter().collect::<Vec<_>>(), &powers)
 }
 
 #[cfg(test)]
@@ -53,10 +85,10 @@ mod tests {
         fn test_user_and_storage_provider_commitments_equal(xs in prop::collection::vec(any::<u8>(), 0..=2 * Fp::size_in_bytes() * DOMAIN.size())
         )
           { let elems = encode_for_domain(&*DOMAIN, &xs);
-            let user_commitment = commit_to_field_elems(&*SRS, *DOMAIN, elems);
+            let user_commitments = commit_to_field_elems(&*SRS, *DOMAIN, elems);
             let blob = FieldBlob::<Fp>::encode(*DOMAIN, &xs);
-            let storeage_provider_commitment = commit_to_blob(&*SRS, blob);
-            prop_assert_eq!(user_commitment, storeage_provider_commitment);
+            let storeage_provider_commitments = commit_to_blob(&*SRS, &blob);
+            prop_assert_eq!(user_commitments, storeage_provider_commitments);
           }
         }
 }
