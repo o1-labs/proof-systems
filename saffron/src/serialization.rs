7b1d77ca1be1029f79ed4876428b9f273126b154
martyall <martyall@protonmail.com>
2025-01-23 11:51:43 -0800

    factor out utils module

--- saffron/src/serialization.rs
+++ saffron/src/serialization.rs
@@ -1,4 +1,5 @@
-use ark_ff::{BigInteger, Field, PrimeField};
+use crate::utils::{decode_into, encode_for_domain};
+use ark_ff::{Field, PrimeField};
 use ark_poly::{univariate::DensePolynomial, EvaluationDomain, Evaluations};
 use ark_serialize::{
     CanonicalDeserialize, CanonicalSerialize, Compress, Read, SerializationError, Valid, Validate,
@@ -7,17 +8,6 @@ use ark_serialize::{
 use o1_utils::FieldHelpers;
 use rayon::prelude::*;
 use tracing::{debug, instrument};
-
-// For injectivity, you can only use this on inputs of length at most
-// 'F::MODULUS_BIT_SIZE / 8', e.g. for Vesta this is 31.
-fn encode<Fp: PrimeField>(bytes: &[u8]) -> Fp {
-    Fp::from_be_bytes_mod_order(bytes)
-}
-
-fn decode_into<Fp: PrimeField>(buffer: &mut [u8], x: Fp) {
-    let bytes = x.into_bigint().to_bytes_be();
-    buffer.copy_from_slice(&bytes);
-}
 
 // A FieldBlob<F> represents the encoding of a Vec<u8> as a list of polynomials over F,
 // where F is a prime field. The polyonomials are represented in the monomial basis.
@@ -77,20 +67,11 @@ impl<F: CanonicalDeserialize + Field> CanonicalDeserialize for FieldBlob<F> {
 impl<F: PrimeField> FieldBlob<F> {
     #[instrument(skip_all)]
     pub fn encode<D: EvaluationDomain<F>>(domain: D, bytes: &[u8]) -> FieldBlob<F> {
-        let n = (F::MODULUS_BIT_SIZE / 8) as usize;
+        let field_elements = encode_for_domain(&domain, bytes);
         let domain_size = domain.size();
 
-        let field_elements = bytes
-            .chunks(n)
-            .map(|chunk| {
-                let mut bytes = vec![0u8; n];
-                bytes[..chunk.len()].copy_from_slice(chunk);
-                encode(&bytes)
-            })
-            .collect::<Vec<_>>();
-
         let data: Vec<DensePolynomial<F>> = field_elements
-            .par_chunks(domain_size)
+            .par_iter()
             .map(|chunk| Evaluations::from_vec_and_domain(chunk.to_vec(), domain).interpolate())
             .collect();
 
@@ -139,38 +120,9 @@ impl<F: PrimeField> FieldBlob<F> {
 mod tests {
     use super::*;
     use ark_poly::Radix2EvaluationDomain;
-    use ark_std::UniformRand;
     use mina_curves::pasta::Fp;
     use once_cell::sync::Lazy;
     use proptest::prelude::*;
-
-    fn decode<Fp: PrimeField>(x: Fp) -> Vec<u8> {
-        let mut buffer = vec![0u8; Fp::size_in_bytes()];
-        decode_into(&mut buffer, x);
-        buffer
-    }
-
-    // Check that [u8] -> Fp -> [u8] is the identity function.
-    proptest! {
-        #[test]
-        fn test_round_trip_from_bytes(xs in any::<[u8;31]>())
-          { let n : Fp = encode(&xs);
-            let ys : [u8; 31] = decode(n).as_slice()[1..32].try_into().unwrap();
-            prop_assert_eq!(xs, ys);
-          }
-    }
-
-    // Check that Fp -> [u8] -> Fp is the identity function.
-    proptest! {
-        #[test]
-        fn test_round_trip_from_fp(
-            x in prop::strategy::Just(Fp::rand(&mut ark_std::rand::thread_rng()))
-        ) {
-            let bytes = decode(x);
-            let y = encode(&bytes);
-            prop_assert_eq!(x,y);
-        }
-    }
 
     static DOMAIN: Lazy<Radix2EvaluationDomain<Fp>> = Lazy::new(|| {
         const SRS_SIZE: usize = 1 << 16;
@@ -181,7 +133,7 @@ mod tests {
     proptest! {
     #![proptest_config(ProptestConfig::with_cases(20))]
     #[test]
-    fn test_round_trip_blob_encoding( xs in any::<Vec<u8>>())
+    fn test_round_trip_blob_encoding( xs in prop::collection::vec(any::<u8>(), 0..=2 * DOMAIN.size()))
       { let blob = FieldBlob::<Fp>::encode(*DOMAIN, &xs);
         let mut buf = Vec::new();
         blob.serialize_compressed(&mut buf).unwrap();
