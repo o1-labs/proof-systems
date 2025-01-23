cc5a7180a97403f23ae82081596cc4fe4d76e01d
martyall <martyall@users.noreply.github.com>
2025-01-23 19:23:16 +0000

    deploy: 48a8ebcff5889a8ff551ce02b1e5a86c5d6ff0a9

--- rustdoc/implementors/ark_serialize/trait.CanonicalSerialize.js
+++ rustdoc/implementors/ark_serialize/trait.CanonicalSerialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"saffron":[["impl&lt;F: CanonicalSerialize&gt; CanonicalSerialize for <a class=\"struct\" href=\"saffron/serialization/struct.FieldBlob.html\" title=\"struct saffron::serialization::FieldBlob\">FieldBlob</a>&lt;F&gt;"]]
+"saffron":[["impl&lt;F: CanonicalSerialize + Field&gt; CanonicalSerialize for <a class=\"struct\" href=\"saffron/serialization/struct.FieldBlob.html\" title=\"struct saffron::serialization::FieldBlob\">FieldBlob</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdoc/implementors/ark_serialize/trait.Valid.js
+++ rustdoc/implementors/ark_serialize/trait.Valid.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"saffron":[["impl&lt;F: Valid&gt; Valid for <a class=\"struct\" href=\"saffron/serialization/struct.FieldBlob.html\" title=\"struct saffron::serialization::FieldBlob\">FieldBlob</a>&lt;F&gt;"]]
+"saffron":[["impl&lt;F: Valid + Field&gt; Valid for <a class=\"struct\" href=\"saffron/serialization/struct.FieldBlob.html\" title=\"struct saffron::serialization::FieldBlob\">FieldBlob</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
