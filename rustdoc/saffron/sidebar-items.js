a7a7c833e7ec3085b0cf7e56cd41a4789a8b9946
dannywillems <dannywillems@users.noreply.github.com>
2025-02-10 19:13:15 +0000

    deploy: 28fc543fb705f8beec5d8f97eaa4d637dd3a1351

--- rustdoc/saffron/sidebar-items.js
+++ rustdoc/saffron/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["blob","cli","commitment","env","proof","utils"]};
+window.SIDEBAR_ITEMS = {"mod":["blob","cli","commitment","diff","env","proof","utils"]};
--- rustdoc/saffron/utils/sidebar-items.js
+++ rustdoc/saffron/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["QueryError"],"fn":["decode_into","encode","encode_as_field_elements","encode_for_domain"],"struct":["QueryBytes","QueryField"]};
+window.SIDEBAR_ITEMS = {"enum":["QueryError"],"fn":["chunk_size_in_bytes","decode_into","encode","encode_as_field_elements","encode_for_domain","min_encoding_chunks"],"struct":["QueryBytes","QueryField"]};
