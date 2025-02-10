cadf895cb51b866c50704bcd1bc4c11bec5817a9
marcbeunardeau88 <marc@o1labs.org>
2025-02-10 11:01:54 +0100

    o1vm/lookup: add prover

--- o1vm/src/pickles/mod.rs
+++ o1vm/src/pickles/mod.rs
@@ -19,6 +19,8 @@ pub mod verifier;
 
 ///Lookup related modules
 pub mod lookup_columns;
+pub mod lookup_prover;
+
 /// Maximum degree of the constraints.
 /// It does include the additional degree induced by the multiplication of the
 /// selectors.
