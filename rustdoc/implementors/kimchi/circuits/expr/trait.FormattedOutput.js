8d3fb3c71543e26b38503af4eecb3a6f5efb45c0
dannywillems <dannywillems@users.noreply.github.com>
2025-02-14 13:23:11 +0000

    deploy: 28a9cee6a7ddd957ef5e7f45ad162296f784c2bb

--- rustdoc/implementors/kimchi/circuits/expr/trait.FormattedOutput.js
+++ rustdoc/implementors/kimchi/circuits/expr/trait.FormattedOutput.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "arrabbiata":[["impl FormattedOutput for <a class=\"enum\" href=\"arrabbiata/columns/enum.Column.html\" title=\"enum arrabbiata::columns::Column\">Column</a>"]],
 "kimchi":[],
-"kimchi_msm":[["impl FormattedOutput for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.usize.html\">usize</a>&gt;"]]
+"kimchi_msm":[["impl <a class=\"trait\" href=\"kimchi/circuits/expr/trait.FormattedOutput.html\" title=\"trait kimchi::circuits::expr::FormattedOutput\">FormattedOutput</a> for <a class=\"enum\" href=\"kimchi_msm/columns/enum.Column.html\" title=\"enum kimchi_msm::columns::Column\">Column</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.usize.html\">usize</a>&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
