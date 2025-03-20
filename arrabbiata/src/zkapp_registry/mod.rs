aabde713576fa58aeae9f8f2490f41ba69c144ab
Danny Willems <be.danny.willems@gmail.com>
2025-03-20 20:06:26 +0100

    Arrabbiata: implement and document a ZkApp trait

---
+++ arrabbiata/src/zkapp_registry/mod.rs
@@ -0,0 +1,114 @@
+use ark_ff::PrimeField;
+
+use crate::{curve::ArrabbiataCurve, interpreter::InterpreterEnv};
+
+/// A ZkApp is a program that can be executed and proven using a
+/// (zero-knowledge) succinct non-interactive argument of knowledge or in short
+/// a zkSNARK. In particular, the interface is designed to be used with the
+/// Arrabbiata accumulation scheme and its corresponding decider.
+///
+/// A ZkApp is defined over a list of instructions (of type `Instruction`),
+/// where each instruction is a step of the computation. The computation is
+/// defined by the control-flow of the ZkApp, which is defined by the methods
+/// [Self::fetch_next_instruction] and [Self::fetch_instruction].
+/// An instruction is considered to be filling only one row of the execution
+/// trace.
+///
+/// A list of instructions sharing the same constraints is called a gadget (of
+/// type `Gadget`). Each instruction must be convertible to a gadget, therefore
+/// the type restriction to `From<Instruction>`. It will be used in particular
+/// by the method [setup] to build the list of selectors.
+///
+/// An instruction can also transport some data, which can be used to guide the
+/// control-flow and to provide additional information to the interpreter while
+/// executing [Self::run].
+/// For instance, a gadget could be `EllipticCurveScaling`, which could be
+/// formed by a set of instructions `EllipticCurveScaling(bit)` where `bit`
+/// defines the bit of the scalar that is being processed.
+///
+/// A ZkApp structure is responsible to provide a dummy witness, used to
+/// generate a first non-folded instance. The dummy witness is a satisfying
+/// execution trace for dummy inputs.
+pub trait ZkApp<C: ArrabbiataCurve, Instruction: Copy, Gadget: From<Instruction>>
+where
+    C::BaseField: PrimeField,
+{
+    /// Provide a dummy witness, used to generate a first non-folded instance.
+    fn dummy_witness(&self, srs_size: usize) -> Vec<Vec<C::ScalarField>>;
+
+    /// Fetch the first instruction to execute.
+    fn fetch_instruction(&self) -> Instruction;
+
+    /// Describe the control-flow of the ZkApp.
+    /// This function should return the next instruction to execute after
+    /// `current_instr`.
+    ///
+    /// If the current instruction is the last one, it should return `None`.
+    ///
+    /// The method is going to be called by the [execute] function and [setup]
+    /// function.
+    fn fetch_next_instruction(&self, current_instr: Instruction) -> Option<Instruction>;
+
+    /// Execute the instruction `instr` over the interpreter environment `E`.
+    ///
+    /// The interpreter environment is responsible to keep track of the
+    /// execution trace, and to provide the necessary values to the ZkApp.
+    ///
+    /// The method is going to be called by the [execute] function, which is
+    /// responsible to build the whole execution trace, instruction by
+    /// instruction. The stoppingcondition is when the
+    /// [Self::fetch_next_instruction] returns `None`.
+    fn run<E: InterpreterEnv>(&self, env: &mut E, instr: Instruction);
+}
+
+/// Execute the ZkApp `zkapp` over the interpreter environment `env`.
+/// This is a generic function that can be used to execute any ZkApp.
+pub fn execute<
+    E: InterpreterEnv,
+    C: ArrabbiataCurve,
+    Instruction: Copy,
+    Gadget: From<Instruction>,
+    Z: ZkApp<C, Instruction, Gadget>,
+>(
+    zkapp: &Z,
+    env: &mut E,
+) where
+    C::BaseField: PrimeField,
+{
+    let mut instr: Option<Instruction> = Some(zkapp.fetch_instruction());
+    while let Some(i) = instr {
+        zkapp.run(env, i);
+        env.reset();
+        instr = zkapp.fetch_next_instruction(i);
+    }
+}
+
+/// Create a setup for the ZkApp.
+///
+/// The setup will define the shape of the execution trace.
+/// It is mostly consisting of the list of selectors that are used to select the
+/// columns that are used in the computation, and how constrained they are.
+///
+/// For now, the concept of gadget and selectors are mixed together. We
+/// should separate them in the future to allow more flexibility.
+pub fn setup<
+    C: ArrabbiataCurve,
+    Instruction: Copy,
+    Gadget: From<Instruction>,
+    Z: ZkApp<C, Instruction, Gadget>,
+>(
+    zkapp: &Z,
+) -> Vec<Gadget>
+where
+    C::BaseField: PrimeField,
+{
+    let mut circuit: Vec<Gadget> = vec![];
+    let mut instr: Option<Instruction> = Some(zkapp.fetch_instruction());
+    while let Some(i) = instr {
+        circuit.push(Gadget::from(i));
+        instr = zkapp.fetch_next_instruction(i);
+    }
+    circuit
+}
+
+pub mod verifier;
