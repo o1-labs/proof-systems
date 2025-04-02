a79271ed1269c239c27c0e6d6d2c8b5f5eea1cdd
Matt Walker <matt.g.d.walker@gmail.com>
2025-04-02 13:26:28 -0400

    Remove useless tests

--- kimchi/benches/expr.rs
+++ kimchi/benches/expr.rs
@@ -69,63 +69,8 @@ fn benchmark_expr_evaluations(c: &mut Criterion) {
 
     let expr: E<Fp> = constraints_expr(None, true).0;
 
-    c.bench_function("expr_evals_vec", |b| {
-        b.iter(|| black_box(expr.clone()).evaluations(black_box(&env)));
-    });
-}
-
-fn benchmark_expr_evaluations_iter(c: &mut Criterion) {
-    let domains = EvaluationDomains::<Fp>::create(1 << 16).unwrap();
-    let domain = domains.d8;
-    let mut rng = rand::thread_rng();
-    let randomized_witness = (0..15)
-        .map(|_| create_random_evaluation(domain, &mut rng))
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-    let randomized_coefficients = (0..15)
-        .map(|_| create_random_evaluation(domain, &mut rng))
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-    let randomized_vanishes_on_zero_knowledge_and_previous_rows =
-        create_random_evaluation(domain, &mut rng);
-    let randomized_z = create_random_evaluation(domain, &mut rng);
-    let randomized_l0_1 = Fp::rand(&mut rng);
-    let constants = Constants {
-        endo_coefficient: Fp::rand(&mut rng),
-        mds: &Vesta::sponge_params().mds,
-        zk_rows: 0,
-    };
-    let challenges = BerkeleyChallenges {
-        alpha: Fp::rand(&mut rng),
-        beta: Fp::rand(&mut rng),
-        gamma: Fp::rand(&mut rng),
-        joint_combiner: Fp::rand(&mut rng),
-    };
-
-    let env = Environment {
-        witness: &randomized_witness,
-        coefficient: &randomized_coefficients,
-        vanishes_on_zero_knowledge_and_previous_rows:
-            &randomized_vanishes_on_zero_knowledge_and_previous_rows,
-        z: &randomized_z,
-        index: HashMap::new(),
-        l0_1: randomized_l0_1,
-        constants,
-        challenges,
-        domain: domains,
-        lookup: None,
-    };
-
-    let expr: E<Fp> = constraints_expr(None, true).0;
-    let iter = expr.evaluations_iter(&env);
-    // iter.populate_cache();
-
     c.bench_function("expr_evals_par", |b| {
-        b.iter(|| {
-            black_box(&iter).par_collect();
-        })
+        b.iter(|| black_box(expr.clone()).evaluations(black_box(&env)));
     });
 }
 
@@ -135,10 +80,4 @@ criterion_group!(
     targets = benchmark_expr_evaluations
 );
 
-criterion_group!(
-    name = evaluation_bench_par;
-    config = Criterion::default().sample_size(10);
-    targets = benchmark_expr_evaluations_iter
-);
-
-criterion_main!(evaluation_bench_seq, evaluation_bench_par);
+criterion_main!(evaluation_bench_seq);
