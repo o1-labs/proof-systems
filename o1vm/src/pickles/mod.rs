563a0ee5c5d603f639434d40933a288121e751f4
marcbeunardeau88 <marc@o1labs.org>
2025-04-28 11:51:17 +0200

    o1vm/lookup: adding multiplicities_verifier

--- o1vm/src/pickles/mod.rs
+++ o1vm/src/pickles/mod.rs
@@ -24,6 +24,7 @@ pub mod lookup_prover;
 pub mod lookup_verifier;
 pub mod multiplicities_columns;
 pub mod multiplicities_prover;
+pub mod multiplicities_verifier;
 
 /// Maximum degree of the constraints.
 /// It does include the additional degree induced by the multiplication of the
