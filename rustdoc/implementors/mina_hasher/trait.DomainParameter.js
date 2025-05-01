14bff84dbe2d66ddb50036ec6b0a227fb8e38b54
dannywillems <dannywillems@users.noreply.github.com>
2025-05-01 17:18:02 +0000

    deploy: 047f3519110c1e02ff61715d672728c8547f7f90

--- rustdoc/implementors/mina_hasher/trait.DomainParameter.js
+++ rustdoc/implementors/mina_hasher/trait.DomainParameter.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "mina_hasher":[],
-"mina_signer":[["impl <a class=\"trait\" href=\"mina_hasher/trait.DomainParameter.html\" title=\"trait mina_hasher::DomainParameter\">DomainParameter</a> for <a class=\"enum\" href=\"mina_signer/enum.NetworkId.html\" title=\"enum mina_signer::NetworkId\">NetworkId</a>"]]
+"mina_signer":[["impl DomainParameter for <a class=\"enum\" href=\"mina_signer/enum.NetworkId.html\" title=\"enum mina_signer::NetworkId\">NetworkId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
