c433017c97549b2f7a8d785abb0bc0713f241778
volhovm <volhovm@users.noreply.github.com>
2025-05-13 13:53:46 +0000

    deploy: 5926415a21a34525e1f545183d721b7013326fec

--- rustdoc/poly_commitment/sidebar-items.js
+++ rustdoc/poly_commitment/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["commitment","error","hash_map_cache","ipa","kzg","pbt_srs","utils"],"trait":["OpenProof","SRS"]};
+window.SIDEBAR_ITEMS = {"mod":["commitment","error","hash_map_cache","ipa","kzg","pbt_srs","precomputed_srs","utils"],"trait":["OpenProof","SRS"]};
