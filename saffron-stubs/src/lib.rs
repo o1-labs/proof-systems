a1a59677dc03d1d0fc181809508c45e7c204c6f8
Danny Willems <be.danny.willems@gmail.com>
2025-05-21 20:17:51 +0200

    Saffron-stubs: sketch structure

---
+++ saffron-stubs/src/lib.rs
@@ -0,0 +1,7 @@
+/// Exposes the symbol caml_saffron_prove in the static library
+/// libsaffron_stubs.a.
+#[ocaml_gen::func]
+#[ocaml::func]
+pub fn caml_saffron_prove() {
+    unimplemented!("TODO")
+}
