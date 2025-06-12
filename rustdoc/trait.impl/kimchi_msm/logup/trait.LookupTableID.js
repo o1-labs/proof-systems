ad821701e7577fe08219e4f92ca9b3fc9175a3e5
volhovm <volhovm@users.noreply.github.com>
2025-06-12 15:00:08 +0000

    deploy: 6a197f6f0c011f85071aa8c40342317e0ab7ebe8

--- rustdoc/trait.impl/kimchi_msm/logup/trait.LookupTableID.js
+++ rustdoc/trait.impl/kimchi_msm/logup/trait.LookupTableID.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["ivc",[["impl&lt;Ff: PrimeField&gt; <a class=\"trait\" href=\"kimchi_msm/logup/trait.LookupTableID.html\" title=\"trait kimchi_msm::logup::LookupTableID\">LookupTableID</a> for <a class=\"enum\" href=\"ivc/ivc/lookups/enum.IVCLookupTable.html\" title=\"enum ivc::ivc::lookups::IVCLookupTable\">IVCLookupTable</a>&lt;Ff&gt;"]]],["kimchi_msm",[]],["o1vm",[["impl <a class=\"trait\" href=\"kimchi_msm/logup/trait.LookupTableID.html\" title=\"trait kimchi_msm::logup::LookupTableID\">LookupTableID</a> for <a class=\"enum\" href=\"o1vm/lookups/enum.LookupTableIDs.html\" title=\"enum o1vm::lookups::LookupTableIDs\">LookupTableIDs</a>"]]]]);
+    var implementors = Object.fromEntries([["ivc",[["impl&lt;Ff: PrimeField&gt; LookupTableID for <a class=\"enum\" href=\"ivc/ivc/lookups/enum.IVCLookupTable.html\" title=\"enum ivc::ivc::lookups::IVCLookupTable\">IVCLookupTable</a>&lt;Ff&gt;"]]],["kimchi_msm",[]],["o1vm",[["impl LookupTableID for <a class=\"enum\" href=\"o1vm/lookups/enum.LookupTableIDs.html\" title=\"enum o1vm::lookups::LookupTableIDs\">LookupTableIDs</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[327,18,290]}
+//{"start":57,"fragment_lengths":[204,18,167]}
