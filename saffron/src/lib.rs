250de96c53c19295027cf5a454cc70ecaba4f267
Anne-Laure <anne-laure@o1labs.org>
2025-07-03 19:24:34 +0200

    Saffron: switch curve to Pallas

--- saffron/src/lib.rs
+++ saffron/src/lib.rs
@@ -19,9 +19,9 @@ pub use mina_poseidon::FqSponge as Sponge;
 
 pub const SRS_SIZE: usize = 1 << 16;
 
-pub type Curve = mina_curves::pasta::Vesta;
-pub type ProjectiveCurve = mina_curves::pasta::ProjectiveVesta;
-pub type CurveParameters = mina_curves::pasta::VestaParameters;
+pub type Curve = mina_curves::pasta::Pallas;
+pub type ProjectiveCurve = mina_curves::pasta::ProjectivePallas;
+pub type CurveParameters = mina_curves::pasta::PallasParameters;
 pub type ScalarField = <CurveParameters as ark_ec::CurveConfig>::ScalarField;
 pub type BaseField = <CurveParameters as ark_ec::CurveConfig>::BaseField;
 
