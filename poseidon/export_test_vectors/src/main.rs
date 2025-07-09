fdcdb179bb14a587930aef630bba6e843d2f6fae
Danny Willems <be.danny.willems@gmail.com>
2025-07-09 10:12:27 +0200

    export-test-vectors: use Clap

--- poseidon/export_test_vectors/src/main.rs
+++ poseidon/export_test_vectors/src/main.rs
@@ -1,12 +1,12 @@
+use clap::{Parser, ValueEnum};
 use core::str::FromStr;
 use std::{
-    env,
     fs::File,
     io::{self, Write},
 };
 mod vectors;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, ValueEnum)]
 pub enum Mode {
     B10,
     Hex,
@@ -24,7 +24,7 @@ impl FromStr for Mode {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, ValueEnum)]
 pub enum ParamType {
     Legacy,
     Kimchi,
@@ -42,45 +42,32 @@ impl FromStr for ParamType {
     }
 }
 
-/// Usage:
-/// cargo run --all-features \
-///           --bin export_test_vectors -- \
-///           [b10|hex]
-///           [legacy|kimchi]
-///           <OUTPUT_FILE>
+#[derive(Parser)]
+#[command(name = "export_test_vectors")]
+#[command(about = "Export test vectors for the mina-poseidon crate")]
+struct Args {
+    /// Output format for the test vectors
+    #[arg(value_enum)]
+    mode: Mode,
+
+    /// Parameter type to use
+    #[arg(value_enum)]
+    param_type: ParamType,
+
+    /// Output file path, use "-" for stdout
+    output_file: String,
+}
+
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        4 => {
-            // parse command-line args
-            let mode: Mode = args
-                .get(1)
-                .expect("missing mode")
-                .parse()
-                .expect("invalid mode");
-            let param_type: ParamType = args
-                .get(2)
-                .expect("missing param type")
-                .parse()
-                .expect("invalid param type");
-            let output_file = args.get(3).expect("missing file");
+    let args = Args::parse();
 
-            // generate vectors
-            let vectors = vectors::generate(mode, param_type);
+    // generate vectors
+    let vectors = vectors::generate(args.mode, args.param_type);
 
-            // save to output file
-            let writer: Box<dyn Write> = match output_file.as_str() {
-                "-" => Box::new(io::stdout()),
-                _ => Box::new(File::create(output_file).expect("could not create file")),
-            };
-            serde_json::to_writer_pretty(writer, &vectors).expect("could not write to file");
-        }
-        _ => {
-            println!(
-                "usage: cargo run -p export_test_vectors -- [{:?}|{:?}] [legacy|kimchi] <OUTPUT_FILE>",
-                Mode::Hex,
-                Mode::B10,
-            );
-        }
-    }
+    // save to output file
+    let writer: Box<dyn Write> = match args.output_file.as_str() {
+        "-" => Box::new(io::stdout()),
+        _ => Box::new(File::create(&args.output_file).expect("could not create file")),
+    };
+    serde_json::to_writer_pretty(writer, &vectors).expect("could not write to file");
 }
