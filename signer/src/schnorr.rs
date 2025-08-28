7c04470d9cbd8e3d75d8c1f2e9fb6843329fd1a8
Danny Willems <be.danny.willems@gmail.com>
2025-08-28 13:35:10 +0200

    Revert "fix: append_scalar()"
    This reverts commit ca8482d981bd82684e804216fa6a4e260caaeaaa.

--- signer/src/schnorr.rs
+++ signer/src/schnorr.rs
@@ -176,7 +176,7 @@ impl<H: 'static + Hashable> Schnorr<H> {
             .to_roinput()
             .append_field(kp.public.point().x)
             .append_field(kp.public.point().y)
-            .append_scalar(kp.secret.scalar().clone())
+            .append_field(BaseField::from(kp.secret.scalar().into_bigint()))
             .append_bytes(&[network_id_value]); // Network ID as packed 8 bits
 
         // Get packed fields
