a0b9e163b0b9ebe979306fe8e663033037ad4927
querolita <anais.querol@gmail.com>
2025-09-22 20:01:32 +0200

    napi: poseidon ffi

---
+++ plonk-napi/src/lib.rs
@@ -0,0 +1,6 @@
+mod poseidon;
+
+pub use poseidon::{
+    caml_pasta_fp_poseidon_block_cipher,
+    caml_pasta_fq_poseidon_block_cipher,
+};
---
+++ plonk-napi/src/poseidon.rs
@@ -0,0 +1,52 @@
+use arkworks::{WasmPastaFp, WasmPastaFq};
+use mina_curves::pasta::{Fp, Fq};
+use mina_poseidon::{constants::PlonkSpongeConstantsKimchi, permutation::poseidon_block_cipher};
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+use wasm_types::{FlatVector, FlatVectorElem};
+
+// fp
+
+#[napi]
+pub fn caml_pasta_fp_poseidon_block_cipher(state: Uint8Array) -> Result<Vec<u8>> {
+    let mut state: Vec<Fp> = FlatVector::<WasmPastaFp>::from_bytes(state.to_vec())
+        .into_iter()
+        .map(Into::into)
+        .collect();
+
+    poseidon_block_cipher::<Fp, PlonkSpongeConstantsKimchi>(
+        mina_poseidon::pasta::fp_kimchi::static_params(),
+        &mut state,
+    );
+
+    let res = state
+        .into_iter()
+        .map(WasmPastaFp)
+        .flat_map(FlatVectorElem::flatten)
+        .collect();
+
+    Ok(res)
+}
+
+// fq
+
+#[napi]
+pub fn caml_pasta_fq_poseidon_block_cipher(state: Uint8Array) -> Result<Vec<u8>> {
+    let mut state: Vec<Fq> = FlatVector::<WasmPastaFq>::from_bytes(state.to_vec())
+        .into_iter()
+        .map(Into::into)
+        .collect();
+
+    poseidon_block_cipher::<Fq, PlonkSpongeConstantsKimchi>(
+        mina_poseidon::pasta::fq_kimchi::static_params(),
+        &mut state,
+    );
+
+    let res = state
+        .into_iter()
+        .map(WasmPastaFq)
+        .flat_map(FlatVectorElem::flatten)
+        .collect();
+
+    Ok(res)
+}
