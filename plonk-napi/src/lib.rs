e25e75de7b9ca1835e7c3b3fa8c741bbc9459986
Anaïs Querol <anais.querol@gmail.com>
2025-09-30 20:12:10 +0200

    napi: wrapper for field types

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,8 +1,9 @@
 mod circuit;
 mod poseidon;
 mod types;
+mod wrappers;
 
 pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
-
+pub use wrappers::field::{WasmPastaFp, WasmPastaFq};
 pub use circuit::prover_to_json;
 pub use types::{prover_index_from_bytes, prover_index_to_bytes, WasmPastaFpPlonkIndex};
