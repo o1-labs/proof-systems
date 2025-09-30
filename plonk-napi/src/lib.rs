a5d32fee40f2f30d51fbfbe5ee788c9aa220a772
Anaïs Querol <anais.querol@gmail.com>
2025-09-30 20:14:53 +0200

    napi: wrapper for group types

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -7,3 +7,4 @@ pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_b
 pub use wrappers::field::{WasmPastaFp, WasmPastaFq};
 pub use circuit::prover_to_json;
 pub use types::{prover_index_from_bytes, prover_index_to_bytes, WasmPastaFpPlonkIndex};
+pub use wrappers::group::{WasmGPallas, WasmGVesta};
