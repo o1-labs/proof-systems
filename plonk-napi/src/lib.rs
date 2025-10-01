8638b153d054385d8621f4f7309e9ea9f39e47eb
Anaïs Querol <anais.querol@gmail.com>
2025-10-01 13:24:13 +0200

    napi: impls for polycomm

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -3,8 +3,10 @@ mod poseidon;
 mod types;
 mod wrappers;
 mod wasm_vector;
+mod poly_comm;
 
 pub use circuit::prover_to_json;
+pub use poly_comm::{pallas::WasmFqPolyComm, vesta::WasmFpPolyComm};
 pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
 pub use types::{prover_index_from_bytes, prover_index_to_bytes, WasmPastaFpPlonkIndex};
 pub use wasm_vector::{fp::WasmVecVecFp, fq::WasmVecVecFq};
