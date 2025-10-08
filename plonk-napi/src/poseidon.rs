2a07e99eb6cbef041a5fa918e3c4638659ac5734
Florian <florian@technotro.com>
2025-10-08 15:51:37 +0700

    dummy logs

--- plonk-napi/src/poseidon.rs
+++ plonk-napi/src/poseidon.rs
@@ -9,6 +9,8 @@ use wasm_types::{FlatVector, FlatVectorElem};
 
 #[napi]
 pub fn caml_pasta_fp_poseidon_block_cipher(state: Uint8Array) -> Result<Uint8Array> {
+    println!("from native rust");
+
     let mut state_vec: Vec<Fp> = FlatVector::<WasmPastaFp>::from_bytes(state.to_vec())
         .into_iter()
         .map(Into::into)
@@ -32,6 +34,8 @@ pub fn caml_pasta_fp_poseidon_block_cipher(state: Uint8Array) -> Result<Uint8Arr
 
 #[napi]
 pub fn caml_pasta_fq_poseidon_block_cipher(state: Uint8Array) -> Result<Uint8Array> {
+    println!("from native rust");
+
     let mut state_vec: Vec<Fq> = FlatVector::<WasmPastaFq>::from_bytes(state.to_vec())
         .into_iter()
         .map(Into::into)
