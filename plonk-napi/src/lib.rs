a9e249bfcd06438f66f7f68096e35e84724b6e23
querolita <anais.querol@gmail.com>
2025-10-15 17:02:51 +0200

    napi: export contents of gate_vector module

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -7,6 +7,22 @@ mod wasm_vector;
 mod poly_comm;
 
 pub use circuit::prover_to_json;
+pub use gate_vector::{
+    fp::{
+        caml_pasta_fp_plonk_circuit_digest, caml_pasta_fp_plonk_circuit_serialize,
+        caml_pasta_fp_plonk_gate_vector_add, caml_pasta_fp_plonk_gate_vector_create,
+        caml_pasta_fp_plonk_gate_vector_digest, caml_pasta_fp_plonk_gate_vector_get,
+        caml_pasta_fp_plonk_gate_vector_len, caml_pasta_fp_plonk_gate_vector_wrap,
+        NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector,
+    },
+    fq::{
+        caml_pasta_fq_plonk_circuit_digest, caml_pasta_fq_plonk_circuit_serialize,
+        caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
+        caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
+        caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
+        NapiFqGate as WasmFqGate, NapiFqGateVector as WasmFqGateVector,
+    },
+};
 pub use poly_comm::{pallas::WasmFqPolyComm, vesta::WasmFpPolyComm};
 pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
 pub use types::{prover_index_from_bytes, prover_index_to_bytes, WasmPastaFpPlonkIndex};
