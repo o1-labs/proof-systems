c74e69a3ff70d42411ddd3ab3e5fd99dc2fa19dd
Florian <florian@technotro.com>
2025-10-15 21:05:34 +0700

    explose and rename modules

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,10 +1,12 @@
 mod circuit;
 mod gate_vector;
+mod pasta_fp_plonk_index;
+mod pasta_fq_plonk_index;
+mod poly_comm;
 mod poseidon;
 mod types;
 mod wrappers;
 mod wasm_vector;
-mod poly_comm;
 
 pub use circuit::prover_to_json;
 pub use gate_vector::{
@@ -18,8 +20,13 @@ pub use gate_vector::{
     NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector, NapiFqGate as WasmFqGate,
     NapiFqGateVector as WasmFqGateVector,
 };
+pub use pasta_fp_plonk_index::{
+    prover_index_fp_from_bytes, prover_index_fp_to_bytes, WasmPastaFpPlonkIndex,
+};
+pub use pasta_fq_plonk_index::{
+    prover_index_fq_from_bytes, prover_index_fq_to_bytes, WasmPastaFqPlonkIndex,
+};
 pub use poly_comm::{pallas::WasmFqPolyComm, vesta::WasmFpPolyComm};
 pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
-pub use types::{prover_index_from_bytes, prover_index_to_bytes, WasmPastaFpPlonkIndex};
 pub use wasm_vector::{fp::WasmVecVecFp, fq::WasmVecVecFq};
 pub use wrappers::{field::{WasmPastaFp, WasmPastaFq}, group::{WasmGPallas, WasmGVesta}};
