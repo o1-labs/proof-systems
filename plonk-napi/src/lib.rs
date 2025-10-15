50a96dd654dfb3eb7a36d8fa70a02454090ecf86
querolita <anais.querol@gmail.com>
2025-10-15 17:02:51 +0200

    napi: export contents of gate_vector module

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -4,11 +4,24 @@ pub(crate) mod poseidon;
 pub(crate) mod wasm_vector;
 pub(crate) mod wrappers;
 
-pub use poseidon::{
-    caml_pasta_fp_poseidon_block_cipher,
-    caml_pasta_fq_poseidon_block_cipher,
-};
+pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
 
-pub use wrappers::group::{WasmGPallas, WasmGVesta};
-pub use wasm_vector::{fp::WasmVecVecFp, fq::WasmVecVecFq};
+pub use gate_vector::{
+    fp::{
+        caml_pasta_fp_plonk_circuit_digest, caml_pasta_fp_plonk_circuit_serialize,
+        caml_pasta_fp_plonk_gate_vector_add, caml_pasta_fp_plonk_gate_vector_create,
+        caml_pasta_fp_plonk_gate_vector_digest, caml_pasta_fp_plonk_gate_vector_get,
+        caml_pasta_fp_plonk_gate_vector_len, caml_pasta_fp_plonk_gate_vector_wrap,
+        NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector,
+    },
+    fq::{
+        caml_pasta_fq_plonk_circuit_digest, caml_pasta_fq_plonk_circuit_serialize,
+        caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
+        caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
+        caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
+        NapiFqGate as WasmFqGate, NapiFqGateVector as WasmFqGateVector,
+    },
+};
 pub use poly_comm::{pallas::WasmFqPolyComm, vesta::WasmFpPolyComm};
+pub use wasm_vector::{fp::WasmVecVecFp, fq::WasmVecVecFq};
+pub use wrappers::group::{WasmGPallas, WasmGVesta};
