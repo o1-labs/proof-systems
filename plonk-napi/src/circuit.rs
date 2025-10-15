9286836d14d030ee7b669bf0a8a29125e7ad1ab4
Florian <florian@technotro.com>
2025-10-15 18:42:52 +0700

    re-implement `circuit.rs `

---
+++ plonk-napi/src/circuit.rs
@@ -0,0 +1,36 @@
+use ark_ff::PrimeField;
+use kimchi::circuits::{constraints::ConstraintSystem, gate::CircuitGate};
+use mina_curves::pasta::Fp;
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+use serde::Serialize;
+
+use crate::types::WasmPastaFpPlonkIndex;
+
+#[derive(Serialize)]
+struct Circuit<F>
+where
+    F: PrimeField,
+{
+    public_input_size: usize,
+    #[serde(bound = "CircuitGate<F>: Serialize")]
+    gates: Vec<CircuitGate<F>>,
+}
+
+impl<F> From<&ConstraintSystem<F>> for Circuit<F>
+where
+    F: PrimeField,
+{
+    fn from(cs: &ConstraintSystem<F>) -> Self {
+        Self {
+            public_input_size: cs.public,
+            gates: cs.gates.to_vec(),
+        }
+    }
+}
+
+#[napi]
+pub fn prover_to_json(prover_index: External<WasmPastaFpPlonkIndex>) -> String {
+    let circuit: Circuit<Fp> = prover_index.0.cs.as_ref().into();
+    serde_json::to_string(&circuit).expect("couldn't serialize constraints")
+}
