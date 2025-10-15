31db09d34cc3d4626fd0808dd1d4f81af2a9791e
querolita <anais.querol@gmail.com>
2025-10-15 18:12:39 +0200

    napi: fix compilation of gate_vector

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -8,20 +8,15 @@ mod poly_comm;
 
 pub use circuit::prover_to_json;
 pub use gate_vector::{
-    fp::{
-        caml_pasta_fp_plonk_circuit_digest, caml_pasta_fp_plonk_circuit_serialize,
-        caml_pasta_fp_plonk_gate_vector_add, caml_pasta_fp_plonk_gate_vector_create,
-        caml_pasta_fp_plonk_gate_vector_digest, caml_pasta_fp_plonk_gate_vector_get,
-        caml_pasta_fp_plonk_gate_vector_len, caml_pasta_fp_plonk_gate_vector_wrap,
-        NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector,
-    },
-    fq::{
-        caml_pasta_fq_plonk_circuit_digest, caml_pasta_fq_plonk_circuit_serialize,
-        caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
-        caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
-        caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
-        NapiFqGate as WasmFqGate, NapiFqGateVector as WasmFqGateVector,
-    },
+    caml_pasta_fp_plonk_circuit_serialize, caml_pasta_fp_plonk_gate_vector_add,
+    caml_pasta_fp_plonk_gate_vector_create, caml_pasta_fp_plonk_gate_vector_digest,
+    caml_pasta_fp_plonk_gate_vector_get, caml_pasta_fp_plonk_gate_vector_len,
+    caml_pasta_fp_plonk_gate_vector_wrap, caml_pasta_fq_plonk_circuit_serialize,
+    caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
+    caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
+    caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
+    NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector, NapiFqGate as WasmFqGate,
+    NapiFqGateVector as WasmFqGateVector,
 };
 pub use poly_comm::{pallas::WasmFqPolyComm, vesta::WasmFpPolyComm};
 pub use poseidon::{caml_pasta_fp_poseidon_block_cipher, caml_pasta_fq_poseidon_block_cipher};
