18dabea68beb183b1f49b35b9764c84f5b269fe8
leon chou <leon@o1labs.org>
2025-10-21 16:20:56 -0400

    cargo

---
+++ plonk-napi/src/canary.rs
@@ -0,0 +1,5 @@
+use napi_derive::napi;
+
+
+#[napi]
+pub const CANARY_VALUE:u64 = 1337;
--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,6 +1,11 @@
 mod poseidon;
+mod canary;
 
 pub use poseidon::{
     caml_pasta_fp_poseidon_block_cipher,
     caml_pasta_fq_poseidon_block_cipher,
+};
+
+pub use canary::{
+  CANARY_VALUE,
 };
