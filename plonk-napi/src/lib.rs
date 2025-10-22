a590c8523e24ea1c5ac1386b1c356e38f5781a0e
Florian <florian@technotro.com>
2025-10-22 15:45:13 +0700

    make plonk-napi compile by reusing gate_vector types

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,23 +1,8 @@
 mod circuit;
-mod gate_vector;
 mod pasta_fp_plonk_index;
 mod pasta_fq_plonk_index;
 mod srs;
 mod tables;
-
-pub use gate_vector::{
-    caml_pasta_fp_plonk_circuit_serialize, caml_pasta_fp_plonk_gate_vector_add,
-    caml_pasta_fp_plonk_gate_vector_create, caml_pasta_fp_plonk_gate_vector_digest,
-    caml_pasta_fp_plonk_gate_vector_from_bytes, caml_pasta_fp_plonk_gate_vector_get,
-    caml_pasta_fp_plonk_gate_vector_len, caml_pasta_fp_plonk_gate_vector_to_bytes,
-    caml_pasta_fp_plonk_gate_vector_wrap, caml_pasta_fq_plonk_circuit_serialize,
-    caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
-    caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_from_bytes,
-    caml_pasta_fq_plonk_gate_vector_get, caml_pasta_fq_plonk_gate_vector_len,
-    caml_pasta_fq_plonk_gate_vector_wrap, GateVectorHandleFp, GateVectorHandleFq, JsGateFp,
-    JsGateFq, JsGateWires, JsWire, NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector,
-    NapiFqGate as WasmFqGate, NapiFqGateVector as WasmFqGateVector,
-};
 
 pub use tables::{JsLookupTableFp, JsLookupTableFq, JsRuntimeTableCfgFp, JsRuntimeTableCfgFq};
 
