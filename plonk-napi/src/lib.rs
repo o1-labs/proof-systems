d7f7afc3e28f7b3b3ab431caf5dedd5cb505869c
querolita <anais.querol@gmail.com>
2025-10-23 19:30:12 +0200

    napi: get rid of gate vector to use external from plonk-wasm

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,5 +1,4 @@
 mod circuit;
-mod gate_vector;
 mod pasta_fp_plonk_index;
 mod pasta_fq_plonk_index;
 mod poly_comm;
@@ -9,17 +8,6 @@ mod tables;
 mod wrappers;
 mod wasm_vector;
 
-pub use gate_vector::{
-    caml_pasta_fp_plonk_circuit_serialize, caml_pasta_fp_plonk_gate_vector_add,
-    caml_pasta_fp_plonk_gate_vector_create, caml_pasta_fp_plonk_gate_vector_digest,
-    caml_pasta_fp_plonk_gate_vector_get, caml_pasta_fp_plonk_gate_vector_len,
-    caml_pasta_fp_plonk_gate_vector_wrap, caml_pasta_fq_plonk_circuit_serialize,
-    caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
-    caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
-    caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
-    NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector, NapiFqGate as WasmFqGate,
-    NapiFqGateVector as WasmFqGateVector,
-};
 pub use pasta_fp_plonk_index::{
     prover_index_fp_from_bytes, prover_index_fp_to_bytes, WasmPastaFpPlonkIndex,
 };
