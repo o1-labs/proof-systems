561ecf20138bc0954fcda89d04a130df69e7a79e
Florian <florian@technotro.com>
2025-10-26 12:34:31 +0700

    add deserialize function for WasmProverProof

--- plonk-wasm/src/plonk_proof.rs
+++ plonk-wasm/src/plonk_proof.rs
@@ -623,6 +623,15 @@ macro_rules! impl_proof {
             }
 
             #[wasm_bindgen]
+            #[allow(deprecated)]
+            pub fn deserialize(s: &str) -> WasmProverProof {
+                let bytes = base64::decode(s).unwrap();
+                let proof: ProverProof<$G, OpeningProof<$G>> = rmp_serde::from_slice(&bytes).unwrap();
+                // Deprecated used on purpose: updating this leads to a bug in o1js
+                WasmProverProof::from((proof, Vec::new()))
+            }
+
+            #[wasm_bindgen]
             pub struct [<Wasm $field_name:camel RuntimeTable>] {
                 id: i32,
                 data: WasmFlatVector<$WasmF>
