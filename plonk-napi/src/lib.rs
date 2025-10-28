d205c85eaf46685260ea228bf8008e07deb22123
querolita <anais.querol@gmail.com>
2025-10-28 20:01:33 +0100

    napi: reintroduce gate vector

--- plonk-napi/src/lib.rs
+++ plonk-napi/src/lib.rs
@@ -1,4 +1,5 @@
 mod circuit;
+mod gate_vector;
 mod pasta_fp_plonk_index;
 mod pasta_fq_plonk_index;
 mod poly_comm;
@@ -8,6 +9,18 @@ mod tables;
 mod wasm_vector;
 mod wrappers;
 
+pub use circuit::prover_to_json;
+pub use gate_vector::{
+    caml_pasta_fp_plonk_circuit_serialize, caml_pasta_fp_plonk_gate_vector_add,
+    caml_pasta_fp_plonk_gate_vector_create, caml_pasta_fp_plonk_gate_vector_digest,
+    caml_pasta_fp_plonk_gate_vector_get, caml_pasta_fp_plonk_gate_vector_len,
+    caml_pasta_fp_plonk_gate_vector_wrap, caml_pasta_fq_plonk_circuit_serialize,
+    caml_pasta_fq_plonk_gate_vector_add, caml_pasta_fq_plonk_gate_vector_create,
+    caml_pasta_fq_plonk_gate_vector_digest, caml_pasta_fq_plonk_gate_vector_get,
+    caml_pasta_fq_plonk_gate_vector_len, caml_pasta_fq_plonk_gate_vector_wrap,
+    NapiFpGate as WasmFpGate, NapiFpGateVector as WasmFpGateVector, NapiFqGate as WasmFqGate,
+    NapiFqGateVector as WasmFqGateVector,
+};
 pub use pasta_fp_plonk_index::{
     prover_index_fp_from_bytes, prover_index_fp_to_bytes, WasmPastaFpPlonkIndex,
 };
