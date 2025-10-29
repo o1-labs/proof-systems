8583558c977289541af6229fca7414d166103f05
Danny Willems <be.danny.willems@gmail.com>
2025-10-29 18:31:34 +0800

    fixup! Test

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -153,7 +153,7 @@ fn build_kimchi_stubs(target_dir: Option<&str>, offline: bool) -> Result<()> {
 }
 
 fn build_wasm(out_dir: &str, target: Target, rust_version: RustVersion) -> Result<()> {
-    const RUSTFLAGS: &str = "-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--max-memory=4294967296";
+    const RUSTFLAGS: &str = "-C linker-flavor=wasm-lld -C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--max-memory=4294967296";
 
     let cargo_target_dir = env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
     let artifact_dir = PathBuf::from(format!("{cargo_target_dir}/bin"));
