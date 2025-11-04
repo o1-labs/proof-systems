fb3f2337c18019e88dd08c85570fd10f0e35bda7
querolita <anais.querol@gmail.com>
2025-11-04 23:49:39 +0100

    napi: use existing polycomm in verifier index

--- plonk-napi/src/plonk_verifier_index/fp.rs
+++ plonk-napi/src/plonk_verifier_index/fp.rs
@@ -1,12 +1,11 @@
+use crate::{poly_comm::vesta::NapiFpPolyComm, wrappers::lookups::NapiLookupInfo};
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain as Domain};
 use ark_serialize::CanonicalSerialize;
 use kimchi::circuits::polynomials::permutation::Shifts as KimchiShifts;
 use mina_curves::pasta::Fp;
-use napi::bindgen_prelude::{Error, Result as NapiResult, Status};
+use napi::bindgen_prelude::{Error, Status};
 use napi_derive::napi;
 use serde::{Deserialize, Serialize};
-
-use crate::wrappers::lookups::NapiLookupInfo;
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
@@ -17,14 +16,7 @@ pub struct WasmFpDomain {
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpPolyComm {
-    pub unshifted: Vec<Vec<u8>>,
-    pub shifted: Option<Vec<u8>>,
-}
-
-#[napi(object)]
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpShifts {
+pub struct NapiFpShifts {
     pub s0: Vec<u8>,
     pub s1: Vec<u8>,
     pub s2: Vec<u8>,
@@ -36,59 +28,61 @@ pub struct WasmFpShifts {
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpLookupSelectors {
-    pub xor: Option<WasmFpPolyComm>,
-    pub lookup: Option<WasmFpPolyComm>,
-    pub range_check: Option<WasmFpPolyComm>,
-    pub ffmul: Option<WasmFpPolyComm>,
+pub struct NapiFpLookupSelectors {
+    pub xor: Option<NapiFpPolyComm>,
+    pub lookup: Option<NapiFpPolyComm>,
+    pub range_check: Option<NapiFpPolyComm>,
+    pub ffmul: Option<NapiFpPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpLookupVerifierIndex {
+pub struct NapiFpLookupVerifierIndex {
     pub joint_lookup_used: bool,
-    pub lookup_table: Vec<WasmFpPolyComm>,
-    pub lookup_selectors: WasmFpLookupSelectors,
-    pub table_ids: Option<WasmFpPolyComm>,
+    pub lookup_table: Vec<NapiFpPolyComm>,
+    pub lookup_selectors: NapiFpLookupSelectors,
+    pub table_ids: Option<NapiFpPolyComm>,
     pub lookup_info: NapiLookupInfo,
-    pub runtime_tables_selector: Option<WasmFpPolyComm>,
+    pub runtime_tables_selector: Option<NapiFpPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpPlonkVerificationEvals {
-    pub sigma_comm: Vec<WasmFpPolyComm>,
-    pub coefficients_comm: Vec<WasmFpPolyComm>,
-    pub generic_comm: WasmFpPolyComm,
-    pub psm_comm: WasmFpPolyComm,
-    pub complete_add_comm: WasmFpPolyComm,
-    pub mul_comm: WasmFpPolyComm,
-    pub emul_comm: WasmFpPolyComm,
-    pub endomul_scalar_comm: WasmFpPolyComm,
-    pub xor_comm: Option<WasmFpPolyComm>,
-    pub range_check0_comm: Option<WasmFpPolyComm>,
-    pub range_check1_comm: Option<WasmFpPolyComm>,
-    pub foreign_field_add_comm: Option<WasmFpPolyComm>,
-    pub foreign_field_mul_comm: Option<WasmFpPolyComm>,
-    pub rot_comm: Option<WasmFpPolyComm>,
+pub struct NapiFpPlonkVerificationEvals {
+    pub sigma_comm: Vec<NapiFpPolyComm>,
+    pub coefficients_comm: Vec<NapiFpPolyComm>,
+    pub generic_comm: NapiFpPolyComm,
+    pub psm_comm: NapiFpPolyComm,
+    pub complete_add_comm: NapiFpPolyComm,
+    pub mul_comm: NapiFpPolyComm,
+    pub emul_comm: NapiFpPolyComm,
+    pub endomul_scalar_comm: NapiFpPolyComm,
+    pub xor_comm: Option<NapiFpPolyComm>,
+    pub range_check0_comm: Option<NapiFpPolyComm>,
+    pub range_check1_comm: Option<NapiFpPolyComm>,
+    pub foreign_field_add_comm: Option<NapiFpPolyComm>,
+    pub foreign_field_mul_comm: Option<NapiFpPolyComm>,
+    pub rot_comm: Option<NapiFpPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFpPlonkVerifierIndex {
+pub struct NapiFpPlonkVerifierIndex {
     pub domain: WasmFpDomain,
     pub max_poly_size: i32,
     pub public_: i32,
     pub prev_challenges: i32,
     pub srs: Vec<u8>,
-    pub evals: WasmFpPlonkVerificationEvals,
-    pub shifts: WasmFpShifts,
-    pub lookup_index: Option<WasmFpLookupVerifierIndex>,
+    pub evals: NapiFpPlonkVerificationEvals,
+    pub shifts: NapiFpShifts,
+    pub lookup_index: Option<NapiFpLookupVerifierIndex>,
     pub zk_rows: i32,
 }
 
 #[napi]
-pub fn caml_pasta_fp_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<WasmFpShifts> {
+pub fn caml_pasta_fp_plonk_verifier_index_shifts(
+    log2_size: i32,
+) -> napi::bindgen_prelude::Result<NapiFpShifts> {
     println!(
         "from napi! caml_pasta_fp_plonk_verifier_index_shifts with log2_size {}",
         log2_size
@@ -101,7 +95,7 @@ pub fn caml_pasta_fp_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<W
     let shifts = KimchiShifts::new(&domain);
     let s = shifts.shifts();
 
-    Ok(WasmFpShifts {
+    Ok(NapiFpShifts {
         s0: serialize_fp(&s[0])?,
         s1: serialize_fp(&s[1])?,
         s2: serialize_fp(&s[2])?,
@@ -112,7 +106,7 @@ pub fn caml_pasta_fp_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<W
     })
 }
 
-fn serialize_fp(value: &Fp) -> NapiResult<Vec<u8>> {
+fn serialize_fp(value: &Fp) -> napi::bindgen_prelude::Result<Vec<u8>> {
     let mut bytes = Vec::new();
     value
         .serialize_compressed(&mut bytes)
--- plonk-napi/src/plonk_verifier_index/fq.rs
+++ plonk-napi/src/plonk_verifier_index/fq.rs
@@ -1,29 +1,22 @@
-use crate::wrappers::lookups::NapiLookupInfo;
+use crate::{poly_comm::pallas::NapiFqPolyComm, wrappers::lookups::NapiLookupInfo};
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain as Domain};
 use ark_serialize::CanonicalSerialize;
 use kimchi::circuits::polynomials::permutation::Shifts as KimchiShifts;
 use mina_curves::pasta::Fq;
-use napi::bindgen_prelude::{Error, Result as NapiResult, Status};
+use napi::bindgen_prelude::{Error, Status};
 use napi_derive::napi;
 use serde::{Deserialize, Serialize};
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqDomain {
+pub struct NapiFqDomain {
     pub log_size_of_group: i32,
     pub group_gen: Vec<u8>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqPolyComm {
-    pub unshifted: Vec<Vec<u8>>,
-    pub shifted: Option<Vec<u8>>,
-}
-
-#[napi(object)]
-#[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqShifts {
+pub struct NapiFqShifts {
     pub s0: Vec<u8>,
     pub s1: Vec<u8>,
     pub s2: Vec<u8>,
@@ -35,59 +28,61 @@ pub struct WasmFqShifts {
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqLookupSelectors {
-    pub xor: Option<WasmFqPolyComm>,
-    pub lookup: Option<WasmFqPolyComm>,
-    pub range_check: Option<WasmFqPolyComm>,
-    pub ffmul: Option<WasmFqPolyComm>,
+pub struct NapiFqLookupSelectors {
+    pub xor: Option<NapiFqPolyComm>,
+    pub lookup: Option<NapiFqPolyComm>,
+    pub range_check: Option<NapiFqPolyComm>,
+    pub ffmul: Option<NapiFqPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqLookupVerifierIndex {
+pub struct NapiFqLookupVerifierIndex {
     pub joint_lookup_used: bool,
-    pub lookup_table: Vec<WasmFqPolyComm>,
-    pub lookup_selectors: WasmFqLookupSelectors,
-    pub table_ids: Option<WasmFqPolyComm>,
+    pub lookup_table: Vec<NapiFqPolyComm>,
+    pub lookup_selectors: NapiFqLookupSelectors,
+    pub table_ids: Option<NapiFqPolyComm>,
     pub lookup_info: NapiLookupInfo,
-    pub runtime_tables_selector: Option<WasmFqPolyComm>,
+    pub runtime_tables_selector: Option<NapiFqPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqPlonkVerificationEvals {
-    pub sigma_comm: Vec<WasmFqPolyComm>,
-    pub coefficients_comm: Vec<WasmFqPolyComm>,
-    pub generic_comm: WasmFqPolyComm,
-    pub psm_comm: WasmFqPolyComm,
-    pub complete_add_comm: WasmFqPolyComm,
-    pub mul_comm: WasmFqPolyComm,
-    pub emul_comm: WasmFqPolyComm,
-    pub endomul_scalar_comm: WasmFqPolyComm,
-    pub xor_comm: Option<WasmFqPolyComm>,
-    pub range_check0_comm: Option<WasmFqPolyComm>,
-    pub range_check1_comm: Option<WasmFqPolyComm>,
-    pub foreign_field_add_comm: Option<WasmFqPolyComm>,
-    pub foreign_field_mul_comm: Option<WasmFqPolyComm>,
-    pub rot_comm: Option<WasmFqPolyComm>,
+pub struct NapiFqPlonkVerificationEvals {
+    pub sigma_comm: Vec<NapiFqPolyComm>,
+    pub coefficients_comm: Vec<NapiFqPolyComm>,
+    pub generic_comm: NapiFqPolyComm,
+    pub psm_comm: NapiFqPolyComm,
+    pub complete_add_comm: NapiFqPolyComm,
+    pub mul_comm: NapiFqPolyComm,
+    pub emul_comm: NapiFqPolyComm,
+    pub endomul_scalar_comm: NapiFqPolyComm,
+    pub xor_comm: Option<NapiFqPolyComm>,
+    pub range_check0_comm: Option<NapiFqPolyComm>,
+    pub range_check1_comm: Option<NapiFqPolyComm>,
+    pub foreign_field_add_comm: Option<NapiFqPolyComm>,
+    pub foreign_field_mul_comm: Option<NapiFqPolyComm>,
+    pub rot_comm: Option<NapiFqPolyComm>,
 }
 
 #[napi(object)]
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct WasmFqPlonkVerifierIndex {
-    pub domain: WasmFqDomain,
+pub struct NapiFqPlonkVerifierIndex {
+    pub domain: NapiFqDomain,
     pub max_poly_size: i32,
     pub public_: i32,
     pub prev_challenges: i32,
     pub srs: Vec<u8>,
-    pub evals: WasmFqPlonkVerificationEvals,
-    pub shifts: WasmFqShifts,
-    pub lookup_index: Option<WasmFqLookupVerifierIndex>,
+    pub evals: NapiFqPlonkVerificationEvals,
+    pub shifts: NapiFqShifts,
+    pub lookup_index: Option<NapiFqLookupVerifierIndex>,
     pub zk_rows: i32,
 }
 
 #[napi]
-pub fn caml_pasta_fq_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<WasmFqShifts> {
+pub fn caml_pasta_fq_plonk_verifier_index_shifts(
+    log2_size: i32,
+) -> napi::bindgen_prelude::Result<NapiFqShifts> {
     println!(
         "from napi! caml_pasta_fp_plonk_verifier_index_shifts with log2_size {}",
         log2_size
@@ -100,7 +95,7 @@ pub fn caml_pasta_fq_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<W
     let shifts = KimchiShifts::new(&domain);
     let s = shifts.shifts();
 
-    Ok(WasmFqShifts {
+    Ok(NapiFqShifts {
         s0: serialize_fp(&s[0])?,
         s1: serialize_fp(&s[1])?,
         s2: serialize_fp(&s[2])?,
@@ -111,7 +106,7 @@ pub fn caml_pasta_fq_plonk_verifier_index_shifts(log2_size: i32) -> NapiResult<W
     })
 }
 
-fn serialize_fp(value: &Fq) -> NapiResult<Vec<u8>> {
+fn serialize_fp(value: &Fq) -> napi::bindgen_prelude::Result<Vec<u8>> {
     let mut bytes = Vec::new();
     value
         .serialize_uncompressed(&mut bytes)
