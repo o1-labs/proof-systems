935491af77ee54644d9c06adead843e64be9cf34
dannywillems <dannywillems@users.noreply.github.com>
2025-11-26 20:00:40 +0000

    deploy: e8e02799802b42caf727d69c2dba7fce9548cb5b

--- rustdoc/trait.impl/wasm_types/flat_vector/trait.FlatVectorElem.js
+++ rustdoc/trait.impl/wasm_types/flat_vector/trait.FlatVectorElem.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["arkworks",[["impl <a class=\"trait\" href=\"wasm_types/flat_vector/trait.FlatVectorElem.html\" title=\"trait wasm_types::flat_vector::FlatVectorElem\">FlatVectorElem</a> for <a class=\"struct\" href=\"arkworks/struct.WasmPastaFp.html\" title=\"struct arkworks::WasmPastaFp\">WasmPastaFp</a>"],["impl <a class=\"trait\" href=\"wasm_types/flat_vector/trait.FlatVectorElem.html\" title=\"trait wasm_types::flat_vector::FlatVectorElem\">FlatVectorElem</a> for <a class=\"struct\" href=\"arkworks/struct.WasmPastaFq.html\" title=\"struct arkworks::WasmPastaFq\">WasmPastaFq</a>"]]]]);
+    var implementors = Object.fromEntries([["arkworks",[["impl FlatVectorElem for <a class=\"struct\" href=\"arkworks/struct.WasmPastaFp.html\" title=\"struct arkworks::WasmPastaFp\">WasmPastaFp</a>"],["impl FlatVectorElem for <a class=\"struct\" href=\"arkworks/struct.WasmPastaFq.html\" title=\"struct arkworks::WasmPastaFq\">WasmPastaFq</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[578]}
+//{"start":57,"fragment_lengths":[304]}
