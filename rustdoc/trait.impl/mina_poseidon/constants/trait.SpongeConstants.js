0386b71beed5396993100b38eab26ae45cb370c1
dannywillems <dannywillems@users.noreply.github.com>
2026-01-06 19:58:42 +0000

    deploy: f4bc2881ae710da0ab3455840d59e1cf9a712e0a

--- rustdoc/trait.impl/mina_poseidon/constants/trait.SpongeConstants.js
+++ rustdoc/trait.impl/mina_poseidon/constants/trait.SpongeConstants.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["arrabbiata",[["impl SpongeConstants for <a class=\"struct\" href=\"arrabbiata/curve/struct.PlonkSpongeConstants.html\" title=\"struct arrabbiata::curve::PlonkSpongeConstants\">PlonkSpongeConstants</a>",0]]],["mina_poseidon",[]]]);
+    const implementors = Object.fromEntries([["arrabbiata",[["impl <a class=\"trait\" href=\"mina_poseidon/constants/trait.SpongeConstants.html\" title=\"trait mina_poseidon::constants::SpongeConstants\">SpongeConstants</a> for <a class=\"struct\" href=\"arrabbiata/curve/struct.PlonkSpongeConstants.html\" title=\"struct arrabbiata::curve::PlonkSpongeConstants\">PlonkSpongeConstants</a>",0]]],["mina_poseidon",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[208,21]}
+//{"start":59,"fragment_lengths":[349,21]}
